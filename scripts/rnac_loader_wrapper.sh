b7f3f501cfefad01813161bc9fa9cef5cb78ddb1
Anton Petrov <apetrov@ebi.ac.uk>
2013-08-30 09:41:04 +0100

    Add a convenience wrapper script for the non-hive pipeline

---
+++ scripts/rnac_loader_wrapper.sh
@@ -0,0 +1,7 @@
+perl scripts/rnac_loader.pl -in=$DATA_IN \
+                            -out=$DATA_OUT \
+                            -user=$ORACLE_USER \
+                            -password=$ORACLE_PASSWORD \
+                            -host=$ORACLE_HOST \
+                            -port=$ORACLE_PORT \
+                            -sid=$ORACLE_SID;
