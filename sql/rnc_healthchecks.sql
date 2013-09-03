4aea7ed957c5c9481636507a4963772cba134d1a
Anton Petrov <apetrov@ebi.ac.uk>
2013-09-03 17:04:58 +0100

    Add PLSQL healthchecks

---
+++ sql/rnc_healthchecks.sql
@@ -0,0 +1,117 @@
+CREATE OR REPLACE
+PACKAGE BODY RNC_HEALTHCHECKS AS
+
+  /*
+  * All UPIs must be unique.
+  */
+  PROCEDURE check_unique_upis AS
+    v_distinct_upi NUMBER;
+    v_distinct_md5 NUMBER;
+    v_all_rna_seq  NUMBER;
+  BEGIN
+
+    SELECT count(DISTINCT upi) INTO v_distinct_upi FROM rna;
+    SELECT count(DISTINCT md5) INTO v_distinct_md5 FROM rna;
+    SELECT count(*)            INTO v_all_rna_seq  FROM rna;
+
+    IF v_distinct_upi != v_distinct_md5 AND
+       v_distinct_md5 != v_all_rna_seq THEN
+      DBMS_OUTPUT.put_line('not ok ... check_unique_upis');
+    ELSE
+      DBMS_OUTPUT.put_line('ok ... check_unique_upis, found '
+                            || v_all_rna_seq || ' RNA sequences');
+    END IF;
+
+  END check_unique_upis;
+
+
+  /*
+  * Taxids should not be negative. This problem was encountered once.
+  */
+  PROCEDURE check_negative_taxid AS
+    v_xref NUMBER;
+    v_staging NUMBER;
+  BEGIN
+    SELECT count(*) INTO v_xref FROM xref WHERE taxid < 0;
+    SELECT count(*) into v_staging FROM load_rnacentral_all WHERE taxid < 0;
+
+    IF v_xref != v_staging OR v_xref != 0 THEN
+      DBMS_OUTPUT.put_line('not ok ... check_negative_taxid');
+    ELSE
+      DBMS_OUTPUT.put_line('ok ... check_negative_taxid');
+    end if;
+
+  end check_negative_taxid;
+
+
+  /*
+  * Sometimes the staging table contains duplicate accession numbers.
+  * The update should not start if this happens.
+  */
+  procedure check_unique_ac_staging
+  AS
+    v_ac_all NUMBER;
+    v_ac_distinct NUMBER;
+  BEGIN
+    SELECT count(*) into v_ac_all FROM load_rnacentral_all;
+    SELECT count(DISTINCT ac) into v_ac_distinct FROM load_rnacentral_all;
+
+    IF v_ac_all != v_ac_distinct THEN
+      DBMS_OUTPUT.put_line('not ok ... check_unique_ac_staging');
+    ELSE
+      DBMS_OUTPUT.put_line('ok ... check_unique_ac_staging');
+    end if;
+
+  end check_unique_ac_staging;
+
+  /*
+  * Make sure that all accessions in xref are unique.
+  */
+  procedure check_unique_ac_xref
+  AS
+    v_ac_all NUMBER;
+    v_ac_distinct NUMBER;
+  BEGIN
+    SELECT count(*) INTO v_ac_all FROM xref WHERE deleted='N';
+    SELECT count(DISTINCT ac) into v_ac_distinct FROM xref where deleted='N';
+
+    IF v_ac_all != v_ac_distinct THEN
+      DBMS_OUTPUT.put_line('not ok ... check_unique_ac_xref');
+    ELSE
+      DBMS_OUTPUT.put_line('ok ... check_unique_ac_xref');
+    end if;
+
+  END check_unique_ac_xref;
+
+
+  PROCEDURE check_rnas_without_xrefs
+  AS
+    v_count NUMBER;
+  BEGIN
+    SELECT count(*) INTO v_count FROM xref x, rna r WHERE x.upi (+) = r.upi AND x.upi IS NULL;
+
+    IF v_count != 0 THEN
+      DBMS_OUTPUT.put_line('not ok ... check_rnas_without_xrefs');
+    else
+      DBMS_OUTPUT.put_line('ok ... check_rnas_without_xrefs');
+    END IF;
+
+  END check_rnas_without_xrefs;
+
+
+  /*
+  * Main entry point.
+  */
+  procedure run_healthchecks AS
+  BEGIN
+
+    check_unique_upis;
+    check_negative_taxid;
+    check_unique_ac_staging;
+    check_unique_ac_xref;
+    check_rnas_without_xrefs;
+
+  END run_healthchecks;
+
+
+END RNC_HEALTHCHECKS;
