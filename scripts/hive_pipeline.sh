246e5e5fc7e353cfea4c95ca68185f6e36a6420d
Anton Petrov <apetrov@ebi.ac.uk>
2013-10-21 12:18:05 +0100

    Send emails with import logs

--- scripts/hive_pipeline.sh
+++ scripts/hive_pipeline.sh
@@ -18,3 +18,24 @@ HIVE_URL='mysql://'$HIVE_USERNAME':'$HIVE_PASSWORD'@'$HIVE_HOST':'$HIVE_PORT'/rn
 perl $ENSEMBL_CVS_ROOT_DIR/ensembl-hive/scripts/beekeeper.pl -url $HIVE_URL -sync;
 perl $ENSEMBL_CVS_ROOT_DIR/ensembl-hive/scripts/generate_graph.pl -url $HIVE_URL -output pipeline.png
 perl $ENSEMBL_CVS_ROOT_DIR/ensembl-hive/scripts/beekeeper.pl -url $HIVE_URL -meadow_type LSF -loop -total_running_workers_max 40;
+
+###############
+## Reporting ##
+###############
+# generate title
+if grep --quiet "WARN\|FATAL" log/rnacentral_import.log; then
+    title='[FAILED] RNAcentral data import'
+else
+    title='[OK] RNAcentral data import'
+fi
+# generate body
+echo 'http://test.rnacentral.org' > email_message_body.txt
+grep "WARN\|FATAL" log/rnacentral_import.log >> email_message_body.txt
+# compress log
+gzip log/rnacentral_import.log
+# email the report
+mutt $RNACENTRAL_ADMIN_EMAIL -s "$title" -a log/rnacentral_import.log.gz -i email_message_body.txt < /dev/null
+# remove body
+rm email_message_body.txt
+# uncompress log
+gzip -d log/rnacentral_import.log.gz
