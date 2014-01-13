0d7c9baef1a5fc10c3fd0bcf02e5544f2a7dd4fc
Dan Staines <dstaines@ebi.ac.uk>
2014-01-13 15:46:44 +0000

    updated license

--- sql/utils/dbusage.sql
+++ sql/utils/dbusage.sql
@@ -1,3 +1,17 @@
+-- Copyright [2009-2014] EMBL-European Bioinformatics Institute
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--      http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 select (select decode(extent_management,'LOCAL','*',' ') ||
                decode(segment_space_management,'AUTO','a ','m ')
 	      from dba_tablespaces where tablespace_name = b.tablespace_name) || nvl(b.tablespace_name,
