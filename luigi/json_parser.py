e148b44c7125fe2868685c0fdc7c14151e2991db
Anton Petrov <apetrov@ebi.ac.uk>
2015-10-09 13:22:43 +0100

    Move common methods to a new base class

---
+++ luigi/json_parser.py
@@ -0,0 +1,117 @@
+"""
+Copyright [2009-2014] EMBL-European Bioinformatics Institute
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import json
+import luigi
+import os
+
+from csv_output_writer import CsvOutputWriter
+
+
+class JsonParser(luigi.Task, CsvOutputWriter):  # pylint: disable=W0232
+    """
+    Base class for all json processors.
+    """
+    input_file = luigi.Parameter()
+    destination = luigi.Parameter(default='/tmp')
+    test = luigi.BoolParameter(default=False, significant=False)
+    data = [] # json input data
+    entries = [] # RNAcentralEntry objects
+
+    def get_accession(self, entry, label):
+        """
+        Generate unique accession.
+        Example: CM000670.1:104203457..104215098:ncRNA:lncipedia
+        """
+        return '{parent_accession}.{seq_version}:{start}..{stop}:{ncRNA_type}:{label}'.format(
+            parent_accession=entry.parent_accession,
+            seq_version=entry.seq_version,
+            start=entry.feature_location_start,
+            stop=entry.feature_location_end,
+            ncRNA_type=entry.feature_type,
+            label=label
+        )
+
+    def get_description(self, entry):
+        """
+        Get entry description.
+        """
+        return '{species} {product}'.format(
+            species=entry.species,
+            product=entry.product
+        )
+
+    def get_feature_start_end(self, assembly_info):
+        """
+        Get feature start and end based on all exons.
+        """
+        primary_start = primary_end = []
+        for exon in assembly_info:
+            primary_start.append(exon['primary_start'])
+            primary_end.append(exon['primary_end'])
+        return (min(primary_start), max(primary_end))
+
+    def parse_input_data(self):
+        """
+        Parse the input json file into a Python object.
+        """
+        with self.input().open('r') as f: # pylint: disable=E1101
+            self.data = json.load(f)
+
+    def write_output(self):
+        """
+        Create output files.
+        """
+        self.format_sequence_data(self.entries, self.output()['short'].fn, self.output()['long'].fn) # pylint: disable=E1101
+        self.format_references(self.entries, self.output()['refs'].fn) # pylint: disable=E1101
+        self.format_accession_info(self.entries, self.output()['ac_info'].fn) # pylint: disable=E1101
+        self.format_genomic_locations(self.entries, self.output()['genomic_locations'].fn) # pylint: disable=E1101
+
+    def requires(self):
+        """
+        Make sure that the input file exists.
+        """
+        filename = self.input_file
+        class InputFile(luigi.Task):  # pylint: disable=W0232
+            """
+            Wrap input file in a luigi task.
+            """
+            def output(self):
+                """
+                """
+                return luigi.LocalTarget(filename)
+        return InputFile()
+
+    def run(self):
+        """
+        Main task code.
+        """
+        self.parse_input_data()
+        self.create_rnacentral_entries() # pylint: disable=E1101
+        self.write_output()
+
+    def output(self):
+        """
+        Generate output file paths and return them as a dictionary.
+        """
+        if not os.path.exists(self.destination):
+            os.makedirs(self.destination)
+        outputs = {}
+        folders = ['short', 'long', 'ac_info', 'genomic_locations', 'refs']
+        for folder in folders:
+            subpath = os.path.join(self.destination, folder)
+            if not os.path.exists(subpath):
+                os.makedirs(subpath)
+            filename = ''.join([self.database, '_', os.path.basename(self.input().fn).split('.')[0], '.csv']) # pylint: disable=E1101
+            outputs[folder] = luigi.LocalTarget(os.path.join(subpath, filename))
+        return outputs
--- luigi/json_parser_lncipedia.py
+++ luigi/json_parser_lncipedia.py
@@ -22,49 +22,21 @@ Optional parameters:
     --no_assembly_mapping # do not map LNCipedia data onto the latest assembly (default: False)
 """
 
-import json
 import luigi
-import os
 import requests
 import time
 
 from rnacentral_entry import RNAcentralEntry
-from csv_output_writer import CsvOutputWriter
+from json_parser import JsonParser
 
 
-class JsonParserLncipedia(luigi.Task, CsvOutputWriter):  # pylint: disable=W0232
+class JsonParserLncipedia(JsonParser):  # pylint: disable=W0232
     """
     Luigi Task for converting LNCipedia Json file into csv files
     that can be loaded into the RNAcentral database.
     """
-    input_file = luigi.Parameter()
-    destination = luigi.Parameter(default='/tmp')
-    test = luigi.BoolParameter(default=False, significant=False)
     no_assembly_mapping = luigi.BoolParameter(default=True, significant=False)
-    data = [] # json input data
-    entries = [] # RNAcentralEntry objects
-
-    def get_accession(self, entry):
-        """
-        Generate unique accession.
-        Example: CM000670.1:104203457..104215098:ncRNA:lncipedia
-        """
-        return '{parent_accession}.{seq_version}:{start}..{stop}:{ncRNA_type}:lncipedia'.format(
-            parent_accession=entry.parent_accession,
-            seq_version=entry.seq_version,
-            start=entry.feature_location_start,
-            stop=entry.feature_location_end,
-            ncRNA_type=entry.feature_type
-        )
-
-    def get_description(self, entry):
-        """
-        Get entry description.
-        """
-        return '{species} {product}'.format(
-            species=entry.species,
-            product=entry.product
-        )
+    database = 'lncipedia'
 
     def map_genomic_assembly(self, rnacentral_entry):
         """
@@ -102,37 +74,21 @@ class JsonParserLncipedia(luigi.Task, CsvOutputWriter):  # pylint: disable=W0232
             time.sleep(0.1)
         return rnacentral_entry.assembly_info
 
-    def parse_input_data(self):
-        """
-        Parse the input json file into a Python object.
-        """
-        with self.input().open('r') as f: # pylint: disable=E1101
-            self.data = json.load(f)
-
-    def get_feature_start_end(self, assembly_info):
-        """
-        """
-        primary_start = primary_end = []
-        for exon in assembly_info:
-            primary_start.append(exon['primary_start'])
-            primary_end.append(exon['primary_end'])
-        return (min(primary_start), max(primary_end))
-
     def create_rnacentral_entries(self):
         """
         Process json file into RNAcentralEntry objects that can be written
         to the output files using standard methods.
         """
-        for i, seq in enumerate(self.data):
-            if self.test and i > 100:
+        for i, seq in enumerate(self.data): # pylint: disable=E1101
+            if self.test and i > 100: # pylint: disable=E1101
                 break
 
-            (feature_location_start, feature_location_end) = self.get_feature_start_end(seq['assembly_info'])
+            (feature_location_start, feature_location_end) = self.get_feature_start_end(seq['assembly_info']) # pylint: disable=E1101
 
             rnacentral_entry = RNAcentralEntry(
                 chromosome=seq['chromosome'],
                 common_name=seq['common_name'],
-                database='LNCIPEDIA',
+                database=self.database.upper(),
                 division='XXX', # pylint: disable=W0511
                 feature_location_end=feature_location_end,
                 feature_location_start=feature_location_start,
@@ -148,7 +104,7 @@ class JsonParserLncipedia(luigi.Task, CsvOutputWriter):  # pylint: disable=W0232
                 product=seq['product'],
                 project='PRJ_LNCPD',
                 seq_version=seq['primary_accession'].split('.')[-1],
-                sequence=seq['sequence'].uppercase(),
+                sequence=seq['sequence'].upper(),
                 species=seq['scientific_name'],
                 references=[
                     {
@@ -163,59 +119,11 @@ class JsonParserLncipedia(luigi.Task, CsvOutputWriter):  # pylint: disable=W0232
             for exon in seq['assembly_info']:
                 rnacentral_entry.assembly_info.append(exon)
 
-            rnacentral_entry.accession = self.get_accession(rnacentral_entry)
-            rnacentral_entry.description = self.get_description(rnacentral_entry)
+            rnacentral_entry.accession = self.get_accession(rnacentral_entry, self.database) # pylint: disable=E1101
+            rnacentral_entry.description = self.get_description(rnacentral_entry) # pylint: disable=E1101
             if not self.no_assembly_mapping:
                 rnacentral_entry.assembly_info = self.map_genomic_assembly(rnacentral_entry)
-            self.entries.append(rnacentral_entry)
-        print 'Done'
-
-    def write_output(self):
-        """
-        Create output files.
-        """
-        self.format_sequence_data(self.entries, self.output()['short'].fn, self.output()['long'].fn) # pylint: disable=E1101
-        self.format_references(self.entries, self.output()['refs'].fn) # pylint: disable=E1101
-        self.format_accession_info(self.entries, self.output()['ac_info'].fn) # pylint: disable=E1101
-        self.format_genomic_locations(self.entries, self.output()['genomic_locations'].fn) # pylint: disable=E1101
-
-    def requires(self):
-        """
-        Make sure that the input file exists.
-        """
-        filename = self.input_file
-        class LncipediaFile(luigi.Task):  # pylint: disable=W0232
-            """
-            Wrap input file in a luigi task.
-            """
-            def output(self):
-                """
-                """
-                return luigi.LocalTarget(filename)
-        return LncipediaFile()
-
-    def run(self):
-        """
-        Main task code.
-        """
-        self.parse_input_data()
-        self.create_rnacentral_entries()
-        self.write_output()
-
-    def output(self):
-        """
-        Generate output file paths and return them as a dictionary.
-        """
-        if not os.path.exists(self.destination):
-            os.makedirs(self.destination)
-        outputs = {}
-        folders = ['short', 'long', 'ac_info', 'genomic_locations', 'refs']
-        for folder in folders:
-            subpath = os.path.join(self.destination, folder)
-            if not os.path.exists(subpath):
-                os.makedirs(subpath)
-            outputs[folder] = luigi.LocalTarget(os.path.join(subpath, 'lncipedia_' + folder + '.csv'))
-        return outputs
+            self.entries.append(rnacentral_entry) # pylint: disable=E1101
 
 
 # main entry point
