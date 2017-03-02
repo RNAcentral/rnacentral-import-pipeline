69b62bb3a9f690cef5a03529af5f086e6c7a7502
Blake Sweeney <bsweeney@ebi.ac.uk>
2017-03-02 16:55:13 +0000

    Improve ensembl parsing
    This does a better job at parsing out the data from the EMBL files that
    ensembl produces. This seems to do the right thing based upon what I
    understand of the database. However, it is really important th I check
    over what I think the database is supposed to store.
    
    The next task is to properly integrate this code with the rest of the
    import code base so we can easily export data.

--- luigi/ensembl.py
+++ luigi/ensembl.py
@@ -1,15 +1,34 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright [2009-2017] EMBL-European Bioinformatics Institute
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import os
 import re
+import csv
+import logging
 import itertools as it
 
-# from data import RNAcentralEntry
+from rnacentral_entry import RNAcentralEntry
 
 import attr
 import luigi
 from Bio import SeqIO
-from Bio.SeqFeature import CompoundLocation
 
 from attr.validators import instance_of as is_a
+
+
+logger = logging.getLogger(__name__)
 
 
 @attr.s()
@@ -31,7 +50,7 @@ class Output(object):
 
 
 @attr.s()
-class OutputWriters(object):
+class OutputHandles(object):
     short_sequences = attr.ib(validator=is_a(file))
     long_sequences = attr.ib(validator=is_a(file))
     references = attr.ib(validator=is_a(file))
@@ -40,16 +59,27 @@ class OutputWriters(object):
 
     @classmethod
     def build(cls, output):
+
         return cls(
-            short_sequences=open(output.short_sequences, 'wb'),
-            long_sequences=open(output.long_sequences, 'wb'),
-            references=open(output.references, 'wb'),
-            accessions=open(output.accesion, 'wb'),
-            locations=open(output.locations, 'wb'),
+            short_sequences=open(output.short_sequences, 'w'),
+            long_sequences=open(output.long_sequences, 'w'),
+            references=open(output.references, 'w'),
+            accessions=open(output.accesion, 'w'),
+            locations=open(output.locations, 'w'),
         )
 
     def __enter__(self):
-        return self
+        def as_csv(handle):
+            return csv.writer(handle, delimiter=',', quotechar='"',
+                              quoting=csv.QUOTE_ALL, lineterminator='\n')
+
+        return OutputHandles(
+            short_sequences=as_csv(self.short_sequences),
+            long_sequences=as_csv(self.long_sequences),
+            references=as_csv(self.references),
+            accessions=as_csv(self.accessions),
+            locations=as_csv(self.locations),
+        )
 
     def __exit__(self, *args):
         self.short_sequences.close()
@@ -70,6 +100,22 @@ class GeneInfo(object):
             locus_tag=feature.qualifiers.get('locus_tag', [None])[0],
             description=feature.qualifiers.get('note', [None])[0],
         )
+
+
+def qualifier_value(feature, name, pattern, max_allowed=1):
+    values = set()
+    for note in feature.qualifiers.get(name, []):
+        match = re.match(pattern, note)
+        if match:
+            values.add(match.group(1))
+    if max_allowed is not None and len(values) > max_allowed:
+        raise ValueError("Multiple values (%s) for %s",
+                         ', '.join(sorted(values)), name)
+    if len(values) == 0:
+        return None
+    if max_allowed == 1:
+        return values.pop()
+    return values
 
 
 class EnsemblImporter(luigi.Task):
@@ -113,8 +159,11 @@ class EnsemblImporter(luigi.Task):
         data = it.ifilter(data, lambda e: e.is_valid())
 
     def run(self):
-        with OutputWriters.build(self.output()) as writers:
-            self.write(writers, self.data())
+        with OutputHandles.build(self.output()) as writers:
+            for entry in self.data():
+                if not entry.is_valid():
+                    logger.info("Skipping invalid entry")
+                self.write(writers, entry)
 
 
 class Importer(EnsemblImporter):
@@ -129,11 +178,27 @@ class Importer(EnsemblImporter):
     def is_ncrna(self, feature):
         return feature.type in {'misc_RNA', 'ncRNA'}
 
-    def assemblies(self, data):
-        return []
+    def assembly_info(self, feature):
+        def as_exon(location):
+            return {
+                'primary_start': location.start + 1,
+                'primary_end': int(location.end),
+                'complement': location.strand == -1,
+            }
+
+        parts = [feature.location]
+        if hasattr(feature.location, 'parts'):
+            parts = feature.location.parts
+        return [as_exon(l) for l in parts]
 
     def references(self, data):
-        return []
+        return [{
+            'author': "Andrew Yates, Wasiu Akanni, M. Ridwan Amode, Daniel Barrell, Konstantinos Billis, Denise Carvalho-Silva, Carla Cummins, Peter Clapham, Stephen Fitzgerald, Laurent Gil1 Carlos Garcín Girón, Leo Gordon, Thibaut Hourlier, Sarah E. Hunt, Sophie H. Janacek, Nathan Johnson, Thomas Juettemann, Stephen Keenan, Ilias Lavidas, Fergal J. Martin, Thomas Maurel, William McLaren, Daniel N. Murphy, Rishi Nag, Michael Nuhn, Anne Parker, Mateus Patricio, Miguel Pignatelli, Matthew Rahtz, Harpreet Singh Riat, Daniel Sheppard, Kieron Taylor, Anja Thormann, Alessandro Vullo, Steven P. Wilder, Amonida Zadissa, Ewan Birney, Jennifer Harrow, Matthieu Muffato, Emily Perry, Magali Ruffier, Giulietta Spudich, Stephen J. Trevanion, Fiona Cunningham, Bronwen L. Aken, Daniel R. Zerbino, Paul Flicek",
+            'location': "Nucleic Acids Res. 2016 44 Database issue:D710-6",
+            'title': "Ensembl 2016",
+            'pmid': 26687719,
+            'doi': "10.1093/nar/gkv115",
+        }]
 
     def standard_annotations(self, record):
         pattern = re.compile('\((.+)\)$')
@@ -144,7 +209,13 @@ class Importer(EnsemblImporter):
             common_name = match.group(1)
             species = re.sub(pattern, '', species).strip()
 
+        taxid = None
+        source = record.features[0]
+        if source.type == 'source':
+            taxid = int(qualifier_value(source, 'db_xref', '^taxon:(\d+)$'))
+
         return {
+            'accession': None,
             'database': 'ensembl',
             'lineage': '; '.join(record.annotations['taxonomy']),
             'mol_type': 'genomic DNA',
@@ -153,32 +224,28 @@ class Importer(EnsemblImporter):
             'seq_version': '',
             'common_name': common_name,
             'species': species,
+            'ncbi_tax_id': taxid,
+            'is_composite': 'N',
+            'references': self.references(record),
+            'sequence': record.seq,
         }
 
-    def description(self, anntotations, feature):
-        return '$species $rna_type $product'.format(
-            species=anntotations['species'],
-            rna_type=None,
-            product=None
+    def description(self, annotations, feature):
+        species = annotations['species']
+        if annotations.get('common_name', None):
+            species += ' (%s)' % annotations['common_name']
+
+        rna_type = self.ncrna(feature) or feature.type
+        return '{species} {rna_type}'.format(
+            species=species,
+            rna_type=rna_type,
         )
 
     def gene(self, feature):
-        gene = feature.qualifiers.get('gene', [])
-        if gene:
-            return gene[0]
-        return None
+        return qualifier_value(feature, 'gene', '^(.+)$')
 
     def transcript(self, feature):
-        transcripts = set()
-        for note in feature.qualifiers.get('note', []):
-            match = re.match('^transcript_id=(.+)$', note)
-            if match:
-                transcripts.add(match.group(1))
-        if not transcripts:
-            return None
-        if len(transcripts) > 1:
-            raise ValueError("Multiple transcripts for %s" % str(feature))
-        return transcripts.pop()
+        return qualifier_value(feature, 'note', '^transcript_id=(.+)$')
 
     def ncrna(self, feature):
         return feature.qualifiers.get('note', [''])[0]
@@ -189,30 +256,47 @@ class Importer(EnsemblImporter):
             return note[1:]
         return None
 
-    def is_composite(self, feature):
-        if issubclass(feature.location.__class__, CompoundLocation):
-            return 'Y'
-        return 'N'
+    def primary_id(self, annotations, feature):
+        transcript = self.transcript(feature)
+        ncrna = self.ncrna(feature)
+        assert transcript, 'Cannot primary id without transcript'
+        assert ncrna, 'Cannot primary id without ncRNA type'
+        assert annotations['parent_accession']
 
-    def create_rnacentral_entries(self, annotations, feature):
-        return RNAcentralEntry.with_constants(
-            annotations,
-            primary_id=feature.id,
-            sequence=str(feature.seq),
-            ncbi_tax_id=annotations['taxid'],
-            accession=None,
-            assembly_info=self.assemblies(feature),
-            db_xrefs=feature.qualifiers.get('db_xref', []),
-            description=self.description(annotations, feature),
-            feature_location_end=int(feature.location.end),
-            feature_location_start=int(feature.location.start),
-            feature_type=feature.type,
-            gene=self.gene(feature),
-            is_composite=self.is_composite(feature),
-            ncrna_class=self.ncrna(feature),
-            note=self.note(feature),
-            optional_id='',
-            references=self.references(feature),
-            standard_name='',
-            locus_tag=feature.qualifiers.get('locus_tag', ''),
+        return '{parent}:{transcript}:{type}'.format(
+            parent=annotations['parent_accession'],
+            transcript=transcript,
+            type=ncrna,
         )
+
+    def sequence(self, annotations, feature):
+        return str(feature.extract(annotations['sequence']))
+
+    def entry_specific_data(self, feature):
+        start, end = sorted([int(feature.location.start),
+                             int(feature.location.end)])
+
+        # Ensure that this is 1 based, even though biopython
+        # switches to 0 based
+        start += 1
+
+        return {
+            'primary_id': self.transcript(feature),
+            'assembly_info': self.assembly_info(feature),
+            'db_xrefs': feature.qualifiers.get('db_xref', []),
+            'feature_location_start': start,
+            'feature_location_end': end,
+            'feature_type': feature.type,
+            'gene': self.gene(feature),
+            'ncrna_class': self.ncrna(feature),
+            'note': self.note(feature),
+            'locus_tag': feature.qualifiers.get('locus_tag', ''),
+        }
+
+    def rnacentral_entry(self, annotations, feature):
+        data = dict(annotations)
+        data.update(self.entry_specific_data(feature))
+        data['accession'] = self.primary_id(annotations, feature)
+        data['description'] = self.description(annotations, feature)
+        data['sequence'] = self.sequence(annotations, feature)
+        return RNAcentralEntry(**data)
--- luigi/tests/ensembl_test.py
+++ luigi/tests/ensembl_test.py
@@ -1,4 +1,5 @@
-import os
+# -*- coding: utf-8 -*-
+
 import unittest as ut
 
 import pytest
@@ -8,14 +9,13 @@ from Bio import SeqIO
 from ensembl import Importer
 
 
-class ParsingTest(ut.TestCase):
-    filename = 'Taeniopygia_guttata.taeGut3.2.4.87.chromosome.1.dat'
+class BaseTest(ut.TestCase):
+    filename = 'luigi/tests/files/Taeniopygia_guttata.taeGut3.2.4.87.chromosome.1.dat'
 
     @classmethod
     def setUpClass(cls):
         cls.features = {}
-        path = os.path.join('tests/files', cls.filename)
-        cls.record = SeqIO.read(path, 'embl')
+        cls.record = SeqIO.read(cls.filename, 'embl')
         for feature in cls.record.features:
             gene = feature.qualifiers.get('gene', [None])[0]
             key = (feature.type, gene)
@@ -24,6 +24,8 @@ class ParsingTest(ut.TestCase):
     def setUp(self):
         self.importer = Importer(input_file=self.filename)
 
+
+class FeatureParsingTest(BaseTest):
     def __getattr__(self, key):
         if hasattr(self.importer, key):
             fn = getattr(self.importer, key)
@@ -31,7 +33,16 @@ class ParsingTest(ut.TestCase):
         raise AttributeError("Unknown attribute %s" % key)
 
 
-class BasicTests(ParsingTest):
+class BothParsingTest(BaseTest):
+    def __getattr__(self, key):
+        if hasattr(self.importer, key):
+            annotation = self.importer.standard_annotations(self.record)
+            fn = getattr(self.importer, key)
+            return lambda *key: fn(annotation, self.features[key])
+        raise AttributeError("Unknown attribute %s" % key)
+
+
+class SimpleTests(FeatureParsingTest):
 
     def test_can_detect_if_is_noncoding(self):
         assert self.is_ncrna('gene', 'ENSTGUG00000005151.1') is False
@@ -59,6 +70,17 @@ class BasicTests(ParsingTest):
             'seq_version': '',
             'common_name': 'zebra finch',
             'species': "Taeniopygia guttata",
+            'ncbi_tax_id':  59729,
+            'is_composite': 'N',
+            'accession': None,
+            'references': [{
+                'author': "Andrew Yates, Wasiu Akanni, M. Ridwan Amode, Daniel Barrell, Konstantinos Billis, Denise Carvalho-Silva, Carla Cummins, Peter Clapham, Stephen Fitzgerald, Laurent Gil1 Carlos Garcín Girón, Leo Gordon, Thibaut Hourlier, Sarah E. Hunt, Sophie H. Janacek, Nathan Johnson, Thomas Juettemann, Stephen Keenan, Ilias Lavidas, Fergal J. Martin, Thomas Maurel, William McLaren, Daniel N. Murphy, Rishi Nag, Michael Nuhn, Anne Parker, Mateus Patricio, Miguel Pignatelli, Matthew Rahtz, Harpreet Singh Riat, Daniel Sheppard, Kieron Taylor, Anja Thormann, Alessandro Vullo, Steven P. Wilder, Amonida Zadissa, Ewan Birney, Jennifer Harrow, Matthieu Muffato, Emily Perry, Magali Ruffier, Giulietta Spudich, Stephen J. Trevanion, Fiona Cunningham, Bronwen L. Aken, Daniel R. Zerbino, Paul Flicek",
+                'location': "Nucleic Acids Res. 2016 44 Database issue:D710-6",
+                'title': "Ensembl 2016",
+                'pmid': 26687719,
+                'doi': "10.1093/nar/gkv115",
+            }],
+            'sequence': self.record.seq,
         }
 
     def test_can_get_transcript_id(self):
@@ -73,30 +95,137 @@ class BasicTests(ParsingTest):
             "transcript_id=ENSTGUT00000018323.1",
         ]
 
+    def test_can_create_easy_locations(self):
+        assert self.assembly_info('gene', 'ENSTGUG00000011206.1') == [
+            {'complement': False, 'primary_start': 41661941, 'primary_end': 41662687}
+        ]
+
+    def test_can_get_joined_locations(self):
+        assert self.assembly_info('mRNA', 'ENSTGUG00000011242.1') == [
+             {'complement': True, 'primary_start': 44551450, 'primary_end': 44552280},
+             {'complement': True, 'primary_start': 44550508, 'primary_end': 44551428},
+             {'complement': True, 'primary_start': 44550496, 'primary_end': 44550506},
+             {'complement': True, 'primary_start': 44550265, 'primary_end': 44550427},
+        ]
+
     def test_can_get_gene_id(self):
         assert self.gene('misc_RNA', "ENSTGUG00000017636.1") == \
             "ENSTGUG00000017636.1"
         assert self.gene('misc_RNA', "ENSTGUG00000017636.1") == \
             "ENSTGUG00000017636.1"
 
-    def test_knows_if_is_composite(self):
-        assert self.is_composite('misc_RNA', "ENSTGUG00000017636.1") is 'N'
-        assert self.is_composite('mRNA', "ENSTGUG00000006067.1") is 'Y'
+
+class LongSpeciesTests(FeatureParsingTest):
+    filename = 'data/test_species_patch.ncr'
+
+    def test_can_load_large_organism_name(self):
+        """This test is meant to see if the biopython reader can handle parsing
+        an EMBL file where the organism name may span more than one name.
+        """
+
+        assert self.importer.standard_annotations(self.record) == {
+            'database': 'ensembl',
+            'lineage': (
+                'Eukaryota; Fungi; Dikarya; Basidiomycota; Agaricomycotina; '
+                'Agaricomycetes; Russulales; Russulaceae; Russula; '
+                'environmental samples'
+            ),
+            'mol_type': 'genomic DNA',
+            'pseudogene': 'N',
+            'parent_accession': "EF411133.1:1..726:misc_RNA",
+            'seq_version': '',
+            'common_name': 'Russula brevipes var. acrior',
+            'species': 'uncultured ectomycorrhiza',
+            'ncbi_tax_id':  446167,
+            'is_composite': 'N',
+            'accession': None,
+
+            # Note this is *NOT* the reference in the file and this is on
+            # purpose. The parser here is not a general EMBL format parser, but
+            # a parser for the data produced by ENSEMBL. For this reason we
+            # only use a default reference, and not what may be in the file.
+            'references': [{
+                'author': """Andrew Yates, Wasiu Akanni, M. Ridwan Amode, Daniel Barrell, Konstantinos Billis, Denise Carvalho-Silva, Carla Cummins, Peter Clapham, Stephen Fitzgerald, Laurent Gil1 Carlos Garcín Girón, Leo Gordon, Thibaut Hourlier, Sarah E. Hunt, Sophie H. Janacek, Nathan Johnson, Thomas Juettemann, Stephen Keenan, Ilias Lavidas, Fergal J. Martin, Thomas Maurel, William McLaren, Daniel N. Murphy, Rishi Nag, Michael Nuhn, Anne Parker, Mateus Patricio, Miguel Pignatelli, Matthew Rahtz, Harpreet Singh Riat, Daniel Sheppard, Kieron Taylor, Anja Thormann, Alessandro Vullo, Steven P. Wilder, Amonida Zadissa, Ewan Birney, Jennifer Harrow, Matthieu Muffato, Emily Perry, Magali Ruffier, Giulietta Spudich, Stephen J. Trevanion, Fiona Cunningham, Bronwen L. Aken, Daniel R. Zerbino, Paul Flicek""",
+                'location': "Nucleic Acids Res. 2016 44 Database issue:D710-6",
+                'title': "Ensembl 2016",
+                'pmid': 26687719,
+                'doi': "10.1093/nar/gkv115",
+            }],
+            'sequence': self.record.seq,
+        }
 
 
-class LoadingTests(ut.TestCase):
+class LoadingTests(FeatureParsingTest):
+
+    def entry(self, *key):
+        return self.entry_specific_data(*key)
+
+    def test_produces_correct_entries(self):
+        assert self.entry('misc_RNA', "ENSTGUG00000017978.1") == {
+            'assembly_info': [{
+                'complement': True,
+                'primary_start': 43202354,
+                'primary_end': 43202459,
+            }],
+            'db_xrefs': [],
+            'feature_location_end':  43202459,
+            'feature_location_start': 43202354,
+            'feature_type': 'misc_RNA',
+            'gene': "ENSTGUG00000017978.1",
+            'ncrna_class': 'miRNA',
+            'note': ['transcript_id=ENSTGUT00000018665.1'],
+            'locus_tag': '',
+            'primary_id': 'ENSTGUT00000018665.1',
+        }
+
+    def test_can_get_xrefs(self):
+        assert self.entry('misc_RNA', "ENSTGUG00000017886.1") == {
+            'assembly_info': [
+                {'complement': True, 'primary_start': 43202481, 'primary_end': 43202567},
+            ],
+            'db_xrefs': ["RNACentral:URS000075DEE3"],
+            'feature_location_end': 43202567,
+            'feature_location_start': 43202481,
+            'feature_type': 'misc_RNA',
+            'gene': "ENSTGUG00000017886.1",
+            'ncrna_class': 'miRNA',
+            'note': ["transcript_id=ENSTGUT00000018573.1"],
+            'locus_tag': '',
+            'primary_id': 'ENSTGUT00000018573.1',
+        }
+
+    def test_can_have_several_xrefs(self):
+        assert self.entry('misc_RNA', "ENSTGUG00000018724.1") == {
+            'assembly_info': [
+                {'complement': False, 'primary_start': 48906815, 'primary_end': 48906895},
+            ],
+            'db_xrefs': ["RFAM_trans_name:SNORD102-201",
+                         "RNACentral:URS0000661E55"],
+            'feature_location_end': 48906895,
+            'feature_location_start': 48906815,
+            'feature_type': 'misc_RNA',
+            'gene': "ENSTGUG00000018724.1",
+            'ncrna_class': 'snoRNA',
+            'note': ["transcript_id=ENSTGUT00000019440.1"],
+            'locus_tag': '',
+            'primary_id': 'ENSTGUT00000019440.1',
+        }
+
+
+class CompleteParsingTest(BothParsingTest):
+    def test_can_create_reasonable_description(self):
+        assert self.description('misc_RNA', "ENSTGUG00000018724.1") == \
+            "Taeniopygia guttata (zebra finch) snoRNA"
+
+    def test_can_create_reasonable_primary_id(self):
+        assert self.primary_id('misc_RNA', "ENSTGUG00000017725.1") == \
+            "1.taeGut3.2.4:ENSTGUT00000018412.1:miRNA"
+
+    def test_produces_valid_data(self):
+        entry = self.rnacentral_entry('misc_RNA', "ENSTGUG00000017725.1")
+        assert entry.sequence == 'GGAATATTCTGGGAGTTGTAGTCTTTCAAACAGAGCTTCACAAGGACATACCTGTATTGGAACACTACAGCTCCCTGAACTTCC'
+        assert entry.is_valid(verbose=True) is True
 
     @pytest.mark.skip()
     def test_can_loads_all_non_coding_rnas(self):
-        pass
-
-
-class DataTests(ut.TestCase):
-
-    @pytest.mark.skip()
-    def test_can_create_correct_taxonomy(self):
-        pass
-
-    @pytest.mark.skip()
-    def test_can_detect_correct_taxid(self):
         pass
