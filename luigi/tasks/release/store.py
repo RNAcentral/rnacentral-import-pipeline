f7fcef282398fdb7c121f869ec6b97dee5d6bbe6
Anton Petrov <apetrov@ebi.ac.uk>
2017-10-04 14:48:59 +0100

    Update release loading instructions

--- luigi/tasks/release/store.py
+++ luigi/tasks/release/store.py
@@ -48,26 +48,20 @@ class StoreRelease(luigi.Task):  # pylint: disable=R0904
 
     def run(self):
         with cursor(db()) as cur:
+            cur.execute("SET work_mem TO '1GB'")
             cur.execute(CREATE_INDEX)
             cur.execute("select rnc_update.prepare_releases('F')")
             cur.execute(SQL)
             for result in cur.fetchall():
                 cur.execute('select rnc_update.new_update_release(%s, %s)',
                             (result[0], result[1]))
+            cur.execute('select rnc_update.update_rnc_accessions()')
+            cur.execute('select rnc_update.update_literature_references()')
 
-                # Analyze loaded table
-                cur.execute('VACUUM (VERBOSE,ANALYZE) rnacen.load_rnc_accessions')
         with open(self.output().fn , 'w') as sentinel_file:
             sentinel_file.write('Done')
 
-                # Compile update procedure? - Probably not needed
-                # psql -d pfmegrnapro -h pgsql-hxvm-038.ebi.ac.uk -U rnacen -f schema/packages/rnc_update/update_rnc_accessions_package.sql
 
-                # run reviewed procedures w/o index creation on
-                # load_rnc_accession (not used in plan) and with work_mem=1GB
-                cur.execute('set work_mem=1GB')
-                cur.execute('select rnc_update.update_rnc_accessions()')
-                cur.execute('select rnc_update.update_literature_references()')
     def output(self):
         """
         Check that a sentinel file exists.
