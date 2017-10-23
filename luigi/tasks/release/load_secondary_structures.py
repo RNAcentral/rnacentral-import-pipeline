cdc1014fdfbd17b912778a8ec47b9a83d6ff5f06
Blake Sweeney <bsweeney@ebi.ac.uk>
2017-10-23 12:01:47 +0100

    Add more checks to loading secondary structures
    This should be optional if no data is around and work when there is
    data.

--- luigi/tasks/release/load_secondary_structures.py
+++ luigi/tasks/release/load_secondary_structures.py
@@ -14,13 +14,19 @@ limitations under the License.
 """
 
 import os
+import logging
+from glob import glob
 
 import luigi
 
 from tasks.config import output
 from tasks.utils.pgloader import PGLoader
 
+from .manage_files import SplitMergedFile
 from .utils.generic import file_pattern
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 CONTROL_FILE = """
@@ -92,13 +98,45 @@ class LoadSecondaryStructures(PGLoader):  # pylint: disable=R0904
     """
 
     database = luigi.Parameter(default='all')
+    directory = 'secondary_structure'
+
+    def requires(self):
+        return SplitMergedFile(directory=self.directory)
+
+    def data_directory(self):
+        """
+        Gets the path to the directory of secondary structures.
+        """
+        return os.path.join(output().base, self.directory)
+
+    def file_pattern(self):
+        """
+        Build the pattern of filenames to use.
+        """
+        return file_pattern(self.database)
+
+    def data_files(self):
+        """
+        Get the fill path, with pattern to the filenames to use.
+        """
+        return os.path.join(self.data_directory(), self.file_pattern())
 
     def control_file(self):
         tablename = 'rnc_secondary_structure'
         return CONTROL_FILE.format(
-            pattern=file_pattern(self.database),
-            directory=os.path.join(output().base, 'secondary_structure'),
+            pattern=self.file_pattern(),
+            directory=self.data_directory(),
             tablename=tablename,
             db_url=self.db_url('load_%s' % tablename),
             search_path=self.db_search_path(),
         )
+
+    def run(self):
+        """
+        It is possible that the directory of secondary structures does not
+        exist. If this is the case then nothing is actually run.
+        """
+
+        if glob(self.data_files()):
+            return super(LoadSecondaryStructures, self).run()
+        LOGGER.info("No secondary structures found, skipping")
