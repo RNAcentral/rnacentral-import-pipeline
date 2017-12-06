d07a9402f7bfa3e32542b231cbbfc2f6b17a2511
Blake Sweeney <bsweeney@ebi.ac.uk>
2017-12-06 14:46:18 +0000

    Initial ENA parser
    This is a very simple ENA parser. It can correctly parse a single
    example that I picked at random. This does not include all the data that
    could be included. This will be extended as I find more examples to work
    with.
    
    In addition, I've started moving around the generic helpers for working
    with EMBL files. We also parse EMBL files from Ensembl so I have some
    common functions that I could move around. I probably broke Ensembl
    parsing for the moment, but I will add that back later.

--- luigi/databases/ena/parsers.py
+++ luigi/databases/ena/parsers.py
@@ -13,6 +13,75 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+from Bio import SeqIO
 
-def parser(handle):
-    return []
+from databases.data import Entry
+
+from . import helpers
+import databases.helpers.embl as embl
+
+
+def parse(handle):
+    """
+    Parse a file like object into an iterable of Entry objects. This will parse
+    each feature in all records of the given EMBL formatted file to produce the
+    Entry objects.
+    """
+
+    for record in SeqIO.parse(handle, 'embl'):
+        taxid = embl.taxid(record)
+        species = embl.species(record)
+        chromosome = helpers.chromosome(record)
+        common_name = embl.common_name(record)
+        lineage = embl.lineage(record)
+        project = embl.project(record)
+        description = embl.description(record)
+        seq_version = embl.seq_version(record)
+        accession = helpers.accession(record)
+        division = embl.division(record)
+        references = embl.references(accession, record)
+        sequence = helpers.sequence(record)
+
+        for feature in record.features[1:]:
+            yield Entry(
+                primary_id=helpers.primary_id(feature),
+                accession=accession,
+                ncbi_tax_id=taxid,
+                database='ENA',
+                sequence=sequence,
+                exons=helpers.exons(feature),
+                rna_type=helpers.rna_type(feature),
+                url=helpers.url(feature),
+                seq_version=seq_version,
+                note_data=helpers.note_data(feature),
+                xref_data=embl.xref_data(feature),
+                chromosome=chromosome,
+                species=species,
+                common_name=common_name,
+                lineage=lineage,
+                gene=embl.gene(feature),
+                locus_tag=embl.locus_tag(feature),
+                product=helpers.product(feature),
+                parent_accession=helpers.parent_accession(record),
+                ordinal=helpers.ordinal(feature),
+                non_coding_id=helpers.non_coding_id(feature),
+                project=project,
+                keywords=helpers.keywords(record),
+                division=division,
+                organelle=helpers.organelle(feature),
+                allele=helpers.allele(feature),
+                anticodon=helpers.anticodon(feature),
+                experiment=helpers.experiment(feature),
+                function=helpers.function(feature),
+                inference=helpers.inference(feature),
+                map=helpers.map(feature),
+                old_locus_tag=helpers.old_locus_tag(feature),
+                operon=helpers.operon(feature),
+                standard_name=embl.standard_name(feature),
+                description=description,
+                mol_type=helpers.mol_type(record),
+                is_composite=helpers.is_composite(feature),
+                pseudogene=helpers.pseudogene(feature),
+                gene_synonyms=helpers.gene_synonyms(feature),
+                references=references,
+            )
