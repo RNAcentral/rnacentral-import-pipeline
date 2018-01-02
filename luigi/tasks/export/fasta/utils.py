e09df167b2a5bfd9473a409d90aedc3090dd8b50
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-01-02 14:38:21 +0000

    Rework utils and test
    This adds tests to the fasta export utils as well as making the code
    more testable. The changes are just to support easier testing. I also
    allowed the code to find the path to psql using distutils. Seems in my
    setup that doesn't always work correctly, so this deals with that.

--- luigi/tasks/export/fasta/utils.py
+++ luigi/tasks/export/fasta/utils.py
@@ -18,6 +18,8 @@ import tempfile
 from contextlib import contextmanager
 import subprocess as sp
 
+from distutils.spawn import find_executable
+
 import luigi
 from luigi.local_target import atomic_file
 
@@ -35,13 +37,45 @@ def tsv_to_records(tsv_lines):
     of Bio.SeqRecord.SeqRecord items.
     """
 
+    seen = False
     for line in tsv_lines:
         sid, description, sequence = line.strip().split('\t')
+        seen = True
         yield SeqRecord(
             Seq(sequence),
             id=sid,
             description=description,
         )
+
+    if not seen:
+        raise ValueError("No lines found")
+
+
+@contextmanager
+def psql_copy(config, sql, psql=None):
+    """
+    This will dump the results of a the query to a TSV file and then create
+    a context manage with a file handle of that file. The file is temporary
+    and is deleted once the handler exits.
+    """
+
+    if psql is None:
+        psql = find_executable('psql')
+
+    with tempfile.NamedTemporaryFile() as out:
+        query = sql.replace('\n', ' ')
+        command = 'COPY ({query}) TO STDOUT'.format(query=query)
+        process = sp.Popen(
+            [psql, '-c', command, config.pgloader_url()],
+            env={'PGPASSWORD': config.password},
+            stdout=out,
+        )
+
+        if process.wait() != 0:
+            raise ValueError('Failed to run psql')
+
+        with open(out.name, 'rb') as readable:
+            yield readable
 
 
 def populate_table_by_partition(connection, table, populate):
@@ -67,31 +101,6 @@ class FastaExportBase(luigi.Task):
     def output(self):
         return luigi.LocalTarget(export().sequences(self.filename))
 
-    @contextmanager
-    def tsv_file(self):
-        """
-        This will dump the results of a the query to a TSV file and then create
-        a context manage with a file handle of that file. The file is temporary
-        and is deleted once the handler exits.
-        """
-
-        config = db()
-        with tempfile.NamedTemporaryFile(delete=False) as out:
-            print(out.name)
-            query = self.fetch.replace('\n', ' ')
-            command = 'COPY ({query}) TO STDOUT'.format(query=query)
-            process = sp.Popen(
-                ['psql', '-c', command, config.pgloader_url()],
-                env={'PGPASSWORD': config.password},
-                stdout=out,
-            )
-
-            if process.wait() != 0:
-                raise ValueError('Failed to run psql')
-
-            with open(out.name, 'rb') as readable:
-                yield readable
-
     def run(self):
         filename = self.output().fn
         try:
@@ -99,5 +108,5 @@ class FastaExportBase(luigi.Task):
         except:
             pass
 
-        with self.tsv_file() as tsv_file, atomic_file(filename) as out:
+        with psql_copy(db(), self.fetch) as tsv_file, atomic_file(filename) as out:
             SeqIO.write(tsv_to_records(tsv_file), out, "fasta")
