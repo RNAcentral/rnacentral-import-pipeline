2b7efdae3dc9a5b9be9e6387ed0f7afade5c77b9
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-01-09 16:49:51 +0000

    Rework Rfam export
    The logic for Rfam export should live outside luigi tasks, the way all
    my other code is organized. This moves code around to do that. In
    addition I have used the generic compress task as part of the rfam
    export. Finally, I've added tests for the logic. They are simple (and
    very slow) but at least cover some functionality.

--- luigi/tests/tasks/helpers.py
+++ luigi/tests/tasks/helpers.py
@@ -13,26 +13,20 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import luigi
+import psycopg2
+import psycopg2.extras
 
-from tasks.utils.compress import GenericCompressTask
-
-from .rfam_annotations import RfamAnnotations
-from .readme import RfamAnnotationsReadMe
-from .examples import RfamAnnotationsExample
+from tasks.config import db
 
 
-class Compress(GenericCompressTask):
-    def inputs(self):
-        yield RfamAnnotations()
+def count(sql):
+    """
+    Returns the count of the nubmer of elements returned by the given sql
+    statement. This will open a new connection to the database.
+    """
 
-    def requires(self):
-        yield RfamAnnotations()
-        yield RfamAnnotationsExample()
-
-
-class ExportRfamAnnotaitons(luigi.WrapperTask):
-    def requires(self):
-        yield RfamAnnotationsReadMe()
-        yield RfamAnnotationsExample()
-        yield Compress()
+    connection = psycopg2.connect(db().pgloader_url())
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute('select count(*) total from (%s) t' % sql)
+    result = cursor.fetchone()
+    return result['total']
