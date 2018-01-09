db8c39b70e15ba5f68214f86256d21ef964e5011
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-01-09 16:49:51 +0000

    Rework Rfam export
    The logic for Rfam export should live outside luigi tasks, the way all
    my other code is organized. This moves code around to do that. In
    addition I have used the generic compress task as part of the rfam
    export. Finally, I've added tests for the logic. They are simple (and
    very slow) but at least cover some functionality.

--- luigi/tasks/export/ftp/rfam/rfam_annotations.py
+++ luigi/tasks/export/ftp/rfam/rfam_annotations.py
@@ -13,70 +13,23 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import os
-
 import luigi
-from luigi.local_target import atomic_file
+
+from tasks.utils.files import atomic_output
 
 from tasks.release.utils.db import get_db_connection
 from tasks.config import db
 from tasks.config import export
 
-CREATE_ACTIVE_TABLE = """
-CREATE TEMP TABLE rna_active (
-    upi varchar(30) primary KEY
-)
-"""
-
-INSERT_INTO_ACTIVE_TABLE = """
-INSERT INTO rna_active
-select distinct upi from xref_p{dbid}_not_deleted
-ON CONFLICT DO NOTHING
-"""
-
-QUERY = """
-select
-    hits.upi,
-    hits.rfam_model_id,
-    score,
-    e_value,
-    sequence_start,
-    sequence_stop,
-    model_start,
-    model_stop,
-    models.long_name
-from rfam_model_hits hits
-join rna_active active on active.upi = hits.upi
-join rfam_models models on models.rfam_model_id = hits.rfam_model_id
-"""
-
+from internal.export.ftp import rfam_annotations as ann
 
 
 class RfamAnnotations(luigi.Task):
     def output(self):
-        filename = export().rfam('rfam_annotations.tsv')
-        return luigi.LocalTarget(filename)
-
-    def command(self, example=False):
-        query = QUERY.replace('\n', '')
-        if example:
-            query = '%s LIMIT 10' % query
-        return "COPY ({query}) to STDOUT DELIMITER '\t'".format(
-            query=QUERY.replace('\n', ' '),
-        )
-
-    def populate_active_table(self, connection):
-        cursor = connection.cursor()
-        cursor.execute(CREATE_ACTIVE_TABLE)
-        cursor.execute('select id from rnc_database order by id')
-        dbids = [r[0] for r in cursor.fetchall()]
-        for dbid in dbids:
-            cursor.execute(INSERT_INTO_ACTIVE_TABLE.format(dbid=dbid))
+        return luigi.LocalTarget(export().rfam('rfam_annotations.tsv'))
 
     def run(self):
         connection = get_db_connection(db(), connect_timeout=10 * 60)
-        # self.populate_active_table(connection)
-        with atomic_file(self.output().fn) as out:
-            cursor = connection.cursor()
-            cursor.copy_expert(self.command(), out)
-            connection.close()
+        with atomic_output(self.output()) as out:
+            ann.write(connection, out)
+        connection.close()
