1317d6d55f09d6b199141b56e91d92bf7490d89a
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-02-02 15:02:08 +0000

    Remove unneeded error handling
    That makes some of errors harder to understand actually.

--- luigi/tasks/utils/entry_writers.py
+++ luigi/tasks/utils/entry_writers.py
@@ -139,26 +139,22 @@ class ReferenceWriter(object):
         """
 
         for reference in data.references:
-            try:
-                authors = reference.authors
-                if authors:
-                    authors = authors.encode('ascii', 'ignore')
-                title = reference.title
-                if title:
-                    title = title.encode('ascii', 'ignore')
+            authors = reference.authors
+            if authors:
+                authors = authors.encode('ascii', 'ignore')
+            title = reference.title
+            if title:
+                title = title.encode('ascii', 'ignore')
 
-                self.csv.writerow([
-                    reference.md5(),
-                    data.accession,
-                    authors,
-                    reference.location,
-                    title,
-                    reference.pmid,
-                    reference.doi,
-                ])
-            except Exception as err:
-                print(reference)
-                raise err
+            self.csv.writerow([
+                reference.md5(),
+                data.accession,
+                authors,
+                reference.location,
+                title,
+                reference.pmid,
+                reference.doi,
+            ])
 
 
 @attr.s()
