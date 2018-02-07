7d3f58d12ef7075cc106254d68faa2ab1358f8e7
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-02-07 11:22:29 +0000

    Remove copy task
    We can remove the ENA specific copy task in favor of the generic Fetch
    task. Reducing code is nice.

--- luigi/tasks/ena/directory.py
+++ luigi/tasks/ena/directory.py
@@ -22,23 +22,23 @@ from databases.ena.parsers import parse_with_mapping_files
 
 from tasks.config import ena
 from tasks.config import output
-from tasks.utils.fetch import FetchTask
 from tasks.utils.entry_writers import Output
 
-from .copy import CopyNcr
+from . import utils
 
 
 class EnaDirectory(luigi.Task):
+    """
+    Import all data in an an NCR directory from ENA. This will produce a single
+    output file for all the files directly below the directory.
+    """
+
     input_dir = luigi.Parameter()
 
     def requires(self):
-        yield CopyNcr(ncr=self.input_dir)
-
-        conf = ena()
-        for database in ena().tpa_databases:
-            local_path = conf.input_tpa_file(database)
-            remote_path = conf.raw_tpa_url(database)
-            yield FetchTask(remote_path=remote_path, local_path=local_path)
+        yield utils.copy_ncr_task(self.input_dir)
+        for task in utils.tpa_tasks():
+            yield task
 
     def output(self):
         prefix = os.path.basename(self.input_dir)
@@ -46,13 +46,15 @@ class EnaDirectory(luigi.Task):
 
     def handles(self):
         """
-        Produce an interable for all compressed non-coding product files that
+        Produce an iterable for all compressed non-coding product files that
         this task imports.
         """
 
-        base_dir = CopyNcr(ncr=self.input_dir).output().fn
+        base_dir = utils.copy_ncr_task(self.input_dir).output().fn
         files = os.listdir(base_dir)
         for filename in files:
+            if not filename.endswith('.ncr.gz'):
+                continue
             filename = os.path.join(self.input_dir, filename)
             with gzip.open(filename, 'rb') as raw:
                 yield raw
@@ -61,6 +63,4 @@ class EnaDirectory(luigi.Task):
         files = ena().all_tpa_files()
         with self.output().writer() as writer:
             for handle in self.handles():
-                for entry in parse_with_mapping_files(handle, files):
-                    if entry.is_valid():
-                        writer.write(entry)
+                writer.write_all(parse_with_mapping_files(handle, files))
---
+++ luigi/tasks/ena/utils.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright [2009-2017] EMBL-European Bioinformatics Institute
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+
+from tasks.config import ena
+
+from tasks.utils.fetch import FetchTask
+
+
+def tpa_tasks():
+    """
+    Create a list of tasks to fetch TPA files.
+    """
+
+    tasks = []
+    conf = ena()
+    for database in ena().tpa_databases:
+        local_path = conf.input_tpa_file(database)
+        remote_path = conf.raw_tpa_url(database)
+        tasks.append(FetchTask(remote_path=remote_path, local_path=local_path))
+    return tasks
+
+
+def copy_ncr_task(filename):
+    """
+    This will create a task to fetch either an NCR file or a directory of NCR
+    files.
+    """
+
+    conf = ena()
+    local_dir = os.path.filename(filename)
+    local_dir = conf.input_ncr_file(local_dir)
+    return FetchTask(remote_path=filename, local_path=local_dir)
