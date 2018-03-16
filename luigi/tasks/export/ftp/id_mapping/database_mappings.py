a34c74cf3bca7bec6e074e53039766c8b95f7cb4
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-03-16 15:27:58 +0000

    Test splitting by database
    This adds some tests and corrects issues with splitting the database
    mappings by ids.

--- luigi/tasks/export/ftp/id_mapping/database_mappings.py
+++ luigi/tasks/export/ftp/id_mapping/database_mappings.py
@@ -22,34 +22,9 @@ import luigi
 
 from tasks.config import export
 
+from rnacentral.export.ftp.id_mapping import split_by_database
+
 from .id_mapping import IdMapping
-
-
-@attr.s()
-class DatabaseWriter(object):
-    handle = attr.ib(validator=is_a(file))
-    writer = attr.ib()
-
-    @classmethod
-    def from_name(cls, name):
-        handle = open(export().database_mappings(name + '.tsv'))
-        return cls(
-            handle=handle,
-            writer=csv.writer(handle, delimiter='\t')
-        )
-
-    def write(self, entry):
-        self.writer.writerow(entry)
-
-
-class SplitWriter(object):
-    ena = attr.ib(validator=is_a(DatabaseWriter))
-    rfam = attr.ib(validator=is_a(DatabaseWriter))
-
-    def split(self, entries):
-        for entry in entries:
-            writer = getattr(self, entry[1].lower())
-            writer.write(entry)
 
 
 class DatabaseSpecificMappings(luigi.Task):
@@ -61,16 +36,5 @@ class DatabaseSpecificMappings(luigi.Task):
         return luigi.LocalTarget(export().database_mappings('ena.tsv'))
 
     def run(self):
-        handles = {}
-        with open(IdMapping().output().fn, 'rb') as raw:
-            reader = csv.reader(raw, delimiter='\t')
-            for row in reader:
-                db_name = row[1].lower()
-                if db_name not in handles:
-                    filename = export().database_mappings(db_name + '.tsv')
-                    handle = open(filename)
-                    handles[db_name] = (handle, csv.writer(handle))
-                handles[db_name][1].writerow(row)
-
-        for (handle, _) in handles.values():
-            handle.close()
+        with self.requires().output().open('r') as raw:
+            split_by_database(raw, export().database_mappings())
