492a18a5cfc16c0d1772c94d3a3b5842f4ac524d
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-03-21 11:49:13 +0000

    Ensure we fetch all XREF data

--- luigi/tasks/config.py
+++ luigi/tasks/config.py
@@ -215,7 +215,7 @@ class ena(luigi.Config):
             'tmRNA-Website',
         )
     )
-    tpa_url = 'https://www.ebi.ac.uk/ena/data/xref/search?source={db}&expanded=true'
+    tpa_url = 'https://www.ebi.ac.uk/ena/data/xref/search?source={db}&expanded=true&limit=0'
 
     def raw_ncr_files(self):
         if not os.path.exists(self.base):
