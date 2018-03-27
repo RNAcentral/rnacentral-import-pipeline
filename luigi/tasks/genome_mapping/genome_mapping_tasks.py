632a8d458d56019eae046eb2a13656a3df58cc1f
Anton Petrov <apetrov@ebi.ac.uk>
2018-03-27 09:55:15 +0100

    Clean up and refactor genome mapping tasks

--- luigi/tasks/genome_mapping/genome_mapping_tasks.py
+++ luigi/tasks/genome_mapping/genome_mapping_tasks.py
@@ -29,18 +29,21 @@ from rnacentral.genome_mapping import genome_mapping as gm
 
 
 class GetFasta(FastaExportBase):
-    taxid = luigi.IntParameter(default=9606)
+    """
+    Export RNAcentral sequences for a particular species to a FASTA file.
+    """
+    taxid = luigi.IntParameter(default=4932)
 
     def output(self):
         return luigi.LocalTarget(genome_mapping().rnacentral_fasta('%i.fa' % self.taxid))
 
     def records(self):
-        return gm.export_rnacentral_fasta2(db(), taxid=self.taxid)
+        return gm.export_rnacentral_fasta(db(), taxid=self.taxid)
 
 
 class CleanSplitFasta(luigi.Task):
-    taxid = luigi.IntParameter(default=9606)
-    chunks = luigi.IntParameter(default=100)
+    taxid = luigi.IntParameter(default=4932)
+    num_chunks = luigi.IntParameter(default=100)
     min_length = luigi.IntParameter(default=20)
     max_length = luigi.IntParameter(default=100000)
 
@@ -48,26 +51,38 @@ class CleanSplitFasta(luigi.Task):
         return GetFasta(taxid=self.taxid)
 
     def output(self):
-        for i in xrange(1, self.chunks):
+        for i in xrange(1, self.num_chunks):
             chunk_fasta = os.path.join(genome_mapping().chunks(self.taxid),
                                        '{taxid}-clean.part_{id}.fasta'.format(
                                         taxid=self.taxid, id='%03d' % i))
             yield luigi.LocalTarget(chunk_fasta)
 
     def run(self):
-        fasta = self.input().path
-        out_dir = genome_mapping().chunks(self.taxid)
-        gm.clean_split(min_length=self.min_length, max_length=self.max_length, fasta=fasta, chunks=self.chunks, out_dir=out_dir, taxid=self.taxid)
+        cmd = ('seqkit seq --min-len {min_length} --max-len {max_length} '
+                   '{fasta} > {taxid}-clean.fasta && '
+               'seqkit split --quiet -f -p {chunks} --out-dir {out_dir} '
+                   '{taxid}-clean.fasta && '
+               'rm {taxid}-clean.fasta').format(
+               max_length=self.max_length,
+               min_length=self.min_length,
+               fasta=self.input().path,
+               chunks=self.num_chunks,
+               out_dir=genome_mapping().chunks(self.taxid),
+               taxid=self.taxid)
+        status = subprocess.call(cmd, shell=True)
+        if status != 0:
+            raise ValueError('Failed to run seqkit: %s' % cmd)
 
 
-class GetChromosome(luigi.Task):
-    taxid = luigi.IntParameter(default=9606)
+class GetChromosomes(luigi.Task):
+    taxid = luigi.IntParameter(default=4932)
 
     def output(self):
         genomes = {
-            9606: 'GCA_000001405.25',
+            4932: 'GCA_000001405.25',
             10090: 'GCA_000001635.7',
             10116: 'GCA_000001895.4',
+            4932: 'GCA_000146045.2',
         }
         chromosomes = genome_mapping().genomes(genomes[self.taxid])
         for filename in iglob(os.path.join(chromosomes, '*.fa')):
@@ -78,13 +93,11 @@ class BlatJob(luigi.Task):
     """
     Run blat to map a fasta file with RNAcentral sequences to a chromosome.
     """
-    taxid = luigi.IntParameter(default=9606)
+    taxid = luigi.IntParameter(default=4932)
     fasta_input = luigi.Parameter()
     chromosome = luigi.Parameter()
 
     def run(self):
-        """
-        """
         genome_path, _ = os.path.split(self.chromosome)
         cmd = ('blat -ooc={genome_path}/11.ooc -noHead -q=rna -stepSize=5 '
                '-repMatch=2253 -minScore=0 -minIdentity=95 '
@@ -98,12 +111,11 @@ class BlatJob(luigi.Task):
             raise ValueError('Failed to run blat: %s' % cmd)
 
     def output(self):
-        """
-        """
         _, fasta_name = os.path.split(self.fasta_input)
         _, chromosome_name = os.path.split(self.chromosome)
         psl_output = genome_mapping().blat_output(str(self.taxid))
-        return luigi.LocalTarget(os.path.join(psl_output, '%s-%s.psl' % (chromosome_name, fasta_name)))
+        psl_filename = '%s-%s.psl' % (chromosome_name, fasta_name)
+        return luigi.LocalTarget(os.path.join(psl_output, psl_filename))
 
 
 class ParsePslOutput(luigi.Task):
@@ -123,7 +135,7 @@ class ParsePslOutput(luigi.Task):
         }
 
     def run(self):
-        cmd = 'source scripts/psl2tsv.sh {blat_output}'.format(blat_output=self.get_blat_output())
+        cmd = 'source scripts/psl2tsv.sh %s' % self.get_blat_output()
         status = subprocess.call(cmd, shell=True)
         if status != 0:
             raise ValueError('Failed to run psl2tsv: %s' % cmd)
