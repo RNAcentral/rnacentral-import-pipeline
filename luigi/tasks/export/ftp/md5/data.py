cd15299c67bedad67f18d55a9a0b71475efbb9c7
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-03-28 14:15:58 +0100

    Whitespace issues

--- luigi/tasks/export/ftp/md5/data.py
+++ luigi/tasks/export/ftp/md5/data.py
@@ -29,6 +29,6 @@ class Md5Data(luigi.Task):
             format=luigi.format.Gzip,
         )
 
-   def run(self):
+    def run(self):
         with self.output().open('w') as raw:
             md5.known(db(), raw)
