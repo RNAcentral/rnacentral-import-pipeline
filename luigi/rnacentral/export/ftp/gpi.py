40ba6d5811aed802d8200295032cb99aafa8324a
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-04-10 16:25:00 +0100

    Start outlining GPI export
    None of this actually works but covers more or less how I am thinking of
    the GPI export working.

---
+++ luigi/rnacentral/export/ftp/gpi.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright [2009-2017] EMBL-European Bioinformatics Institute
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import csv
+
+import attr
+from attr.validators import instance_of as is_a
+
+from rnacentral.psql import PsqlWrapper
+
+QUERY = """
+"""
+
+PRECUSOR_MAPPING_QUERY = """
+SELECT
+    t1.upi as precursor,
+    t4.upi as mature,
+    t1.taxid
+FROM xref precursor_xref
+JOIN rnc_accessions precursor_acc
+    t2 ON precursor_xref.ac = precursor_acc.accession
+    AND precursor_acc.feature_name = 'precursor_RNA'
+JOIN rnc_accessions mature_acc
+ON
+    mature_acc.external_id = precursor_acc.external_id
+    AND mature_acc.feature_name != 'precursor_RNA'
+    AND mature_xref.accession != precursor_acc.accession
+JOIN xref mature_xref
+ON
+    mature_xref.ac = mature_acc.accession
+    and mature_xref.dbid = precursor_xref.dbid
+    AND precursor_xref.upi != mature_xref.upi
+    AND precursor_xref.taxid = mature_xref.taxid
+WHERE
+    AND precursor_xref.dbid = 4
+    AND precursor_xref.deleted = 'N'
+    and mature_xref.deleted = 'N'
+"""
+
+FIELDS = [
+    'database',
+    'DB_Object_ID',
+    'DB_Object_Symbol',
+    'DB_Object_Name',
+    'DB_Object_Synonym',
+    'DB_Object_Type',
+    'Taxon',
+    'Parent_Object_ID',
+    'DB_Xref',
+    'Gene_Product_Properties',
+]
+
+
+class GpiEntry(object):
+    upi = attr.ib(validator=is_a(basestring))
+    taxid = attr.ib(validator=is_a(int))
+    description = attr.ib(validator=is_a(basestring))
+    so_term = attr.ib(validator=is_a(basestring))
+
+    precursor_rnas = attr.ib(validator=is_a(list), default=[])
+
+    @classmethod
+    def build(cls, result):
+        return cls(
+            upi=result['upi'],
+            taxid=result['taxid'],
+            description=result['description'],
+            so_term=result['so_term'],
+        )
+
+    def rna_id(self):
+        return '{upi}_{taxid}'.format(upi=self.upi, taxid=self.taxid)
+
+    def precursor_info(self):
+        return 'precursor_rna={upis}'.format(
+            upis=','.join(self.precursor_rnas),
+        )
+
+    def as_dict(self):
+        return {
+            'database': 'RNAcentral',
+            'DB_Object_ID': self.rna_id,
+            'DB_Object_Symbol': '',
+            'DB_Object_Name': self.description,
+            'DB_Object_Synonym': '',
+            'DB_Object_Type': self.so_term,
+            'Taxon': 'taxon:%s' % self.taxid,
+            'Parent_Object_ID': '',
+            'DB_Xref': '',
+            'Gene_Product_Properties': self.precursor_info(),
+        }
+
+
+def mirna_precursor_mapping(db):
+    pass
+
+
+def entries(db):
+    psql = PsqlWrapper(db)
+    for result in psql.copy_to_iterable(QUERY):
+        entry = GpiEntry.build(result)
+        yield entry.as_dict()
+
+
+def export(db, handle):
+    handle.write('!gpi-version: 1.2\n')
+    writer = csv.DictWriter(handle, FIELDS)
+    writer.writerows(entries(db))
