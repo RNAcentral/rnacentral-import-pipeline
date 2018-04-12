57218990e38ab50c7af2de8544959931dfd5c6f6
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-04-12 11:40:44 +0100

    Add code to fetch ontology information from OLS
    The OLS (https://www.ebi.ac.uk/ols/index) is an EBI service to provide
    information about ontologies. Seems useful. This adds some logic to
    lookup ontology data from them instead of from several ontologies.
    Hopefully we are fetching a small subset of the data so this will be
    fast enough.

---
+++ luigi/ontologies/helpers.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright [2009-2017] EMBL-European Bioinformatics Institute
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import urllib
+
+import requests
+from functools32 import lru_cache
+
+from . import data
+
+INFO_URL = 'https://www.ebi.ac.uk/ols/api/ontologies/{id}'
+TERM_URL = INFO_URL + '/terms/{iri}'
+
+
+def as_iri(url):
+    iri = url
+    for _ in xrange(2):
+        iri = [('', iri)]
+        iri = urllib.urlencode(iri)
+        iri = iri[1:]  # Strip off leading '='
+
+    return iri
+
+
+@lru_cache()
+def term(term):
+    ontology, rest = term.split(':')
+    response = requests.get(INFO_URL.format(id=ontology))
+    response.raise_for_status()
+    info = response.json()
+    base = info['config']['baseUris'][0]
+    url = base + rest
+    response = requests.get(TERM_URL.format(id=ontology, iri=as_iri(url)))
+    response.raise_for_status()
+    term_info = response.json()
+
+    return data.Term(
+        ontology=ontology,
+        ontology_id=term,
+        name=term_info['label'],
+        definition=' '.join(term_info['description']),
+        synonyms=term_info.get('synonyms', []),
+    )
