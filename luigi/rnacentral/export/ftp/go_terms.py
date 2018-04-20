72217d0e5948e2f07692a2103a5696af925f3277
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-04-20 16:22:14 +0100

    Update query and compress
    The output should be compressed. Additionally, I have updated the query
    to only export active sequences. We also need to prepend 'Rfam:' to the
    family name as part of the export.

--- luigi/rnacentral/export/ftp/go_terms.py
+++ luigi/rnacentral/export/ftp/go_terms.py
@@ -19,21 +19,15 @@ from rnacentral.psql import PsqlWrapper
 QUERY = """
 select
     pre.id,
-    go_terms.go_term_id,
-    string_agg(hits.rfam_model_id, '|')
+    go_terms.ontology_term_id,
+    string_agg('Rfam:' || hits.rfam_model_id, '|')
 from rnc_rna_precomputed pre
 join rfam_model_hits hits on hits.upi = pre.upi
 join rfam_go_terms go_terms on hits.rfam_model_id = go_terms.rfam_model_id
 where
-    exists(
-        select 1
-        from qa_status qa
-        where
-            qa.upi = pre.upi
-            and qa.taxid = pre.taxid
-            and qa.has_issue = false
-    )
-group by pre.id, go_terms.go_term_id
+    exists(select 1 from qa_status qa where qa.upi = pre.upi and qa.taxid = pre.taxid and qa.has_issue = false)
+    and exists(select 1 from xref where xref.upi = pre.upi and xref.taxid = pre.taxid and xref.deleted = 'N')
+group by pre.id, go_terms.ontology_term_id
 """
 
 
