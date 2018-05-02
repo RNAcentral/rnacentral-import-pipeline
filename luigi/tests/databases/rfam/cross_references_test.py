247624b8b71d4cab6c6e6ee0f9ce88075a035454
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-05-02 16:29:35 +0100

    Let database link extract ontology data
    This adds some functions and tests to see that we can now parse and
    extract data for ontology terms.

--- luigi/tests/databases/rfam/cross_references_test.py
+++ luigi/tests/databases/rfam/cross_references_test.py
@@ -14,10 +14,14 @@ limitations under the License.
 """
 
 import tempfile
+from StringIO import StringIO
 
 import pytest
 
 import databases.rfam.cross_references as cr
+
+from ontologies.data import Term
+from ontologies import helpers as ont
 
 
 @pytest.fixture
@@ -48,3 +52,40 @@ def test_correctly_parses_other(data):
         'Mus_musculus300888;',
         None,
     )
+
+
+def test_can_fetch_ontology_data(data):
+    assert data[0].ontology_term() ==  Term(
+        'SO',
+        'SO:0000378',
+        'DsrA_RNA',
+        ('DsrA RNA regulates both transcription, by overcoming '
+         'transcriptional silencing by the nucleoid-associated H-NS protein, '
+         'and translation, by promoting efficient translation of the stress '
+         'sigma factor, RpoS. These two activities of DsrA can be separated '
+         'by mutation: the first of three stem-loops of the 85 nucleotide RNA '
+         'is necessary for RpoS translation but not for anti-H-NS action, '
+         'while the second stem-loop is essential for antisilencing and less '
+         'critical for RpoS translation. The third stem-loop, which behaves '
+         'as a transcription terminator, can be substituted by the trp '
+         'transcription terminator without loss of either DsrA function. The '
+         'sequence of the first stem-loop of DsrA is complementary with the '
+         'upstream leader portion of RpoS messenger RNA, suggesting that '
+         'pairing of DsrA with the RpoS message might be important for '
+         'translational regulation.'),
+        ['DsrA RNA']
+    )
+
+
+def test_can_extract_all_ontology_terms():
+    with open('data/rfam/database_link.tsv', 'r') as raw:
+        sample = StringIO()
+        for line in raw.readlines()[:10]:
+            sample.write(line)
+        sample.seek(0)
+        assert list(cr.ontology_terms(sample)) == [
+            ont.term('SO:0000378'),
+            ont.term('SO:0000593'),
+            ont.term('GO:0006396'),
+            ont.term('GO:0005730'),
+        ]
