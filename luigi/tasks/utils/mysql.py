f2f97da1b26767890e98788f5c16dbc889f66084
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-05-04 15:07:44 +0100

    Use MysqlQueryTask to get family data

--- luigi/tasks/utils/mysql.py
+++ luigi/tasks/utils/mysql.py
@@ -13,9 +13,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import urlparse
-
 import luigi
+
+from rnacentral.mysql import MysqlWrapper
 
 from tasks.utils.files import atomic_output
 
