209523f3a2f0949ba3130241159344a01011eea6
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-06-14 12:18:23 +0100

    Fix tsv2fasta.py
    This now correctly runs

--- bin/tsv2fasta.py
+++ bin/tsv2fasta.py
@@ -10,12 +10,16 @@ from Bio.SeqRecord import SeqRecord
 
 
 def sequences(handle):
-    for row in csv.reader(tsv_file, sep='\t'):
-        yield SeqRecord(Seq(row[1]), id=row[0])
+    for row in csv.reader(handle, delimiter='\t'):
+        yield SeqRecord(Seq(row[1]), id=row[0], description='')
 
 
 @click.command()
 @click.argument('tsv_file', type=click.File('rb'))
-@click.argument('output', type=click.File('wb'))
-def cli(tsv_file, output):
+@click.argument('output', default='-', type=click.File('wb'))
+def cli(tsv_file, output=None):
     SeqIO.write(sequences(tsv_file), output, "fasta")
+
+
+if __name__ == '__main__':
+    cli()  # pylint: disable=no-value-for-parameter
