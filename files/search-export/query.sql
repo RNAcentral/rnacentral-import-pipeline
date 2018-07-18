0f201be60372852d2355fa6b7f3d7cd53644c666
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-07-18 08:58:04 +0100

    Fix QA status part of the query

--- files/search-export/query.sql
+++ files/search-export/query.sql
@@ -39,7 +39,7 @@ SELECT
             'possible_contamination', bool_or(qa.possible_contamination),
             'incomplete_sequence', bool_or(qa.incomplete_sequence),
             'missing_rfam_match', bool_or(qa.missing_rfam_match),
-        )),
+        ),
         'tax_strings', array_agg(acc.classification),
         'functions', array_agg(acc.function),
         'genes', array_agg(acc.gene),
