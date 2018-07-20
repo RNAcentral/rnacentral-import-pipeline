92bb936ddd150ff61a69196694e7d67bdb75a111
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-07-20 17:25:34 +0100

    More work on creating BED using the new setup
    Deleted the old unused test, added a new blank one and moved code to use
    the new data structures.

--- rnacentral_pipeline/rnacentral/ftp_export/coordinates/bed.py
+++ rnacentral_pipeline/rnacentral/ftp_export/coordinates/bed.py
@@ -14,69 +14,121 @@ limitations under the License.
 """
 
 import csv
-import json
+import operator as op
+import itertools as it
+
+import attr
+from attr.validators import instance_of as is_a
+
+from . import data as coord
 
 
-def format_chromosome_name(chromosome, region):
+@attr.s(frozen=True)
+class BedBlock(object):
+    start = attr.ib(validator=is_a(int))
+    stop = attr.ib(validator=is_a(int))
+
+    @classmethod
+    def from_endpoint(cls, endpoint):
+        return cls(
+            start=endpoint.start,
+            stop=endpoint.stop,
+        )
+
+    @property
+    def size(self):
+        start = self.start - 1
+        return (self.stop - start) or 1
+
+
+@attr.s(slots=True, frozen=True)
+class BedEntry(object):
+    chromosome = attr.ib(validator=is_a(basestring))
+    rna_id = attr.ib(validator=is_a(basestring))
+    blocks = attr.ib(validator=is_a(list))
+    strand = attr.ib(validator=is_a(int))
+    rna_type = attr.ib(validator=is_a(basestring))
+    databases = attr.ib(validator=is_a(basestring))
+    coord_system = attr.ib(validator=is_a(basestring))
+    score = attr.ib(default=0, validator=is_a(int))
+
+    @classmethod
+    def from_region(cls, region):
+        return cls(
+            chromosome=region.chromosome,
+            rna_id=region.rna_id,
+            blocks=[BedBlock.from_endpoint(e) for e in region.endpoints],
+            strand=region.strand,
+            rna_type=region.rna_type,
+            databases=region.databases,
+            coord_system=region.coord_system,
+        )
+
+    def block_sizes(self):
+        return [b.size for b in self.blocks]
+
+    def block_starts(self):
+        return [0] + [b.start - self.start for b in self.blocks[1:]]
+
+    def bed_block_sizes(self):
+        return ','.join(self.block_sizes())
+
+    def bed_block_starts(self):
+        return ','.join(self.block_starts())
+
+    @property
+    def start(self):
+        return self.blocks[0].start
+
+    @property
+    def stop(self):
+        return self.blocks[-1].stop
+
+    def bed_strand(self):
+        if self.strand == 1:
+            return '+'
+        if self.strand == -1:
+            return '-'
+        raise ValueError('Unknown Strand')
+
+    @property
+    def bed_chromosome(self):
+        chromosome = self.chromosome
+        if self.coord_system == 'chromosome':
+            chromosome = 'chr' + chromosome
+        if chromosome in ['MT', 'chrMT']:
+            chromosome = 'chrM'
+        return chromosome
+
+    def writeable(self):
+        return [
+            self.bed_chromosome,
+            self.start,
+            self.stop,
+            self.rna_id,
+            self.score,
+            self.bed_strand,
+            self.start,
+            self.stop,
+            '63,125,151',
+            len(self.blocks),
+            self.bed_block_sizes(),
+            self.bed_block_starts(),
+            '.',
+            self.rna_type,
+            self.databases,
+        ]
+
+
+def located_sequences_as_bed(sequences):
     """
-    Format chromosome names according to UCSC style.
-    `region` is an array in the following format:
-    {
-        'name': '1',
-        'length': 1000000,
-        'coord_system': 'chromosome'
-    }
-    """
-    if region['coord_system'] == 'chromosome':
-        chromosome = 'chr' + chromosome
-    if chromosome in ['MT', 'chrMT']:
-        chromosome = 'chrM'
-    return chromosome
-
-
-def format_as_bed(region):
-    """
-    Format the region into an array that can be written to a BED file.
+    Transform the iterable of LocatedSequence into an iterable of BedEntry
+    objects.
     """
 
-    block_sizes = []
-    block_starts = []
-
-    exons = region['exons']
-    min_start = exons[0]['start']
-    max_stop = exons[0]['stop']
-    chromosome = format_chromosome_name(region['chromosome'], region)
-
-    for index, exon in enumerate(exons):
-        exon['start'] = exon['start'] - 1  # BED files are 0-based
-        size = exon['stop'] - exon['start'] or 1  # equals 1 if start == end
-        block_sizes.append(size)
-        if index == 0:
-            block_starts.append(0)
-        else:
-            block_starts.append(exon['start'] - min_start)
-
-    databases = '.'
-    if region['databases']:
-        databases = region['databases'].replace(' ', '')
-
-    return [
-        chromosome,
-        min_start,
-        max_stop,
-        region['rnacentral_id'],
-        0,
-        '+' if region['strand'] == 1 else '-',
-        min_start,
-        max_stop,
-        '63,125,151',
-        len(block_sizes),
-        ','.join(str(x) for x in block_sizes),
-        ','.join(str(x) for x in block_starts),
-        '.',
-        region['rna_type'],
-        databases,
-    ]
+    for sequence in sequences:
+        for region in sequence.regions:
+            yield BedEntry.from_region(region)
 
 
 def from_json(handle, out):
@@ -84,8 +136,8 @@ def from_json(handle, out):
     Transform raw coordinate data into bed format.
     """
 
+    data = coord.from_file(handle)
+    bed = located_sequences_as_bed(data)
+    bed = it.imap(op.methodcaller('writeable'), bed)
     writer = csv.writer(out, delimiter='\t')
-    for line in handle:
-        result = json.load(line)
-        bed_line = format_as_bed(result)
-        writer.writerow(bed_line)
+    writer.writerows(bed)
