ec99b9608e35c0bb39c6a33892c7f39b91525ff5
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-07-20 17:25:34 +0100

    More work on creating BED using the new setup
    Deleted the old unused test, added a new blank one and moved code to use
    the new data structures.

--- tests/rnacentral/ftp_export/coodinates/bed_test.py
+++ tests/rnacentral/ftp_export/coodinates/bed_test.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright [2009-2017] EMBL-European Bioinformatics Institute
+Copyright [2009-2018] EMBL-European Bioinformatics Institute
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -13,10 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from rnacentral_pipeline.rnacentral.ftp_export import bed
 
-def test_can_correctly_generate_simple_example():
-    assert bed.format_as_bed({
-        'region_id': '',
-    }) == [ ]
-
+def test_can_build_bed_from_region():
+    assert False
