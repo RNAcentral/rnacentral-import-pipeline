eca1556eb7b38e7bfff53d8c803380c126c8ba8b
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-08-06 14:02:22 +0100

    Improve loaded coordinate data
    This will now load the fraction identity as well as indicate if a
    location is from an expert-database or from an alignment. This is a
    requested feature for our GFF3 export so this will prove helpful.

--- rnacentral_pipeline/rnacentral/ftp_export/coordinates/data.py
+++ rnacentral_pipeline/rnacentral/ftp_export/coordinates/data.py
@@ -18,8 +18,9 @@ import operator as op
 import itertools as it
 
 import attr
-from attr.validators import instance_of as is_a
+from attr.validators import optional
 from attr.validators import in_ as one_of
+from attr.validators import instance_of as is_a
 
 
 @attr.s()
@@ -34,10 +35,11 @@ class ExonicSequence(object):
         exons = []
         if raw['known_coordinates']['region_id'] and \
                 raw['known_coordinates']['start'] is not None:
-            exons.append(Exon.build(raw['known_coordinates']))
+            exons.append(Exon.build('expert-database', raw['known_coordinates']))
+
         if raw['mapped_coordinates']['region_id'] and \
                 raw['mapped_coordinates']['start'] is not None:
-            exons.append(Exon.build(raw['mapped_coordinates']))
+            exons.append(Exon.build('alignment', raw['mapped_coordinates']))
 
         assert exons, "Could build any exons"
         return cls(
@@ -63,17 +65,41 @@ class LocatedSequence(object):
             exons.extend(exonic.exons)
 
         regions = []
-        key = op.attrgetter('region_id')
-        exon_key = op.attrgetter('chromosome', 'start', 'stop')
-        exons.sort(key=key)
+        grouping_key = op.attrgetter('region_id')
+        exons.sort(key=grouping_key)
         seen = set()
-        for index, (_, exons) in enumerate(it.groupby(exons, key)):
-            ordered = sorted(exons, key=exon_key)
-            region_id = '%s.%i' % (exonics[0].rna_id, index)
-            region = Region.from_exons(region_id, ordered)
+        for _, exons in it.groupby(exons, grouping_key):
+            ordered = sorted(exons, key=op.attrgetter('start', 'stop'))
+
+            sources = {e.source for e in ordered}
+            if not sources:
+                raise ValueError("No sources defined")
+            elif len(sources) == 1:
+                source = sources.pop()
+            elif sources == {'expert-database', 'alignment'}:
+                source = 'expert-database'
+            else:
+                raise ValueError("Unknown sources in: %s" % str(sources))
+
+            region = Region.from_exons(
+                exonics[0].rna_id,
+                source,
+                ordered
+            )
             if region not in seen:
                 regions.append(region)
                 seen.add(region)
+
+        def region_key(region):
+            try:
+                chromosome = int(region.chromosome)
+            except:
+                chromosome = region.chromosome
+            return (chromosome, region.start, region.stop)
+
+        regions.sort(key=region_key)
+        from pprint import pprint
+        pprint(regions)
 
         return cls(
             rna_id=exonics[0].rna_id,
@@ -85,14 +111,23 @@ class LocatedSequence(object):
 
 @attr.s(hash=True, slots=True, frozen=True)
 class Region(object):
-    region_id = attr.ib(validator=is_a(basestring), hash=False, cmp=False)
+    rna_id = attr.ib(validator=is_a(basestring))
     chromosome = attr.ib(validator=is_a(basestring))
     strand = attr.ib(validator=is_a(int))
     endpoints = attr.ib(validator=is_a(tuple))
-    coordinate_system = attr.ib(validator=one_of(['chromosome', 'scaffold']))
+    source = attr.ib(
+        validator=one_of(['expert-database', 'alignment']),
+        cmp=False,
+    )
+    # coordinate_system = attr.ib(validator=one_of(['chromosome', 'scaffold']))
+    identity = attr.ib(
+        validator=optional(is_a(float)),
+        default=None,
+        cmp=False,
+    )
 
     @classmethod
-    def from_exons(cls, region_id, exons):
+    def from_exons(cls, rna_id, source, exons):
         region_ids = set()
         chromosomes = set()
         strands = set()
@@ -108,11 +143,27 @@ class Region(object):
         assert len(chromosomes) == 1
         assert len(strands) == 1
 
+        identity = exons[0].identity
+        if source == 'expert-database':
+            identity = None
+
         return cls(
-            region_id=region_id,
+            rna_id=rna_id,
             chromosome=chromosomes.pop(),
             strand=strands.pop(),
-            endpoints=tuple(sorted(endpoints, key=op.attrgetter('start', 'stop')))
+            source=source,
+            endpoints=tuple(sorted(endpoints, key=op.attrgetter('start', 'stop'))),
+            identity=identity
+        )
+
+    @property
+    def region_id(self):
+        return '{rna_id}@{chromosome}/{start}-{stop}:{strand}'.format(
+            rna_id=self.rna_id,
+            chromosome=self.chromosome,
+            start=self.start,
+            stop=self.stop,
+            strand=self.string_strand(),
         )
 
     @property
@@ -130,8 +181,8 @@ class Region(object):
             return '-'
         raise ValueError("Unknown type of strand")
 
-    def is_chromosomal(self):
-        return self.coordinate_system == 'chromosome'
+    # def is_chromosomal(self):
+    #     return self.coordinate_system == 'chromosome'
 
 
 @attr.s()
@@ -141,10 +192,23 @@ class Exon(object):
     strand = attr.ib(validator=is_a(int), convert=int)
     start = attr.ib(validator=is_a(int))
     stop = attr.ib(validator=is_a(int))
+    source = attr.ib(validator=is_a(basestring))
+    identity = attr.ib(validator=optional(is_a(float)))
 
     @classmethod
-    def build(cls, raw):
-        return cls(**raw)
+    def build(cls, source, raw):
+        identity = None
+        if 'identity' in raw:
+            identity = float(raw['identity'])
+        return cls(
+            region_id=raw['region_id'],
+            chromosome=raw['chromosome'],
+            strand=raw['strand'],
+            start=raw['start'],
+            stop=raw['stop'],
+            source=source,
+            identity=identity
+        )
 
 
 @attr.s(hash=True, slots=True, frozen=True)
