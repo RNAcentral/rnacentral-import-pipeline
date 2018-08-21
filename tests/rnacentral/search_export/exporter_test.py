7ee217fffc45932ec3786bbf682de5aef78b311a
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-08-21 16:01:35 +0100

    Add another gene name test

--- tests/rnacentral/search_export/exporter_test.py
+++ tests/rnacentral/search_export/exporter_test.py
@@ -271,15 +271,18 @@ def test_correctly_gets_expert_db(upi, ans):
         'let-7g-5p',
         'let-7g',
         'let-7',
+    ]),
+    ('URS00001F1DA8_9606', [
+        'MIR126',
+        'hsa-miR-126-3p ',
+        'hsa-miR-126',
+        'miR-126',
     ])
 ])
 def test_correctly_assigns_mirbase_gene_using_product(upi, ans):
-    vals = []
-    for entry in ans:
-        vals.append({'attrib': {'name': 'gene'}, 'text': entry})
-    vals.sort()
-    data = sorted(load_and_get_additional(upi, "gene"))
-    assert data == vals
+    data = load_and_get_additional(upi, "gene")
+    val = sorted(d['text'] for d in data)
+    assert val == sorted(ans)
 
 
 @pytest.mark.skip()  # pylint: disable=E1101
