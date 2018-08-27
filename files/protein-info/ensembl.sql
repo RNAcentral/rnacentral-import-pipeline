a32fedc5bfa3140f7115ac5144ea7dd3b4a09e09
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-08-27 22:59:01 +0100

    First version of protein import
    This creates the first version of the code that will be used to import
    Ensembl protein information into RNAcentral. We want this in order to
    search by interacting proteins when we have TarBase data.

---
+++ files/protein-info/ensembl.sql
@@ -0,0 +1,13 @@
+select
+	gene.stable_id,
+	gene.description,
+	gene_xref.display_label,
+	external_synonym.`synonym`
+
+from gene
+join xref gene_xref on gene_xref.xref_id = gene.display_xref_id
+join `external_synonym` on external_synonym.xref_id = gene_xref.xref_id
+where
+	gene.biotype = 'protein_coding'
+	and gene.stable_id = 'ENSG00000198901'
+;
