450c63486d56458236107f0510a0654e07c138f6
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-08-28 14:19:27 +0100

    Switch how fields use ','
    Can't use OUTFILE as that requires a filename, thus we can't use the
    OUTFILE options. We have to just use tr then. Also, remove the
    constraint that was limiting things to a single gene.

--- files/protein-info/ensembl.sql
+++ files/protein-info/ensembl.sql
@@ -1,16 +1,11 @@
-select
-	gene.stable_id,
-	gene.description,
-	gene_xref.display_label,
-	external_synonym.`synonym`
-
-from gene
-join xref gene_xref on gene_xref.xref_id = gene.display_xref_id
-join `external_synonym` on external_synonym.xref_id = gene_xref.xref_id
-where
-	gene.biotype = 'protein_coding'
-	and gene.stable_id = 'ENSG00000198901'
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
+SELECT
+  gene.stable_id,
+  gene.description,
+  gene_xref.display_label,
+  external_synonym.`synonym`
+FROM gene
+JOIN xref gene_xref ON gene_xref.xref_id = gene.display_xref_id
+JOIN `external_synonym` ON external_synonym.xref_id = gene_xref.xref_id
+WHERE
+  gene.biotype = 'protein_coding'
 ;
