c6db01620b99e83c202a44ac2596cfcefc7657fd
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-08-29 16:05:49 +0100

    More improvements to tests
    This fixes some spurious failures.

--- tests/databases/ensembl/parser_test.py
+++ tests/databases/ensembl/parser_test.py
@@ -17,7 +17,7 @@ import pytest
 import attr
 
 from rnacentral_pipeline.databases.ensembl.parser import parse
-from rnacentral_pipeline.databases.data import Exon
+from rnacentral_pipeline.databases import data as dat
 
 
 def parse_with_family(filename):
@@ -26,8 +26,16 @@ def parse_with_family(filename):
         return list(parse(raw, family_file))
 
 
-def entry_for(entries, transcript_id):
-    return next(e for e in entries if e.accession == accession)
+
+def entries_for(entries, accession):
+    return [e for e in entries if e.accession == accession]
+
+
+def entry_for(entries, accession):
+    val = entries_for(entries, accession)
+    assert len(val) == 1
+    return val[0]
+
 
 
 @pytest.fixture(scope='module')
@@ -39,34 +47,43 @@ def test_it_sets_primary_id_to_versionless_transcript_id(human_12):
     assert entry_for(human_12, 'ENST00000516089.1').primary_id == \
         'ENST00000516089'
 
+
 def test_it_generates_correct_seq_version(human_12):
     assert entry_for(human_12, 'ENST00000516089.1').seq_version == '1'
+
 
 def test_sets_optional_id_to_gene_id(human_12):
     assert entry_for(human_12, 'ENST00000516089.1').optional_id == \
         "ENSG00000251898.1"
 
+
 def test_it_gets_gene_id_to_locus(human_12):
     assert entry_for(human_12, 'ENST00000516089.1').gene == 'SCARNA11'
 
+
 def test_it_gets_the_locus_tag(human_12):
     assert entry_for(human_12, 'ENST00000516089.1').locus_tag == 'SCARNA11'
+
 
 def test_it_sets_rna_type_to_snRNA(human_12):
     assert entry_for(human_12, 'ENST00000516089.1').rna_type == 'snoRNA'
     assert entry_for(human_12, 'ENST00000540226.1').rna_type == 'antisense_RNA'
 
+
 def test_it_sets_product_to_snaRNA(human_12):
     assert entry_for(human_12, "ENST00000516089.1").product == 'scaRNA'
     assert entry_for(human_12, "ENST00000516089.1").rna_type == 'snoRNA'
+
 
 def test_it_sets_accession_to_transcript_id(human_12):
     assert entry_for(human_12, 'ENST00000540868.1').accession == \
         'ENST00000540868.1'
 
+
 def test_it_does_not_create_entries_for_pseudogenes(human_12):
-    entries = {e.optional_id for e in self.data()}
+    entries = {e.optional_id for e in human_12}
     assert 'ENSG00000252079.1' not in entries
+
 
 def test_it_normalizes_lineage_to_standard_one(human_12):
     assert entry_for(human_12, 'ENST00000540868.1').lineage == (
@@ -75,28 +92,35 @@ def test_it_normalizes_lineage_to_standard_one(human_12):
         "Haplorrhini; Catarrhini; Hominidae; Homo; Homo sapiens"
     )
 
+
 def test_calls_lincRNA_lncRNA(human_12):
     assert entry_for(human_12, 'ENST00000538041.1').rna_type == 'lncRNA'
+
 
 def test_uses_gene_description_if_possible(human_12):
     assert entry_for(human_12, 'ENST00000538041.1').description == \
         "Homo sapiens long intergenic non-protein coding RNA 1486"
 
+
 def test_description_strips_source(human_12):
     assert entry_for(human_12, 'ENST00000516089.1').description == \
         "Homo sapiens small Cajal body-specific RNA 11"
+
 
 def test_generated_description_includes_locus(human_12):
     assert entry_for(human_12, 'ENST00000501075.2').description == \
         "Homo sapiens (human) antisense RNA RP5-940J5.6"
 
+
 def test_can_correct_rfam_name_to_type(human_12):
     assert entry_for(human_12, 'ENST00000620330.1').rna_type == 'SRP_RNA'
+
 
 def test_it_gets_simple_locations(human_12):
     assert entry_for(human_12, 'ENST00000495392.1').exons == [
         Exon(chromosome_name='12', assembly_id='GRCh38', primary_start=3211663, primary_end=3211917, complement=True)
     ]
+
 
 def test_can_get_joined_locations(human_12):
     assert entry_for(human_12, 'ENST00000635814.1').exons == [
@@ -109,6 +133,7 @@ def test_can_get_joined_locations(human_12):
         Exon(chromosome_name='12', assembly_id='GRCh38', primary_start=3298210, primary_end=3298262, complement=True),
     ]
 
+
 def test_it_gets_cross_references(human_12):
     assert entry_for(human_12, 'ENST00000504074.1').xref_data == {
         "Vega_transcript": ["OTTHUMT00000397330"],
@@ -120,87 +145,71 @@ def test_it_gets_cross_references(human_12):
         "RefSeq_ncRNA": ['NR_026823'],
     }
 
+
 def test_it_uses_correct_antisense_type(human_12):
     assert entry_for(human_12, 'ENST00000605233.2').rna_type == 'antisense_RNA'
 
+
 def test_it_does_not_import_suprressed_rfam_families(human_12):
-    assert not self.entries_for('ENST00000611210.1')
+    assert not entries_for(human_12, 'ENST00000611210.1')
+
 
 def test_it_builds_correct_entries(human_12):
     val = attr.asdict(entry_for(human_12, 'ENST00000620330.1'))
     del val['sequence']
-    assert val == {
-        'allele': None,
-        'anticodon': None,
-        'division': None,
-        'experiment': None,
-        'primary_id': 'ENST00000620330',
-        'accession': 'ENST00000620330.1',
-        'ncbi_tax_id': 9606,
-        'database': 'ENSEMBL',
-        'exons': [{
-            'chromosome_name': "12",
-            "primary_start": 3124777,
-            "primary_end": 3125063,
-            'assembly_id': 'GRCh38',
-            "complement": False
-        }],
-        'rna_type': 'SRP_RNA',
-        'url': 'http://www.ensembl.org/Homo_sapiens/Transcript/Summary?t=ENST00000620330.1',
-        'lineage': (
+    assert val == attr.asdict(dat.Entry(
+        primary_id='ENST00000620330',
+        accession='ENST00000620330.1',
+        ncbi_tax_id=9606,
+        database='ENSEMBL',
+        sequence='',
+        exons=[dat.Exon(
+            chromosome_name="12",
+            primary_start=3124777,
+            primary_end=3125063,
+            assembly_id='GRCh38',
+            complement=False
+        )],
+        rna_type='SRP_RNA',
+        url='http://www.ensembl.org/Homo_sapiens/Transcript/Summary?t=ENST00000620330.1',
+        seq_version='1',
+        lineage=(
             "Eukaryota; Metazoa; Chordata; Craniata; Vertebrata; "
             "Euteleostomi; Mammalia; Eutheria; Euarchontoglires; Primates; "
             "Haplorrhini; Catarrhini; Hominidae; Homo; Homo sapiens"
         ),
-        "chromosome": "12",
-        'parent_accession': '12.GRCh38',
-        'common_name': 'human',
-        'species': 'Homo sapiens',
-        'gene': 'Metazoa_SRP',
-        'gene_synonyms': [],
-        'locus_tag': 'Metazoa_SRP',
-        'optional_id': 'ENSG00000278469.1',
-        'function': None,
-        'inference': None,
-        'keywords': None,
-        'map': None,
-        'location_start': None,
-        'location_end': None,
-        'description': 'Homo sapiens Metazoan signal recognition particle RNA',
-        'note_data': {
+        chromosome="12",
+        parent_accession='12.GRCh38',
+        common_name='human',
+        species='Homo sapiens',
+        gene='Metazoa_SRP',
+        locus_tag='Metazoa_SRP',
+        optional_id='ENSG00000278469.1',
+        description='Homo sapiens Metazoan signal recognition particle RNA',
+        note_data={
             'transcript_id': ['ENST00000620330.1']
         },
-        'xref_data': {
+        xref_data={
             "UCSC": ["uc058jxg.1"],
             "RFAM_trans_name": ["Metazoa_SRP.190-201"],
         },
-        'product': None,
-        'project': None,
-        'references': [{
-            'authors': (
+        references=[dat.Reference(
+            authors=(
                 "Aken BL, Ayling S, Barrell D, Clarke L, Curwen V, Fairley "
                 "S, Fernandez Banet J, Billis K, Garci a Giro n C, Hourlier "
                 "T, Howe K, Kahari A, Kokocinski F, Martin FJ, Murphy DN, "
                 "Nag R, Ruffier M, Schuster M, Tang YA, Vogel JH, White "
                 "S, Zadissa A, Flicek P, Searle SM."
             ),
-            'location': "Database (Oxford). 2016 Jun 23",
-            'title': "The Ensembl gene annotation system",
-            'pmid': 27337980,
-            'doi': "10.1093/database/baw093",
-        }],
-        'mol_type': 'genomic DNA',
-        'pseudogene': 'N',
-        'is_composite': 'N',
-        'seq_version': '1',
-        'non_coding_id': None,
-        'old_locus_tag': None,
-        'operon': None,
-        'ordinal': None,
-        'organelle': None,
-        'standard_name': None,
-        'secondary_structure': {'dot_bracket': ''},
-    }
+            location="Database (Oxford). 2016 Jun 23",
+            title="The Ensembl gene annotation system",
+            pmid=27337980,
+            doi="10.1093/database/baw093",
+        )],
+        mol_type='genomic DNA',
+        pseudogene='N',
+        is_composite='N',
+    ))
 
 
 # @pytest.mark.slowtest
