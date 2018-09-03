10b8bfce0f870dbb637f0d9136c92e68cb4c4faa
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-09-03 09:40:36 +0100

    Left join on synonyms
    Not all genes have synonyms. In order to deal with this a LEFT JOIN is
    needed.

--- files/protein-info/ensembl.sql
+++ files/protein-info/ensembl.sql
@@ -5,7 +5,7 @@ SELECT
   external_synonym.`synonym`
 FROM gene
 JOIN xref gene_xref ON gene_xref.xref_id = gene.display_xref_id
-JOIN `external_synonym` ON external_synonym.xref_id = gene_xref.xref_id
+LEFT JOIN `external_synonym` ON external_synonym.xref_id = gene_xref.xref_id
 WHERE
   gene.biotype = 'protein_coding'
 ;
