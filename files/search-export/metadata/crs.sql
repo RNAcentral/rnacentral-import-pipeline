84557b0af383216aa908d1d910d9a753627f0639
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-09-19 15:06:20 +0100

    Try to export CRS ids with search export
    This attempts to update the search export to include CRS ids as well as
    a flag if a sequence has a CRS.

--- files/search-export/metadata/crs.sql
+++ files/search-export/metadata/crs.sql
@@ -2,9 +2,12 @@ COPY (
 SELECT
   json_build_object(
     'rna_id', features.upi || '_' || features.taxid,
-    'has_crs', true
+    'crs', json_build_object(
+      'crs_ids', array_agg(distinct (features.metadata->'crs_id')::text)
+    )
   )
 FROM rnc_sequence_features features
 WHERE
   features.feature_name = 'conserved_rna_structure'
+group by features.upi, features.taxid
 ) TO STDOUT
