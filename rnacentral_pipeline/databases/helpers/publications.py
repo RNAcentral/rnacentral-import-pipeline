441b7bd3d5bd3b2f3e7ff2bbaaa43490a0c27add
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-11-01 20:39:04 +0000

    More work on deferring reference import
    By changing the primary way I lookup references to create an IdReference
    I can defer looking them up until later in the pipeline. This adds some
    tests and logic for doing that. Additionally, it checks that we can
    correctly parse the XML metadata that EuropePMC provides. It does seem
    to differ in at least some cases from what can be fetched online.

--- rnacentral_pipeline/databases/helpers/publications.py
+++ rnacentral_pipeline/databases/helpers/publications.py
@@ -17,23 +17,18 @@ import re
 import csv
 import json
 import sqlite3
-from time import sleep
 from xml.etree import cElementTree as ET
 
 import attr
 import requests
+from retry import retry
 from functools32 import lru_cache
 
 from ..data import Reference
-
-URL = 'https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=EXT_ID:{pmid}+AND+SRC:MED&format=json'
-
-
-class UnknownPmid(Exception):
-    pass
+from ..data import IdReference
 
 
-class FailedPublicationId(Exception):
+class UnknownReference(Exception):
     pass
 
 
@@ -42,31 +37,24 @@ class TooManyPublications(Exception):
 
 
 @lru_cache()
-def summary(pmid):
-    for count in xrange(5):
-        response = requests.get(URL.format(pmid=pmid))
-        try:
-            response.raise_for_status()
-            break
-        except requests.HTTPError:
-            if response.status_code == 500:
-                sleep(0.1 * (count + 1))
-                continue
-        else:
-            raise FailedPublicationId(pmid)
+@retry(requests.HTTPError, tries=5, delay=1)
+def summary(id_reference):
+    response = requests.get(id_reference.external_url())
+    response.raise_for_status()
 
     data = response.json()
     assert data, "Somehow got no data"
     if data['hitCount'] == 0:
-        raise UnknownPmid(pmid)
+        raise UnknownReference(id_reference)
 
-    if data['hitCount'] == 1:
-        return data['resultList']['result'][0]
+    if data['hitCount'] > 1:
+        raise TooManyPublications(id_reference)
 
-    raise TooManyPublications(pmid)
+    return data['resultList']['result'][0]
 
 
 def pretty_location(data):
+    print(data)
     issue = data.get('issue', '')
     if issue:
         issue = ('(%s)' % issue)
@@ -86,63 +74,107 @@ def pretty_location(data):
     return location
 
 
-def reference(pmid):
-    data = summary(pmid)
-    title = re.sub(r'\.$', '', data['title'])
+def reference(ref_id):
+    return IdReference.build(ref_id)
+
+
+def clean_title(title):
+    return re.sub(r'\.$', '', title)
+
+
+def lookup_reference(id_reference):
+    data = summary(id_reference)
+    pmid = data.get('pmid', None)
+    if pmid:
+        pmid = int(pmid)
+
     return Reference(
         authors=data['authorString'],
         location=pretty_location(data),
-        title=title,
-        pmid=int(pmid),
+        title=clean_title(data['title']),
+        pmid=pmid,
         doi=data.get('doi', None),
     )
 
 
+def xml_text(tag, node, missing=None, fn=None):
+    found = node.find(tag)
+    if found is None or not found.text:
+        return missing
+    if fn:
+        return fn(found.text)
+    return found.text
+
+
 def node_to_reference(node):
-    pmid = node.find('./pmid')
-    doi = node.find('./DOI')
+    pmid = xml_text('pmid', node, fn=int)
+    doi = xml_text('DOI', node)
     if not pmid and not doi:
         return None
 
-    if pmid:
-        pmid = int(pmid.text)
+    authors = []
+    for author in node.findall('./AuthorList/Author'):
+        last = author.find('LastName').text
+        initials = author.find('Initials').text
+        authors.append('%s %s' % (last, initials))
+    authors = ', '.join(authors) + '.'
 
-    if doi:
-        doi = doi.text
+    data = {
+        'journalTitle': xml_text('journalTitle', node, missing=''),
+        'issue': xml_text('issue', node, missing=''),
+        'journalVolumne': xml_text('journalVolume', node, missing=''),
+        'pageInfo': xml_text('pageInfo', node, missing=''),
+        'pubYear': xml_text('pubYear', node, missing=''),
+    }
 
-    data = {}
     return Reference(
         authors=authors,
         location=pretty_location(data),
-        title=node.find('./title').text,
+        title=xml_text('title', node, fn=clean_title),
         pmid=pmid,
         doi=doi,
     )
 
 
-def index_xml(output, xml_files):
-    conn = sqlite3.connect(output)
-    conn.execute('CREATE TABLE publications (id int, data text)')
+def parse_xmls(xml_files):
     for xml_file in xml_files:
         tree = ET.parse(xml_file)
         root = tree.getroot()
         for node in root.findall('./PMC_ARTICLE'):
-            data = node_to_reference(node)
-            if not data:
+            ref = node_to_reference(node)
+            if not ref:
                 continue
-            pmid = data['pmid']
-            data = json.dumps(attr.asdict(data))
-            conn.execute('INSERT INTO publications VALUES(?, ?)', pmid, data)
+            yield ref
+
+
+def index_xml(output, xml_files):
+    conn = sqlite3.connect(output)
+    tables = {'pmid': 'int', 'doi': 'text'}
+    for name, pid_type in tables.items():
+        stmt = 'CREATE TABLE {name}s (id {type} primary key, data text)'
+        conn.execute(stmt.format(name=name, type=pid_type))
+
+    for ref in parse_xmls(xml_files):
+        cursor = conn.cursor()
+        for table in tables.keys():
+            key = getattr(ref, table, None)
+            if not key:
+                continue
+            stmt = 'INSERT INTO %ss VALUES(?, ?)' % table
+            data = json.dumps(attr.asdict(ref))
+            cursor.execute(stmt, key, data)
 
 
 def lookup_refs(db, handle):
     conn = sqlite3.connect(db)
     reader = csv.reader(handle)
-    for (pmid, accession) in reader:
-        raw_ref = conn.execute(
-            "SELECT data from publications WHERE id=?",
-            pmid
-        )
+    for (ref_id, accession) in reader:
+        cursor = conn.cursor()
+        id_ref = IdReference.build(ref_id)
+        query = 'SELECT data from %ss WHERE id=?' % id_ref.namespace
+        raw_ref = cursor.execute(query, id_ref.external_id).fetchone()
+        if not raw_ref:
+            raise UnknownReference(id_ref)
         ref = Reference(**json.loads(raw_ref))
         yield accession, ref
 
@@ -156,6 +188,7 @@ def write_lookup(db, handle, output):
 def from_file(handle, output):
     reader = csv.reader(handle)
     writer = csv.writer(output)
-    for (pmid, accession) in reader:
-        ref = reference(int(pmid))
-        writer.writerows(ref.writeable(accession))
+    for (ref_id, accession) in reader:
+        id_ref = IdReference.build(ref_id)
+        complete = lookup_reference(id_ref)
+        writer.writerows(complete.writeable(accession))
--- tests/databases/helpers/publications_test.py
+++ tests/databases/helpers/publications_test.py
@@ -17,11 +17,12 @@ import attr
 import pytest
 
 from rnacentral_pipeline.databases.data import Reference
+from rnacentral_pipeline.databases.data import IdReference
 import rnacentral_pipeline.databases.helpers.publications as pub
 
 
 def test_can_fetch_publication():
-    assert pub.summary(28815543) == {
+    assert pub.summary(IdReference.build(28815543)) == {
         "id": "28815543",
         "source": "MED",
         "pmid": "28815543",
@@ -52,13 +53,17 @@ def test_can_fetch_publication():
 
 
 def test_complains_given_bad_pmid():
-    with pytest.raises(pub.UnknownPmid):
-        pub.summary(-1)
+    with pytest.raises(pub.UnknownReference):
+        pub.summary(IdReference.build(-1))
+
+
+def lookup(ref_id):
+    return attr.asdict(pub.lookup_reference(IdReference.build(ref_id)))
 
 
 def test_can_build_reference():
-    assert attr.asdict(pub.reference(27858507)) == attr.asdict(Reference(
-        authors=u"Lünse CE, Weinberg Z, Breaker RR.",
+    assert lookup(27858507) == attr.asdict(Reference(
+        authors=u"Lünse CE, Weinberg Z, Weinberg Z, Breaker RR.",
         location='RNA Biol 14(11):1499-1507 (2017)',
         title=(
             'Numerous small hammerhead ribozyme variants associated with '
@@ -70,13 +75,13 @@ def test_can_build_reference():
 
 
 def test_can_deal_with_unicode():
-    reference = pub.reference(27334534)
-    assert u'\xa0' not in reference.title
-    assert reference.md5() == 'a84bed065b6f62d0c096d8bd7547b578'
+    reference = lookup(27334534)
+    assert u'\xa0' not in reference['title']
+    # assert reference.md5() == 'a84bed065b6f62d0c096d8bd7547b578'
 
 
 def test_builds_correction_location():
-    assert attr.asdict(pub.reference(26184978)) == attr.asdict(Reference(
+    assert lookup(26184978) == attr.asdict(Reference(
         authors='Xu Z, Han Y, Liu J, Jiang F, Hu H, Wang Y, Liu Q, Gong Y, Li X.',
         location='Sci Rep 5:12276 (2015)',
         title=(
@@ -90,9 +95,9 @@ def test_builds_correction_location():
 
 
 def test_can_handle_missing_volume():
-    assert attr.asdict(pub.reference(27389411)) == attr.asdict(Reference(
+    assert lookup(27389411) == attr.asdict(Reference(
         authors='Carr G, Barrese V, Stott JB, Povstyan OV, Jepps TA, Figueiredo HB, Zheng D, Jamshidi Y, Greenwood IA.',
-        location='Cardiovasc Res (2016)',
+        location='Cardiovasc Res 112(2):581-589 (2016)',
         title='MicroRNA-153 targeting of KCNQ4 contributes to vascular dysfunction in hypertension',
         pmid=27389411,
         doi='10.1093/cvr/cvw177',
@@ -100,10 +105,72 @@ def test_can_handle_missing_volume():
 
 
 def test_it_can_find_if_duplicate_ext_ids():
-    assert attr.asdict(pub.reference(375006)) == attr.asdict(Reference(
+    assert lookup(375006) == attr.asdict(Reference(
         authors='Macino G, Tzagoloff A.',
         location='Mol Gen Genet 169(2):183-188 (1979)',
         title='Assembly of the mitochondrial membrane system: two separate genes coding for threonyl-tRNA in the mitochondrial DNA of Saccharomyces cerevisiae',
         pmid=375006,
         doi='10.1007/bf00271669',
     ))
+
+
+def test_can_lookup_by_doi():
+    assert lookup('doi:10.1007/bf00271669') == attr.asdict(Reference(
+        authors='Macino G, Tzagoloff A.',
+        location='Mol Gen Genet 169(2):183-188 (1979)',
+        title='Assembly of the mitochondrial membrane system: two separate genes coding for threonyl-tRNA in the mitochondrial DNA of Saccharomyces cerevisiae',
+        pmid=375006,
+        doi='10.1007/bf00271669',
+    ))
+
+
+@pytest.mark.parametrize('ref_id', [
+    26184978,
+    'pmid:26184978',
+    'PMID:26184978',
+    'doi:10.1038/srep12276',
+    'DOI:10.1038/srep12276',
+])
+def test_can_handle_several_reference_formats(ref_id):
+    assert lookup(ref_id) == attr.asdict(Reference(
+        authors='Xu Z, Han Y, Liu J, Jiang F, Hu H, Wang Y, Liu Q, Gong Y, Li X.',
+        location='Sci Rep 5:12276 (2015)',
+        title=(
+            'MiR-135b-5p and MiR-499a-3p Promote Cell '
+            'Proliferation and Migration in Atherosclerosis by Directly '
+            'Targeting MEF2C'
+        ),
+        pmid=26184978,
+        doi='10.1038/srep12276',
+    ))
+
+
+@pytest.mark.parametrize('raw_id,namespace,external_id', [
+    (26184978, 'pmid', '26184978'),
+    ('pmid:26184978', 'pmid', '26184978'),
+    ('PMID:26184978', 'pmid', '26184978'),
+    ('doi:10.1038/srep12276', 'doi', '10.1038/srep12276'),
+    ('DOI:10.1038/srep12276', 'doi', '10.1038/srep12276'),
+])
+def test_reference_builds_a_id_ref(raw_id, namespace, external_id):
+    assert pub.reference(raw_id) == IdReference(
+        namespace=namespace,
+        external_id=external_id,
+    )
+
+
+def test_can_parse_xml_data_correctly():
+    with open('data/publications/example.xml', 'r') as raw:
+        data = list(pub.parse_xmls([raw]))
+        assert len(data) == 1
+        assert attr.asdict(data[0]) == attr.asdict(Reference(
+            authors='Xu Z, Han Y, Liu J, Jiang F, Hu H, Wang Y, Liu Q, Gong Y, Li X.',
+            location='Sci Rep 5:12276 (2015)',
+            title=(
+                'MiR-135b-5p and MiR-499a-3p Promote Cell '
+                'Proliferation and Migration in Atherosclerosis by Directly '
+                'Targeting MEF2C'
+            ),
+            pmid=26184978,
+            doi='10.1038/srep12276',
+        ))
