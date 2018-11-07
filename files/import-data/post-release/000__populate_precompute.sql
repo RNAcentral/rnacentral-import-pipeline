32465c0f4f0ad6e266a56c1a1e48e5c1ce657923
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-11-07 16:26:19 +0000

    Rename post-load scripts
    It will be easier if I have a third level of priority to work with, so I
    added it. This also adds a populate precompute which allows us to put
    partial data into the table. We need this to ensure we have ids to
    create foreign keys to.

---
+++ files/import-data/post-release/000__populate_precompute.sql
@@ -0,0 +1,12 @@
+-- Populate rnc_rna_precomputed with partial data so we can create foreign keys
+-- into it later.
+INSERT INTO rnc_rna_precompute (id, upi, taxid, is_active) (
+SELECT
+  xref.upi || '_' || xref.taxid,
+  xref.upi,
+  xref.taxid,
+  true
+FROM xref
+WHERE
+  xref.deleted = 'N'
+) ON CONFLICT DO NOTHING;
