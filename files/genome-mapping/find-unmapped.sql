05815aa36416ab2d031ac4aefbff16bcada68214
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-11-13 13:54:16 +0000

    Outline genome mapping pipeline
    This is an untested version of what I think will be needed for genome
    mapping pipeline. It outlines the steps needed and the general scripts
    that will have to be added. This does not yet actually run it is just
    added to have something to test.

---
+++ files/genome-mapping/find-unmapped.sql
@@ -0,0 +1,16 @@
+COPY (
+  select
+    json_build_object(
+      'id', pre.id,
+      'sequence', COALESCE(rna.seq_short, rna.seq_long)
+    )
+    FROM rna
+    JOIN rnc_rna_precomputed pre on pre.upi = rna.upi
+    LEFT JOIN rnc_sequence_regions regions
+    ON
+      regions.urs_taxid = pre.id
+      and regions.assembly_id = :'assembly_id'
+    WHERE
+      pre.taxid is not null
+      and regions.id is null
+) TO STDOUT
