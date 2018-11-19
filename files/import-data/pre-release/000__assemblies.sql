d1a0c37b4f3401e55de350359702db570090614a
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-11-19 15:08:42 +0000

    Try to improve assembly loading
    We should delete the assemblies which will conflict and update the
    remaining ones each run.

---
+++ files/import-data/pre-release/000__assemblies.sql
@@ -0,0 +1,54 @@
+DELETE FROM ensembl_assemblies ensembl
+USING load_assemblies load
+WHERE
+  load.taxid = ensembl.taxid
+  and load.assembly_id != ensembl.assembly_id
+;
+
+INSERT INTO ensembl_assemblies (
+  assembly_id,
+  assembly_full_name,
+  gca_accession,
+  assembly_ucsc,
+  common_name,
+  taxid,
+  ensembl_url,
+  division,
+  blat_mapping,
+  example_chromosome,
+  example_end,
+  example_start,
+  subdomain
+) (
+SELECT
+  assembly_id,
+  assembly_full_name,
+  gca_accession,
+  assembly_ucsc,
+  common_name,
+  taxid,
+  ensembl_url,
+  division,
+  blat_mapping,
+  example_chromosome,
+  example_end,
+  example_start,
+  subdomain
+FROM load_assemblies
+) ON CONFLICT (assembly_id) DO UPDATE
+SET
+  assembly_full_name = EXCLUDED.assembly_full_name,
+  gca_accession = EXCLUDED.gca_accession,
+  assembly_ucsc = EXCLUDED.assembly_ucsc,
+  common_name = EXCLUDED.common_name,
+  taxid = EXCLUDED.taxid,
+  ensembl_url = EXCLUDED.ensembl_url,
+  division = EXCLUDED.division,
+  blat_mapping = EXCLUDED.blat_mapping,
+  example_chromosome = EXCLUDED.example_chromosome,
+  example_end = EXCLUDED.example_end,
+  example_start = EXCLUDED.example_start,
+  subdomain = EXCLUDED.subdomain
+;
+
+DROP TABLE load_assemblies;
