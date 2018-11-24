5b82735154c3b238d6464a429aae82fb6b62ee05
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-11-24 21:27:57 +0000

    Add files to load karyotype information

---
+++ files/import-data/post-release/001__karyotypes.sql
@@ -0,0 +1,12 @@
+INSERT INTO ensembl_karyotypes (
+    assembly_id,
+    karyotype
+) (
+select
+    assembly_id,
+    karyotype
+from load_karyotypes
+) ON CONFLICT (assembly_id) DO UPDATE
+SET
+    karyotype = excluded.karyotype
+;
