a5cb5dec168657a5c77040db878028acb3c741d1
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-12-11 20:59:42 +0000

    Drop the go annotation publications
    We don't display them anyway.

--- files/search-export/metadata/go_annotations.sql
+++ files/search-export/metadata/go_annotations.sql
@@ -7,17 +7,11 @@ json_build_object(
       'go_term_id', anno.ontology_term_id,
       'qualifier', anno.qualifier,
       'go_name', ont.name,
-      'assigned_by', anno.assigned_by,
-      'pubmed_ids', pubmed.ref_pubmed_id,
-      'dois', pubmed.doi
+      'assigned_by', anno.assigned_by
     )
   )
 )
 FROM go_term_annotations anno
 JOIN ontology_terms ont ON ont.ontology_term_id = anno.ontology_term_id
-LEFT JOIN go_term_publication_map go_map
-ON
-  go_map.go_term_annotation_id = anno.go_term_annotation_id
-LEFT JOIN ref_pubmed pubmed ON pubmed.ref_pubmed_id = go_map.ref_pubmed_id
 GROUP BY anno.rna_id
 ) TO STDOUT
