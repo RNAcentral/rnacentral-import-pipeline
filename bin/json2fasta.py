fd183b2bb6f986a164e78cd7d43858a276ead3a4
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-12-11 11:09:48 +0000

    Try to fix UTF-8 issues
    Maybe this helps? It is hard to test generally.

--- bin/json2fasta.py
+++ bin/json2fasta.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 
+# -*- coding: utf-8 -*-
+
+"""
+Copyright [2009-2018] EMBL-European Bioinformatics Institute
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import json
+import itertools as it
 
 import click
 
@@ -9,24 +25,34 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
 
-def sequences(handle):
-    for line in handle:
-        data = json.loads(line)
-        description = data.get('description', '')
-        if description:
-            description = description.encode('utf-8', 'ignore')
+def as_record(entry):
+    description = entry.get('description', '')
+    description = description.encode('ascii', 'ignore')
 
-        yield SeqRecord(
-            Seq(data['sequence']),
-            id=data['id'].encode('utf-8'),
-            description=description,
-        )
+    return SeqRecord(
+        Seq(entry['sequence']),
+        id=entry['id'],
+        description=description,
+    )
+
+
+def sequences(handle):
+    """
+    Parse each line and create a generator of SeqRecords to write.
+    """
+
+    data = it.imap(json.loads, handle)
+    return it.imap(as_record, data)
 
 
 @click.command()
 @click.argument('tsv_file', type=click.File('rb'))
-@click.argument('output', default='-', type=click.File('wb'))
+@click.argument('output', default='-', type=click.File('w'))
 def cli(tsv_file, output=None):
+    """
+    Turn a file with JSON lines into a FASTA file. Each line must be an object
+    with an id and sequence entry and an optional description entry.
+    """
     SeqIO.write(sequences(tsv_file), output, "fasta")
 
 
