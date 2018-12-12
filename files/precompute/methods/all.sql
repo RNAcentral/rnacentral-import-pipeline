f16a26283d44c7210847dcb9b7bf546fb5f5c959
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-12-12 09:09:00 +0000

    Correct logic for setting up all precompute
    The logic was a little wrong as the previous table was not removed and
    the indexing was incorrect.

--- files/precompute/methods/all.sql
+++ files/precompute/methods/all.sql
@@ -1,7 +1,9 @@
+DROP TABLE IF EXISTS :tablename;
+
 CREATE TABLE :tablename as
 select id, upi from rna;
 
-alter table upis_to_pr:tablename
+alter table :tablename
   add constraint fk_to_precompute__upi FOREIGN KEY (upi) REFERENCES rna(upi),
   add primary key (id)
 ;
