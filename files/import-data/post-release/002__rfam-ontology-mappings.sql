8c21494186b495a7f36564e9578051437f8e330f
Blake Sweeney <bsweeney@ebi.ac.uk>
2018-12-12 11:27:39 +0000

    Delete old GO annotations prior to loading
    This will prevent keeping outdated annotations around.

--- files/import-data/post-release/002__rfam-ontology-mappings.sql
+++ files/import-data/post-release/002__rfam-ontology-mappings.sql
@@ -1,15 +1,21 @@
+DELETE FROM rfam_go_terms rfam
+USING load_rfam_go_terms load
+WHERE
+  load.rfam_model_id = rfam.rfam_model_id
+;
+
 INSERT INTO rfam_go_terms (
-    ontology_term_id,
-    rfam_model_id
+  ontology_term_id,
+  rfam_model_id
 ) (
 SELECT
-    ontology_term_id,
-    rfam_model_id
+  ontology_term_id,
+  rfam_model_id
 FROM load_rfam_go_terms
 )
 ON CONFLICT (ontology_term_id, rfam_model_id) DO UPDATE SET
-    ontology_term_id = excluded.ontology_term_id,
-    rfam_model_id = excluded.rfam_model_id
+  ontology_term_id = excluded.ontology_term_id,
+  rfam_model_id = excluded.rfam_model_id
 ;
 
 DROP TABLE load_rfam_go_terms;
