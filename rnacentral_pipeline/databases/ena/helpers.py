08e7b6009a68b2459cdc427fb5221956911c245d
Blake Sweeney <bsweeney@ebi.ac.uk>
2019-02-13 11:53:12 +0000

    Remove some unneeded functions
    These always return None so we should not call them. Plus the data that
    they would produce if we indexed would not be interesting.

--- rnacentral_pipeline/databases/ena/helpers.py
+++ rnacentral_pipeline/databases/ena/helpers.py
@@ -224,10 +224,6 @@ def anticodon(record, feature):
     return raw_anti
 
 
-def map(_):
-    return None
-
-
 def keywords(record):
     keys = [k for k in record.annotations['keywords'] if k]
     if not keys:
@@ -237,10 +233,6 @@ def keywords(record):
 
 def parent_accession(record):
     return record.id.split('.', 1)[0]
-
-
-def ordinal(_):
-    return None
 
 
 def organelle(record):
@@ -274,10 +266,6 @@ def gene_synonyms(feature):
     for synonym in synonyms:
         result.extend(synonym.split('; '))
     return result
-
-
-def non_coding_id(_):
-    return None
 
 
 def taxid(record):
@@ -327,13 +315,6 @@ def lineage(record):
     if taxonomy:
         taxonomy.append(species(record))
         return '; '.join(taxonomy)
-    return None
-
-
-def division(record):
-    # try:
-    #     return embl.division(record)
-    # except UnknownTaxonId:
     return None
 
 
