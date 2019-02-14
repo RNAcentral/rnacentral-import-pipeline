71923d7e8ef22c2a95cc46b5b847d2e173cca018
Blake Sweeney <bsweeney@ebi.ac.uk>
2019-02-14 17:06:18 +0000

    Update to an existing sequence
    The sequence that was there before was inactive.

--- tests/rnacentral/precompute/qa/data_test.py
+++ tests/rnacentral/precompute/qa/data_test.py
@@ -77,7 +77,7 @@ def test_can_detect_problems_with_mismatched_rna_types(rna_id, rna_type, flag):
 
 
 @pytest.mark.parametrize('rna_id,rna_type,messages', [  # pylint: disable=no-member
-    ('URS000090395E_9606', 'rRNA', [
+    ('URS00003EA5AC_9606', 'rRNA', [
         (u'This human sequence matches a Bacteria Rfam model '
          u'(<a href="http://rfam.org/family/RF02541">LSU_rRNA_bacteria</a>). '
          u'<a href="/help/rfam-annotations">Learn more &rarr;</a>'),
