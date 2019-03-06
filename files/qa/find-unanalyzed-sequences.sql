6c218c19b5ad4541c062d38f35f7560bf12ec4be
Blake Sweeney <bsweeney@ebi.ac.uk>
2019-03-06 10:40:09 +0000

    Do not use version
    This isn't truly implemented yet.

--- files/qa/find-unanalyzed-sequences.sql
+++ files/qa/find-unanalyzed-sequences.sql
@@ -9,7 +9,7 @@ left join qa_analysis_status analyzed
 on
   analyzed.urs = rna.upi
   and analyzed.qa_source = :'program'
-  and analyzed.qa_source_version = :'version'
+  -- and analyzed.qa_source_version = :'version'
 where
   analyzed.urs is null
 ) TO STDOUT
