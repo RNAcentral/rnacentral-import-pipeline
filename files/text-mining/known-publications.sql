ac0bcf9ad5813627bd61c81290c871743c3b0a46
Blake Sweeney <bsweeney@ebi.ac.uk>
2019-03-07 14:37:18 +0000

    Update to fix and make it run

--- files/text-mining/known-publications.sql
+++ files/text-mining/known-publications.sql
@@ -1,6 +1,6 @@
 COPY (
-select 
-  refs.*
+select distinct
+  refs.pmid
 from xref 
 join rnc_accessions acc on acc.accession = xref.ac
 join rnc_reference_map rmap on rmap.accession = acc.accession
@@ -8,4 +8,4 @@ join rnc_references refs on refs.id = rmap.reference_id
 where
   xref.deleted = 'N'
   and refs.pmid is not null
-) TO STDOUT
+) TO STDOUT CSV HEADER
