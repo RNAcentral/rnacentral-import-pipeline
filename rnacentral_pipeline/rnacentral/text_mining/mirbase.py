83ed339b951ac254b464f32fc191e5660abb7a22
Blake Sweeney <bsweeney@ebi.ac.uk>
2019-03-08 10:01:10 +0000

    Extend the mirbase pattern
    This will also find gene names, not just mature miRNA's.

--- rnacentral_pipeline/rnacentral/text_mining/mirbase.py
+++ rnacentral_pipeline/rnacentral/text_mining/mirbase.py
@@ -16,7 +16,7 @@ limitations under the License.
 from . import patterns
 
 PATTERNS = [
-    r'\w{3}-mir-[0-9]+\w?([35]p)?',
+    r'\w{3}?-mir-[0-9]+\w?([35]p)?',
     r'\w{3}-let-7\w-[35]p',
     r'miRNA-\d+',
 ]
