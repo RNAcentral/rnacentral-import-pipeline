17470eb3efad6053b3e3dd3b556a15a03bbb81c7
Blake Sweeney <bsweeney@ebi.ac.uk>
2019-03-09 20:59:34 +0000

    Remove extra option

--- templates/query-and-split.sh
+++ templates/query-and-split.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-psql -v ON_ERROR_STOP=1 -v ${variables} -f "$query" "$PGDATABASE" > raw.json
+psql -v ON_ERROR_STOP=1 ${variables} -f "$query" "$PGDATABASE" > raw.json
 json2fasta.py raw.json rnacentral.fasta
 seqkit shuffle --two-pass rnacentral.fasta > shuffled.fasta
 seqkit split --two-pass --by-size ${chunk_size} --out-dir 'parts/' shuffled.fasta
