427c4eb66042bc19833808f81692c865c561d7f8
Blake Sweeney <bsweeney@ebi.ac.uk>
2019-03-27 14:58:53 +0000

    Improve the fetching of data
    This will now fetch the data for several cases which were not found previously.

--- tests/databases/data/references_test.py
+++ tests/databases/data/references_test.py
@@ -15,6 +15,7 @@ limitations under the License.
 
 import attr
 import pytest
+import requests
 
 from rnacentral_pipeline.databases import data
 
@@ -55,3 +56,35 @@ def test_can_build_id_reference_for_pmcid(pmc):
         namespace='pmcid',
         external_id='PMC4648659'
     ))
+
+
+@pytest.mark.parametrize('raw_data,title', [
+    ('PMC1172314', 'Books also Received.'),
+    ('PMC3131748', 'Abstracts - Invited Speakers.'),
+    ('PMC3131791', 'Abstracts - Poster Presentations.'),
+    ('PMID:30715521', 'LncBook: a curated knowledgebase of human long non-coding RNAs.'),
+    ('doi:10.1007/s11524-007-9234-y', 'Section I: Oral Sessions.'),
+    ('doi:10.1016/S1470-2045(16)30240-6', "Genome-wide association studies in oesophageal adenocarcinoma and Barrett's oesophagus: a large-scale meta-analysis."),
+    ('doi:10.1016/S1672-0229(04)02021-2', "Application of proteomics in the study of tumor metastasis."),
+    ('doi:10.1083/jcb.1851iti3', 'What do kidneys and embryonic fish skin have in common?'),
+    ('doi:10.1083/jcb.2111iti1', 'mTORC2 tips the balance in cell survival.'),
+    ('doi:10.1093/database/baw138', 'IRNdb: the database of immunologically relevant non-coding RNAs.'),
+    ('doi:10.1093/ofid/ofx163.867', 'The Expression of hsp-miRNA-200b-3p and -200c-3p in Human Cytomegalovirus-infected Formalin-Fixed, Paraffin-Embedded Tissues.'),
+    ('doi:10.1093/schbul/sby014.092', '23.2 NETRIN-1 RECEPTORS CONTROL MESOCORTICAL DOPAMINE CONNECTIVITY IN ADOLESCENCE.'),
+    ('doi:10.1097/01.WOX.0000411770.14047.89', '25 Role of Myeloid Derived Suppressor Cells in Asthma.'),
+    ('doi:10.1097/MD.0000000000002371', 'UMIB Summit 2015.'),
+    ('doi:10.1111/cas.12358', 'In This Issue.'),
+    ('doi:10.1111/cas.12385', 'In This Issue.'),
+    ('doi:10.1177/2050640615601611', 'UEG Week 2015 Oral Presentations.'),
+    ('doi:10.1371/journal.pbio.0020114', 'Exploring Small RNA Function.'),
+    ('doi:10.1534/g3.116.036848', 'Meeting Report: The Allied Genetics Conference 2016.'),
+    ('pmcid:PMC1480519', 'Fellowships, Grants, & Awards.'),
+    ('pmcid:PMC5064671', 'Abstracts - USICON 2016.'),
+    ('pmid:17254355', 'Retroviral activation of the mir-106a microRNA cistron in T lymphoma.'),
+    ('pmid:24531370', 'New insights into the promoterless transcription of DNA coligo templates by RNA polymerase III.'),
+])
+def test_can_query_for_expected_data(raw_data, title):
+    ref = data.IdReference.build(raw_data)
+    response = requests.get(ref.external_url())
+    assert response.json()['hitCount'] == 1
+    assert response.json()['resultList']['result'][0]['title'] == title
