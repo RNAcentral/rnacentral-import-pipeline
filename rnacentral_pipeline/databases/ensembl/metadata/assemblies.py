6d029b41c5aa83cf050a12b6ffe1be29e77cff90
Blake Sweeney <bsweeney@ebi.ac.uk>
2019-04-09 14:48:56 +0100

    More py27 fixes

--- rnacentral_pipeline/databases/ensembl/metadata/assemblies.py
+++ rnacentral_pipeline/databases/ensembl/metadata/assemblies.py
@@ -181,7 +181,7 @@ class AssemblyInfo(object):
 def fetch(connections, query_handle, example_locations):
     results = db.run_queries_across_databases(connections, query_handle)
     for (_, rows) in results:
-        raw = {r['meta_key']: r['meta_value'] for r in rows}
+        raw = {r['meta_key']: six.text_type(r['meta_value']) for r in rows}
         if raw['species.division'] == 'EnsemblBacteria':
             continue
         info = AssemblyInfo.build(raw, example_locations)
