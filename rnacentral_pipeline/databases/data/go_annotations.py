2294f65059700b51ef1717ec38dee8e52207641f
Blake Sweeney <bsweeney@ebi.ac.uk>
2019-04-11 12:40:32 +0100

    Use current argument
    convert= is depreciated.

--- rnacentral_pipeline/databases/data/go_annotations.py
+++ rnacentral_pipeline/databases/data/go_annotations.py
@@ -37,12 +37,12 @@ class GoTermAnnotation(object):
     RNAcentral sequences.
     """
 
-    rna_id = attr.ib(validator=is_a(six.text_type), convert=six.text_type)
-    qualifier = attr.ib(validator=is_a(six.text_type), convert=six.text_type)
-    term_id = attr.ib(validator=is_a(six.text_type), convert=six.text_type)
-    evidence_code = attr.ib(validator=is_a(six.text_type), convert=six.text_type)
+    rna_id = attr.ib(validator=is_a(six.text_type), converter=six.text_type)
+    qualifier = attr.ib(validator=is_a(six.text_type), converter=six.text_type)
+    term_id = attr.ib(validator=is_a(six.text_type), converter=six.text_type)
+    evidence_code = attr.ib(validator=is_a(six.text_type), converter=six.text_type)
     extensions = attr.ib()
-    assigned_by = attr.ib(validator=is_a(six.text_type), convert=six.text_type)
+    assigned_by = attr.ib(validator=is_a(six.text_type), converter=six.text_type)
     publications = attr.ib(validator=is_a(list))
 
     @property
