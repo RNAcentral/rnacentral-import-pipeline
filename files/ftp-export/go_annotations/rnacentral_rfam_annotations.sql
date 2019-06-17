ef143ccef2690cb2eeef5f5ea55d5544d96bf95a
Blake Sweeney <bsweeney@ebi.ac.uk>
2019-06-17 13:44:15 +0100

    Rfam models must be unique
    The model ids should be unique, so we make the query use distinct.

--- files/ftp-export/go_annotations/rnacentral_rfam_annotations.sql
+++ files/ftp-export/go_annotations/rnacentral_rfam_annotations.sql
@@ -5,7 +5,7 @@ select
     'upi', pre.upi,
     'taxid', pre.taxid,
     'ontology_term_id', go_terms.ontology_term_id,
-    'models', string_agg('Rfam:' || hits.rfam_model_id, '|')
+    'models', string_agg(distinct 'Rfam:' || hits.rfam_model_id, '|')
   )
 from rnc_rna_precomputed pre
 join rfam_model_hits hits on hits.upi = pre.upi
