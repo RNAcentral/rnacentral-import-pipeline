bc2f4a3ccb24bd3bb545baae88a8f4f2b2342d67
Blake Sweeney <bsweeney@ebi.ac.uk>
2019-06-19 15:34:36 +0100

    Try to simplify the Rfam pipeline
    This merges the sql scripts so things are easier. Also we can use a
    cleaner value 'rRNA' for tracking the rRNA sequences.

--- files/traveler/find-families.sql
+++ files/traveler/find-families.sql
@@ -1,3 +1,25 @@
+DROP TABLE IF EXISTS urs_with_one_rfam;
+
+CREATE TABLE urs_with_one_rfam AS
+SELECT 
+	t.upi,
+	t.rfam_hit_id,
+	models.*
+FROM (
+SELECT
+	hits.upi upi,
+	min(hits.rfam_hit_id) rfam_hit_id,
+	min(hits.rfam_model_id) rfam_model_id
+FROM rfam_model_hits hits
+GROUP BY hits.upi
+HAVING count(hits.rfam_hit_id) = 1
+) t
+JOIN rfam_models models ON t.rfam_model_id = models.rfam_model_id
+;
+
+CREATE INDEX ix_urs_with_one_rfam__upi ON urs_with_one_rfam(upi);
+CREATE INDEX ix_urs_with_one_rfam__rfam_hit_id ON urs_with_one_rfam(rfam_hit_id);
+
 COPY (
   SELECT 
     rfam_model_id 
