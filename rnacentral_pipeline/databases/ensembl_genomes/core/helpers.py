c9a986b7249439b4ebd019d0b30860013d2c8b95
Blake Sweeney <bsweeney@ebi.ac.uk>
2019-07-03 13:57:12 +0100

    Improve sequence version computation
    Seems that protists sometimes uses -1 to mean version 1. This fixes
    things so that we can deal with that.

--- rnacentral_pipeline/databases/ensembl_genomes/core/helpers.py
+++ rnacentral_pipeline/databases/ensembl_genomes/core/helpers.py
@@ -74,7 +74,10 @@ def rna_type(feature):
 
 
 def seq_version(feature):
-    return ensembl.seq_version(feature) or '1'
+    version = ensembl.seq_version(feature) or '1'
+    if '-' in version:
+        return version.split('-')[-1]
+    return version
 
 
 def description(gene, entry):
