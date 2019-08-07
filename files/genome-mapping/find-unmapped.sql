daf2c70f6353c55983c1d238c1307a2e36e751c4
Blake Sweeney <bsweeney@ebi.ac.uk>
2019-08-07 16:36:22 +0100

    Improve unmapped sequence chunking
    There were a couple issues with the previous code. First we did not
    filter by max/min size. We need to do this so we get good alignments.
    Secondly, we should chunk not by the number of sequences but instead by
    the total size of sequences. It seems that blat's performance depends
    on this. With the current setup we can sometimes get random failures
    likely due to out of memory issues. This should fix that.

--- files/genome-mapping/find-unmapped.sql
+++ files/genome-mapping/find-unmapped.sql
@@ -10,6 +10,7 @@ ON
   pre.upi = rna.upi
 WHERE
   pre.taxid = :taxid
+  AND rna.len BETWEEN :min_length AND :max_length
   AND (pre.has_coordinates IS false or pre.has_coordinates IS NULL)
   AND (pre.is_active = true OR pre.is_active IS NULL)
 ) TO STDOUT
