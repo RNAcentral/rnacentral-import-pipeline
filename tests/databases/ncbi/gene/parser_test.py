3a484f2e628fadd6e6d01f1cb3823df0ae8a1d27
Blake Sweeney <bsweeney@ebi.ac.uk>
2019-09-26 13:48:43 +0100

    Better version of NCBI gene parsing
    This now does a better job at parsing NCBI Gene data. This may still
    suffer from the problems of taking too long to fetch all the data
    though.

--- tests/databases/ncbi/gene/parser_test.py
+++ tests/databases/ncbi/gene/parser_test.py
@@ -17,6 +17,9 @@ import tempfile
 
 import pytest
 
+from rnacentral_pipeline.databases import data
+from rnacentral_pipeline.databases.helpers import publications as pub
+
 from rnacentral_pipeline.databases.ncbi.gene import fetch
 from rnacentral_pipeline.databases.ncbi.gene import helpers
 from rnacentral_pipeline.databases.ncbi.gene import parser
@@ -32,8 +35,80 @@ def parse(filename):
         return list(parser.parse(tmp))
 
 
-@pytest.mark.parametrize('filename,count', [
-    ('data/ncbi_gene/simple.txt', 11),
+@pytest.fixture(scope='module')
+def simple():
+    parsed = parse('data/ncbi_gene/simple.txt')
+    return {p.primary_id: p for p in parsed}
+
+
+def test_parses_all_data(simple):
+    assert set(simple.keys()) == {
+        'NCBI_GENE:113344155',
+        'NCBI_GENE:114058594',
+        'NCBI_GENE:107303968',
+        'NCBI_GENE:112780674',
+        'NCBI_GENE:112442518',
+        'NCBI_GENE:115038462',
+        'NCBI_GENE:107303968',
+        'NCBI_GENE:113275511',
+        'NCBI_GENE:34399170',
+        'NCBI_GENE:108869573',
+        'NCBI_GENE:31217049',
+        'NCBI_GENE:38465973',
+    }
+
+
+def test_produces_correct_data(simple):
+    assert simple['NCBI_GENE:113344155'] == data.Entry(
+        primary_id='NCBI_GENE:113344155',
+        accession='NCBI_GENE:113344155',
+        ncbi_tax_id=3469,
+        database='NCBI_GENE',
+        sequence='TTTTGGCAGTGATGACTTATACACAGCTTTATGCCAGTTCTGCTTCAGAAAATTCTTGATAGAGAGCTGAAATATATCTGAGTCTAAT',
+        regions=[],
+        rna_type='snoRNA',
+        url='https://www.ncbi.nlm.nih.gov/gene/113344155',
+        seq_version='1',
+        species='Papaver somniferum',
+        common_name='opium poppy',
+        lineage=(
+            'Eukaryota; Viridiplantae; Streptophyta; '
+            'Embryophyta; Tracheophyta; Spermatophyta; Magnoliopsida; '
+            'Ranunculales; Papaveraceae; Papaveroideae; Papaver; Papaver '
+            'somniferum'
+        ),
+        xref_data={},
+        product="small nucleolar RNA Z223",
+        gene='LOC113344155',
+        references=[pub.reference(25355515)],
+        description='Papaver somniferum (opium poppy) LOC113344155',
+    )
+
+
+@pytest.mark.parametrize('gene_id,rna_type', [
+    ('NCBI_GENE:113344155', 'snoRNA'),
+    ('NCBI_GENE:107303968', 'lncRNA'),
+    ('NCBI_GENE:114058594', 'lncRNA'),
+    ('NCBI_GENE:112780674', 'snoRNA'),
+    ('NCBI_GENE:112442518', 'tRNA'),
+    ('NCBI_GENE:115038462', 'tRNA'),
+    ('NCBI_GENE:113275511', 'lncRNA'),
+    ('NCBI_GENE:34399170', 'tRNA'),
+    ('NCBI_GENE:108869573', 'tRNA'),
+    ('NCBI_GENE:31217049', 'tRNA'),
+    ('NCBI_GENE:38465973', 'tRNA'),
 ])
-def test_parses_all_data(filename, count):
-    assert len(parse(filename)) == count
+def test_assigns_correct_rna_types(simple, gene_id, rna_type):
+    assert simple[gene_id].rna_type == rna_type
+
+
+# @pytest.mark.parametrize('gene_id,product', [
+#     ('NCBI_GENE:112442518', 'tRNA-Leu'),
+#     ('NCBI_GENE:115038462', 'tRNA-Glu'),
+#     ('NCBI_GENE:34399170', None),
+#     ('NCBI_GENE:108869573', 'tRNA-Gly'),
+#     ('NCBI_GENE:31217049', 'tRNA-Val'),
+#     ('NCBI_GENE:38465973', 'tRNA-Pro')
+# ])
+# def test_gets_correct_trna_product(simple, gene_id, product):
+#     assert simple[gene_id].product == product
