cb800e53cce952b08eac5497fdd024daef174551
Blake Sweeney <bsweeney@ebi.ac.uk>
2019-10-03 17:00:33 +0100

    Initial testing of loading and using known
    This tests the logic for just fetching and using the known assemblies.
    This isn't a complete solution for the issue of hiding multiple genomes
    because we need to also update the old genome when needed. For example
    we can do a point update to the human genome, or we can update any
    organism to the latest genome if needed.

--- files/import-data/ensembl/known-assemblies.sql
+++ files/import-data/ensembl/known-assemblies.sql
@@ -1,20 +1,15 @@
-COPY (
-  SELECT
-    json_build_object(
-      'assembly_id', genome.assembly_id,
-      'assembly_full_name', genome.assembly_full_name,
-      'gca_accession', genome.gca_accession,
-      'assembly_ucsc', genome.assembly_ucsc,
-      'common_name', genome.common_name,
-      'taxid', genome.taxid,
-      'ensembl_url', genome.ensembl_url,
-      'division', genome.division,
-      'blat_mapping', genome.blat_mapping,
-      'example', json_build_object(
-        'chromosome', genome.chromosome,
-        'start', genome.assembly.start,
-        'end', genome.assembly_end
-      )
-    )
-    FROM ensembl_assembly genome
-) TO STDOUT
+SELECT
+    genome.assembly_id as assembly_id,
+    genome.assembly_full_name as assembly_full_name,
+    genome.gca_accession as gca_accession,
+    genome.assembly_ucsc as assembly_ucsc,
+    genome.common_name as common_name,
+    genome.taxid as taxid,
+    genome.ensembl_url as ensembl_url,
+    genome.division as division,
+    genome.blat_mapping as blat_mapping,
+    genome.example_chromosome as example_chromosome,
+    genome.example_start as example_start,
+    genome.example_end example_end
+FROM ensembl_assembly genome
+;
