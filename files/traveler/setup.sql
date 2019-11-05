2e349e97e9545805bc5df33c724d6e76e4843e05
Blake Sweeney <bsweeney@ebi.ac.uk>
2019-11-05 16:06:01 +0000

    Try to rework to new auto-traveler interface
    We have to find sequences to run and the names of model types in a
    different way now. This should be roughly in line with what needs to
    happen.

--- files/traveler/setup.sql
+++ files/traveler/setup.sql
@@ -3,7 +3,11 @@ DROP TABLE IF EXISTS :tablename;
 CREATE TABLE :tablename AS
 SELECT 
 	t.upi,
-	models.rfam_model_id as model
+  case
+  when clans.rfam_clan_id == 'CL00111' THEN 'crw'
+  when clans.rfam_clan_id == 'CL00112' and models.rfam_model_id != 'CL00112' THEN 'ribovision'
+  else models.rfam_model_id
+  end as model
 FROM (
 SELECT
 	hits.upi upi,
@@ -13,31 +17,15 @@ FROM rfam_model_hits hits
 GROUP BY hits.upi
 HAVING count(hits.rfam_hit_id) = 1
 ) t
-JOIN rfam_models models ON t.rfam_model_id = models.rfam_model_id
-WHERE
-  rfam_rna_type != 'Gene; rRNA'
+JOIN rfam_models models 
+ON 
+  t.rfam_model_id = models.rfam_model_id
+LEFT JOIN rfam_clans clans
+ON
+  models.rfam_clan_id = clans.rfam_clan_id
 ;
 
 CREATE UNIQUE INDEX un_traveler_sequences_to_analyze__upi ON :tablename(upi);
-
-INSERT INTO :tablename (
-  upi,
-  model
-) (
-SELECT DISTINCT
-  pre.upi,
-  'rRNA' as model
-FROM rnc_rna_precomputed pre
-JOIN qa_status qa ON qa.rna_id = pre.id
-WHERE
-  pre.is_active = true
-  AND pre.rna_type = 'rRNA'
-  AND qa.incomplete_sequence = false
-) ON CONFLICT (upi) DO UPDATE
-SET
-  model = excluded.model
-;
-
 CREATE INDEX ix_traveler_sequences_to_analyze__model ON :tablename(model);
 
 -- Delete already computed
@@ -45,6 +33,15 @@ DELETE FROM :tablename urs
 USING rnc_secondary_structure_layout layout
 WHERE
   layout.urs = urs.upi
+;
+
+DELETE FROM :tablename to_draw
+JOIN qa_status qa 
+ON 
+  qa.upi = to_draw.upi
+WHERE
+  to_draw.model in ('crw', 'ribovision')
+  AND qa.incomplete_sequence = true
 ;
 
 -- Delete all lncRNAs
