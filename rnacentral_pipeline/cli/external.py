dfe440a749fb0cf24e2b9d9ed97aa0f66da66215
Blake Sweeney <bsweeney@ebi.ac.uk>
2020-02-20 10:39:35 +0000

    Cleanups
    Add docs, formats and such.

--- rnacentral_pipeline/cli/external.py
+++ rnacentral_pipeline/cli/external.py
@@ -17,55 +17,59 @@ limitations under the License.
 import click
 from click_aliases import ClickAliasedGroup
 
-from rnacentral_pipeline.writers import write_entries
-from rnacentral_pipeline.writers import write_ontology_annotations as onto_writer
-
 from rnacentral_pipeline.databases import rfam
 from rnacentral_pipeline.databases.ena import parser as ena
-from rnacentral_pipeline.databases.pdb import parser as pdb
-from rnacentral_pipeline.databases.generic import parser as generic
-from rnacentral_pipeline.databases.refseq import parser as refseq
 from rnacentral_pipeline.databases.ensembl import parser as ensembl
-from rnacentral_pipeline.databases.ensembl_genomes import plants as e_plants
 from rnacentral_pipeline.databases.ensembl_genomes import fungi as e_fungi
 from rnacentral_pipeline.databases.ensembl_genomes import metazoa as e_metazoa
-from rnacentral_pipeline.databases.ensembl_genomes import protists as e_protists
-from rnacentral_pipeline.databases.quickgo import parser as quickgo
+from rnacentral_pipeline.databases.ensembl_genomes import plants as e_plants
+from rnacentral_pipeline.databases.ensembl_genomes import \
+    protists as e_protists
+from rnacentral_pipeline.databases.genecards_suite import genecards, malacards
+from rnacentral_pipeline.databases.generic import parser as generic
 from rnacentral_pipeline.databases.gtrnadb import parser as gtrnadb
-from rnacentral_pipeline.databases.ncbi.gene import parser as ncbi_gene
-from rnacentral_pipeline.databases.silva import parser as silva
 from rnacentral_pipeline.databases.intact import parser as intact
+from rnacentral_pipeline.databases.ncbi.gene import parser as ncbi_gene
+from rnacentral_pipeline.databases.pdb import parser as pdb
+from rnacentral_pipeline.databases.quickgo import parser as quickgo
+from rnacentral_pipeline.databases.refseq import parser as refseq
+from rnacentral_pipeline.databases.silva import parser as silva
+from rnacentral_pipeline.writers import write_entries
+from rnacentral_pipeline.writers import \
+    write_ontology_annotations as onto_writer
 
 
-@click.group('external', cls=ClickAliasedGroup)
+@click.group("external", cls=ClickAliasedGroup)
 def cli():
     """
     This is a group of commands for processing data from various databases into
     the CSV files that can be loaded by pgloader.
     """
-    pass
 
 
-@cli.command('json-schema', aliases=[
-    '5srrnadb',
-    'flybase',
-    'lncbase',
-    'lncbook',
-    'lncipedia',
-    'mirbase',
-    'mirgenedb',
-    'pombase',
-    'snodb',
-    'sgd',
-    'tarbase',
-    'zwd',
-])
-@click.argument('json_file', type=click.File('r'))
-@click.argument('output', default='.', type=click.Path(
-    writable=True,
-    dir_okay=True,
-    file_okay=False,
-))
+@cli.command(
+    "json-schema",
+    aliases=[
+        "5srrnadb",
+        "flybase",
+        "lncbase",
+        "lncbook",
+        "lncipedia",
+        "mirbase",
+        "mirgenedb",
+        "pombase",
+        "sgd",
+        "snodb",
+        "tarbase",
+        "zwd",
+    ],
+)
+@click.argument("json_file", type=click.File("r"))
+@click.argument(
+    "output",
+    default=".",
+    type=click.Path(writable=True, dir_okay=True, file_okay=False),
+)
 def process_json_schema(json_file, output):
     """
     This parses our JSON schema files to produce the importable CSV files.
@@ -73,45 +77,36 @@ def process_json_schema(json_file, output):
     write_entries(generic.parse, output, json_file)
 
 
-@cli.command('ensembl')
-@click.argument('ensembl_file', type=click.File('r'))
-@click.argument('family_file', type=click.Path(
-    file_okay=True,
-    dir_okay=False,
-    readable=True,
-))
-@click.argument('gencode_gff', type=click.Path(
-    file_okay=True,
-    dir_okay=False,
-    readable=True,
-))
-@click.argument('exclude', type=click.File('r'))
-@click.argument('output', default='.', type=click.Path(
-    writable=True,
-    dir_okay=True,
-    file_okay=False,
-))
+@cli.command("ensembl")
+@click.argument("ensembl_file", type=click.File("r"))
+@click.argument(
+    "family_file", type=click.Path(file_okay=True, dir_okay=False, readable=True)
+)
+@click.argument(
+    "gencode_gff", type=click.Path(file_okay=True, dir_okay=False, readable=True)
+)
+@click.argument("exclude", type=click.File("r"))
+@click.argument(
+    "output",
+    default=".",
+    type=click.Path(writable=True, dir_okay=True, file_okay=False,),
+)
 def process_ensembl(ensembl_file, family_file, gencode_gff, exclude, output):
     """
     This will parse EMBL files from Ensembl to produce the expected CSV files.
     """
     write_entries(
-        ensembl.parse, 
-        output, 
-        ensembl_file, 
-        family_file, 
-        gencode_gff,
-        exclude,
+        ensembl.parse, output, ensembl_file, family_file, gencode_gff, exclude,
     )
 
 
-@cli.command('ensembl_plants')
-@click.argument('ensembl_file', type=click.File('r'))
-@click.argument('output', default='.', type=click.Path(
-    writable=True,
-    dir_okay=True,
-    file_okay=False,
-))
+@cli.command("ensembl_plants")
+@click.argument("ensembl_file", type=click.File("r"))
+@click.argument(
+    "output",
+    default=".",
+    type=click.Path(writable=True, dir_okay=True, file_okay=False,),
+)
 def process_ensembl_plants(ensembl_file, output):
     """
     This will process the Ensembl Plant data to produce files for import. The
@@ -120,47 +115,56 @@ def process_ensembl_plants(ensembl_file, output):
     write_entries(e_plants.parse, output, ensembl_file)
 
 
-@cli.command('ensembl_fungi')
-@click.argument('ensembl_file', type=click.File('r'))
-@click.argument('output', default='.', type=click.Path(
-    writable=True,
-    dir_okay=True,
-    file_okay=False,
-))
+@cli.command("ensembl_fungi")
+@click.argument("ensembl_file", type=click.File("r"))
+@click.argument(
+    "output",
+    default=".",
+    type=click.Path(writable=True, dir_okay=True, file_okay=False,),
+)
 def process_ensembl_fungi(ensembl_file, output):
+    """
+    Process data from Ensembl!Fungi and produces files for import.
+    """
     write_entries(e_fungi.parse, output, ensembl_file)
 
 
-@cli.command('ensembl_metazoa')
-@click.argument('ensembl_file', type=click.File('r'))
-@click.argument('output', default='.', type=click.Path(
-    writable=True,
-    dir_okay=True,
-    file_okay=False,
-))
+@cli.command("ensembl_metazoa")
+@click.argument("ensembl_file", type=click.File("r"))
+@click.argument(
+    "output",
+    default=".",
+    type=click.Path(writable=True, dir_okay=True, file_okay=False,),
+)
 def process_ensembl_metazoa(ensembl_file, output):
+    """
+    Process data from Ensembl!Metazoa and produces files for import.
+    """
     write_entries(e_metazoa.parse, output, ensembl_file)
 
 
-@cli.command('ensembl_protists')
-@click.argument('ensembl_file', type=click.File('r'))
-@click.argument('output', default='.', type=click.Path(
-    writable=True,
-    dir_okay=True,
-    file_okay=False,
-))
+@cli.command("ensembl_protists")
+@click.argument("ensembl_file", type=click.File("r"))
+@click.argument(
+    "output",
+    default=".",
+    type=click.Path(writable=True, dir_okay=True, file_okay=False,),
+)
 def process_ensembl_protists(ensembl_file, output):
+    """
+    Process data from Ensembl!Protists and produces files for import.
+    """
     write_entries(e_protists.parse, output, ensembl_file)
 
 
-@cli.command('pdb')
-@click.argument('pdb_data', default='pdb.json', type=click.File('r'))
-@click.argument('extra', default='pdb-extra.json', type=click.File('r'))
-@click.argument('output', default='.', type=click.Path(
-    writable=True,
-    dir_okay=True,
-    file_okay=False,
-))
+@cli.command("pdb")
+@click.argument("pdb_data", default="pdb.json", type=click.File("r"))
+@click.argument("extra", default="pdb-extra.json", type=click.File("r"))
+@click.argument(
+    "output",
+    default=".",
+    type=click.Path(writable=True, dir_okay=True, file_okay=False,),
+)
 def process_pdb(pdb_data, extra, output):
     """
     This will fetch and parse all sequence data from PDBe to produce the csv
@@ -169,14 +173,14 @@ def process_pdb(pdb_data, extra, output):
     write_entries(pdb.parse, output, pdb_data, extra)
 
 
-@cli.command('ena')
-@click.argument('ena_file', type=click.File('r'))
-@click.argument('mapping_file', type=click.File('r'))
-@click.argument('output', default='.', type=click.Path(
-    writable=True,
-    dir_okay=True,
-    file_okay=False,
-))
+@cli.command("ena")
+@click.argument("ena_file", type=click.File("r"))
+@click.argument("mapping_file", type=click.File("r"))
+@click.argument(
+    "output",
+    default=".",
+    type=click.Path(writable=True, dir_okay=True, file_okay=False,),
+)
 def process_ena(ena_file, mapping_file, output):
     """
     Process ENA EMBL formatted files into CSV to import. The additional mapping
@@ -185,24 +189,24 @@ def process_ena(ena_file, mapping_file, output):
     write_entries(ena.parse_with_mapping_file, output, ena_file, mapping_file)
 
 
-@cli.command('ncbi-gene')
-@click.argument('data-file', type=click.File('r'))
-@click.argument('output', default='.', type=click.Path(
-    writable=True,
-    dir_okay=True,
-    file_okay=False,
-))
+@cli.command("ncbi-gene")
+@click.argument("data-file", type=click.File("r"))
+@click.argument(
+    "output",
+    default=".",
+    type=click.Path(writable=True, dir_okay=True, file_okay=False,),
+)
 def process_ncbi_gene(data_file, output):
     write_entries(ncbi_gene.parse, output, data_file)
 
 
-@cli.command('quickgo')
-@click.argument('raw_data', type=click.File('r'))
-@click.argument('output', default='.', type=click.Path(
-    writable=True,
-    dir_okay=True,
-    file_okay=False,
-))
+@cli.command("quickgo")
+@click.argument("raw_data", type=click.File("r"))
+@click.argument(
+    "output",
+    default=".",
+    type=click.Path(writable=True, dir_okay=True, file_okay=False,),
+)
 def ontologies_quickgo(raw_data, output):
     """
     This will process a quickgo file and output files into the given directory.
@@ -210,13 +214,13 @@ def ontologies_quickgo(raw_data, output):
     onto_writer(quickgo.parser, output, raw_data)
 
 
-@cli.command('refseq')
-@click.argument('refseq_file', type=click.File('r'))
-@click.argument('output', default='.', type=click.Path(
-    writable=True,
-    dir_okay=True,
-    file_okay=False,
-))
+@cli.command("refseq")
+@click.argument("refseq_file", type=click.File("r"))
+@click.argument(
+    "output",
+    default=".",
+    type=click.Path(writable=True, dir_okay=True, file_okay=False,),
+)
 def process_refseq(refseq_file, output):
     """
     This will parse GenBank files from refseq to produce the expected CSV files.
@@ -224,14 +228,14 @@ def process_refseq(refseq_file, output):
     write_entries(refseq.parse, output, refseq_file)
 
 
-@cli.command('rfam')
-@click.argument('rfam_file', type=click.File('r'))
-@click.argument('mapping_file', type=click.File('r'))
-@click.argument('output', default='.', type=click.Path(
-    writable=True,
-    dir_okay=True,
-    file_okay=False,
-))
+@cli.command("rfam")
+@click.argument("rfam_file", type=click.File("r"))
+@click.argument("mapping_file", type=click.File("r"))
+@click.argument(
+    "output",
+    default=".",
+    type=click.Path(writable=True, dir_okay=True, file_okay=False,),
+)
 def process_rfam(rfam_file, mapping_file, output):
     """
     Process Rfam's JSON format into the files to import.
@@ -239,54 +243,54 @@ def process_rfam(rfam_file, mapping_file, output):
     write_entries(rfam.parser.parse, output, rfam_file, mapping_file)
 
 
-@cli.command('silva')
-@click.argument('silva-file', type=click.File('r'))
-@click.argument('output', default='.', type=click.Path(
-    writable=True,
-    dir_okay=True,
-    file_okay=False,
-))
+@cli.command("silva")
+@click.argument("silva-file", type=click.File("r"))
+@click.argument(
+    "output",
+    default=".",
+    type=click.Path(writable=True, dir_okay=True, file_okay=False,),
+)
 def process_silva(silva_file, output):
     write_entries(silva.parse, output, silva_file)
 
 
-@cli.command('gtrnadb')
-@click.argument('data_file', type=click.File('r'))
-@click.argument('output', default='.', type=click.Path(
-    writable=True,
-    dir_okay=True,
-    file_okay=False,
-))
+@cli.command("gtrnadb")
+@click.argument("data_file", type=click.File("r"))
+@click.argument(
+    "output",
+    default=".",
+    type=click.Path(writable=True, dir_okay=True, file_okay=False,),
+)
 def process_gtrnadb(data_file, output):
     write_entries(gtrnadb.parse, output, data_file)
 
 
-@cli.command('genecards')
-@click.argument('data_file', type=click.File('r'))
-@click.argument('known_sequences', type=click.File('rb'))
-@click.argument('output', default='.', type=click.Path(
-    writable=True,
-    dir_okay=True,
-    file_okay=False,
-))
+@cli.command("genecards")
+@click.argument("data_file", type=click.File("r"))
+@click.argument("known_sequences", type=click.File("rb"))
+@click.argument(
+    "output",
+    default=".",
+    type=click.Path(writable=True, dir_okay=True, file_okay=False,),
+)
 def process_genecarrds(data_file, known_sequences, output):
     write_entries(genecards.parse, output, data_file, known_sequences)
 
 
-@cli.command('malacards')
-@click.argument('data_file', type=click.File('r'))
-@click.argument('known_sequences', type=click.File('rb'))
-@click.argument('output', default='.', type=click.Path(
-    writable=True,
-    dir_okay=True,
-    file_okay=False,
-))
+@cli.command("malacards")
+@click.argument("data_file", type=click.File("r"))
+@click.argument("known_sequences", type=click.File("rb"))
+@click.argument(
+    "output",
+    default=".",
+    type=click.Path(writable=True, dir_okay=True, file_okay=False,),
+)
 def process_malacards(data_file, known_sequences, output):
     write_entries(malacards.parse, output, data_file, known_sequences)
 
 
-@cli.command('intact')
-@click.argument('data_file', type=click.File('r'))
-@click.argument('output', default='interactions.csv', type=click.File('w'))
+@cli.command("intact")
+@click.argument("data_file", type=click.File("r"))
+@click.argument("output", default="interactions.csv", type=click.File("w"))
 def process_intact(data_file, output):
     intact.parse_and_write(data_file, output)
