b0109a244cd490380886bcf7107c8546f1a54f73
Blake Sweeney <bsweeney@ebi.ac.uk>
2020-03-23 11:11:15 +0000

    Fix column name
    New columns try to use urs (as that is correct), but I didn't catch
    that.

--- files/qa/rfam.sql
+++ files/qa/rfam.sql
@@ -7,7 +7,7 @@ select
 from rna
 left join pipeline_tracking_qa_scan analyzed
 on
-  analyzed.upi = rna.upi
+  analyzed.urs = rna.upi
   and analyzed.model_source = 'rfam'
 where
   analyzed.upi is null
