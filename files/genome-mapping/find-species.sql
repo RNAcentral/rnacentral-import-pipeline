dda9a7ec54e693ad981b6fb1106fd8e54946133e
Blake Sweeney <bsweeney@ebi.ac.uk>
2020-03-31 11:58:12 +0100

    Fix genome mapping species logic
    We should only extract the species to map if the species do not have any
    unattempted sequences.

--- files/genome-mapping/find-species.sql
+++ files/genome-mapping/find-species.sql
@@ -1,8 +1,10 @@
 COPY (
 SELECT
-  ensembl_url,
-  assembly_id,
-  taxid,
-  division
-FROM :species_to_map to_map
+  species.ensembl_url,
+  species.assembly_id,
+  species.taxid,
+  species.division
+FROM :species_to_map species
+where
+  exists(select 1 from :tablename to_map where to_map.assembly_id = species.assembly_id)
 ) TO STDOUT CSV;
