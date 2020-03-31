60578bb7729f9f18d6374dd690f5805245844027
Blake Sweeney <bsweeney@ebi.ac.uk>
2020-03-31 12:01:18 +0100

    Fix typos
    Some simple typos in the queries.

--- files/genome-mapping/find-unmapped.sql
+++ files/genome-mapping/find-unmapped.sql
@@ -2,10 +2,10 @@ COPY (
 SELECT
 json_build_object(
   'id', to_map.urs_taxid,
-  'sequence', gm.seq
+  'sequence', to_map.seq
 )
 FROM :tablename to_map
 WHERE
   to_map.taxid = :taxid
-  AND to_map.assembly_id = :assembly_id
+  AND to_map.assembly_id = :'assembly_id'
 ) TO STDOUT
