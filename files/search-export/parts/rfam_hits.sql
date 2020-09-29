d2cd99de1ba4ae3a2068f9692d3a92f2850a42f2
Blake Sweeney <bsweeney@ebi.ac.uk>
2020-09-29 19:32:05 +0100

    Start outlining search speed ups
    As part of the speed up I will use more, smaller queries as well as
    possibly generating the queries automatically. One natural way to split
    queries for xrefs is by partition. I will probably use this, along with
    the smaller queries to build up a faster pipeline.

--- files/search-export/parts/rfam_hits.sql
+++ files/search-export/parts/rfam_hits.sql
@@ -1,17 +1,13 @@
 COPY (
   SELECT
     json_build_object(
-      'id', todo.id,
-      'rfam_ids', array_agg(hits.rfam_model_id),
-      'rfam_family_names', array_agg(models.short_name),
-      'rfam_clans', array_agg(models.rfam_clan_id),
+      'upi', hits.upi,
+      'rfam_ids', hits.rfam_model_id,
+      'rfam_family_names', models.short_name,
+      'rfam_clans', models.rfam_clan_id,
     )
-  FROM :tablename todo
-  JOIN rfam_model_hits hits 
-  ON 
-    hits.upi = todo.urs
+  FROM rfam_model_hits hits 
   JOIN rfam_models models 
   ON 
     hits.rfam_model_id = models.rfam_model_id
-  GROUP BY todo.id
 ) TO STDOUT
