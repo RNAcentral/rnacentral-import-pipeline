410b78b26e573de439e469e7919887fe2fe024c0
Blake Sweeney <bsweeney@ebi.ac.uk>
2020-12-15 20:05:30 +0000

    Add logic to index name -> SO mapping
    I will need this in a couple places I think. Seems like it is used for
    converting to SO terms as well as normalizing Ensembl names, so might as
    well centralize it.

--- tests/databases/sequence_ontology/tree_test.py
+++ tests/databases/sequence_ontology/tree_test.py
@@ -13,26 +13,49 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+import tempfile
+from functools import lru_cache
+
 from rnacentral_pipeline.databases.sequence_ontology import tree as so
 
 import pytest
 
 
+@lru_cache()
+def load():
+    return so.load_ontology(so.REMOTE_ONTOLOGY)
+
+
 @pytest.mark.parametrize("so_term_id,expected", [
     ('rRNA', [('SO:0000655', 'ncRNA'), ('SO:0000252', 'rRNA')]),
     ('rRNA_18S', [
-        ('SO:0000655', 'ncRNA'), 
+        ('SO:0000655', 'ncRNA'),
         ('SO:0000252', 'rRNA'),
         ('SO:0000650', 'small_subunit_rRNA'),
         ('SO:0000407', 'rRNA_18S'),
     ]),
     ('lincRNA', [
-        ('SO:0000655', 'ncRNA'), 
+        ('SO:0000655', 'ncRNA'),
         ('SO:0001877', 'lnc_RNA'),
         ('SO:0001463', 'lincRNA'),
     ]),
     ('group_I_intron', [('SO:0000587', 'group_I_intron')])
 ])
 def test_can_compute_some_simple_paths(so_term_id, expected):
-    data = so.load_ontology(so.REMOTE_ONTOLOGY)
+    data = load()
     assert so.rna_type_tree(data, so_term_id) == expected
+
+
+@pytest.mark.parametrize("so_id,name", [
+    ('SO:0000584', 'tmRNA'),
+    ('SO:0000602', 'guide_RNA'),
+    ('SO:0000390', 'telomerase_RNA'),
+    ('SO:0001877', 'lnc_RNA'),
+    ('SO:0001877', 'lncRNA'),
+])
+def test_can_create_expected_mapping(so_id, name):
+    with tempfile.NamedTemporaryFile() as tmp:
+        ont = load()
+        mapping = so.name_index(ont, tmp.name)
+        assert mapping[so_id] == so_id
+        assert mapping[name] == so_id
