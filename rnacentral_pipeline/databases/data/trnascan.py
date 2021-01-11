585260f636680da05d1a4667e70ac6e1221bf22f
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-01-11 19:12:00 +0000

    Add some code to parse trnscan results
    These files also have useful information we should be able to parse.
    This adds some basics for that.

---
+++ rnacentral_pipeline/databases/data/trnascan.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright [2009-2021] EMBL-European Bioinformatics Institute
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import typing as ty
+
+import attr
+from attr.validators import instance_of as is_a
+from attr.validators import optional
+
+
+def intron_endpoint(raw: str) -> ty.Optional[int]:
+    if raw == '0':
+        return None
+    return int(raw)
+
+
+@attr.s()
+class TRnaScanResults:
+    sequence_id = attr.ib(validator=is_a(str))
+    hit_index = attr.ib(validator=is_a(int))
+    sequence_start = attr.ib(validator=is_a(int))
+    sequence_stop = attr.ib(validator=is_a(int))
+    anticodon = attr.ib(validator=is_a(str))
+    trna_type = attr.ib(validator=is_a(str))
+    intron_start = attr.ib(validator=optional(is_a(int)))
+    intron_stop = attr.ib(validator=optional(is_a(int)))
+    score = attr.ib(validator=is_a(float))
+    note = attr.ib(validator=is_a(str))
+
+    @classmethod
+    def from_line(cls, line: str) -> "TRnaScanResults":
+        print(line)
+        print(line.split('\t'))
+        parts = [p.strip() for p in line.split("\t")]
+        return cls(
+            sequence_id=parts[0],
+            hit_index=int(parts[1]),
+            sequence_start=int(parts[2]),
+            sequence_stop=int(parts[3]),
+            anticodon=parts[5],
+            trna_type=parts[4],
+            intron_start=intron_endpoint(parts[6]),
+            intron_stop=intron_endpoint(parts[7]),
+            score=float(parts[8]),
+            note=parts[9],
+        )
+
+    @property
+    def is_pseduo(self) -> bool:
+        return "pseudo" in self.note
