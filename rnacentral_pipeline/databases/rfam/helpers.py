9c10df4413c3c5c410acf4e78cbd9691119fe14a
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-01-11 17:30:06 +0000

    Properly convert the sequence to DNA

--- rnacentral_pipeline/databases/rfam/helpers.py
+++ rnacentral_pipeline/databases/rfam/helpers.py
@@ -13,87 +13,89 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import re
-import operator as op
 import collections as coll
+import operator as op
+import re
 
 from rnacentral_pipeline.databases.helpers.publications import reference
+
 from ..data import Exon
 
-seq_version = op.itemgetter('seq_version')
-species = op.itemgetter('species')
-product = op.itemgetter('description')
-primary_id = op.itemgetter('primary_id')
-common_name = op.itemgetter('common_name')
-optional_id = op.itemgetter('optional_id')
-parent_accession = op.itemgetter('parent_accession')
+seq_version = op.itemgetter("seq_version")
+species = op.itemgetter("species")
+product = op.itemgetter("description")
+primary_id = op.itemgetter("primary_id")
+common_name = op.itemgetter("common_name")
+optional_id = op.itemgetter("optional_id")
+parent_accession = op.itemgetter("parent_accession")
 
 
 def lineage(data):
-    base = re.sub(r'\.$', '', data['lineage'])
-    return '; '.join(base.split(' ') + [data['species']])
+    base = re.sub(r"\.$", "", data["lineage"])
+    return "; ".join(base.split(" ") + [data["species"]])
 
 
-def taxid(data):
-    return int(data['ncbi_tax_id'])
+def taxid(data) -> int:
+    return int(data["ncbi_tax_id"])
 
 
 def feature_location_endpoints(data):
-    start = int(data['feature_location_start'])
-    stop = int(data['feature_location_end'])
+    start = int(data["feature_location_start"])
+    stop = int(data["feature_location_end"])
     return (start, stop)
 
 
-
-def mol_type(data):
-    if data['is_seed'] == '0':
-        return 'full'
-    return 'seed'
+def mol_type(data) -> str:
+    if data["is_seed"] == "0":
+        return "full"
+    return "seed"
 
 
 def references(data):
     refs = [reference(29112718)]
-    refs.extend(reference(pmid) for pmid in data.get('references', []))
+    refs.extend(reference(pmid) for pmid in data.get("references", []))
     return refs
 
 
 def note(data):
     result = coll.defaultdict(list)
-    result['Alignment'] = mol_type(data)
-    result.update(data['ontology'])
+    result["Alignment"] = mol_type(data)
+    result.update(data["ontology"])
     return result
 
 
 def exons(data):
     location_range = feature_location_endpoints(data)
-    complement = int(data['feature_location_start']) != location_range[0]
-    return [Exon(
-        chromosome_name='',
-        primary_start=location_range[0],
-        primary_end=location_range[1],
-        assembly_id='',
-        complement=complement,
-    )]
+    complement = int(data["feature_location_start"]) != location_range[0]
+    return [
+        Exon(
+            chromosome_name="",
+            primary_start=location_range[0],
+            primary_end=location_range[1],
+            assembly_id="",
+            complement=complement,
+        )
+    ]
 
 
-def rna_type(data):
-    so_id = data['ontology']['SO']
-    if so_id == '0010039':
-        so_id = '0000655'
-    return 'SO:' + so_id
+def rna_type(data) -> str:
+    so_id = data["ontology"]["SO"]
+    if so_id == "0010039":
+        so_id = "0000655"
+    return "SO:" + so_id
 
 
-def sequence(data):
-    return data['sequence'].upper()
+def sequence(data) -> str:
+    return data["sequence"].upper().replace("U", "T")
 
 
-def experiment(data):
-    return ' '.join(data['references'])
+def experiment(data) -> str:
+    return " ".join(data["references"])
 
 
 def accession(data):
     location_range = feature_location_endpoints(data)
-    return ('{parent}.{version}:{start}..{stop}:rfam').format(
+    return ("{parent}.{version}:{start}..{stop}:rfam").format(
         parent=parent_accession(data),
         version=seq_version(data),
         start=location_range[0],
@@ -102,11 +104,8 @@ def accession(data):
 
 
 def description(data):
-    return '{species} {product}'.format(
-        species=species(data),
-        product=product(data)
-    )
+    return "{species} {product}".format(species=species(data), product=product(data))
 
 
 def url(data):
-    return 'http://rfam.org/family/%s' % data['primary_id']
+    return "http://rfam.org/family/%s" % data["primary_id"]
