386148667d132cc39da8c180205809ab0a726227
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-01-14 12:26:37 +0000

    Reformat and deal with missing directories
    Sometimes not all r2dt directories are created, and we can deal with it
    by skipping it.

--- rnacentral_pipeline/rnacentral/r2dt/parser.py
+++ rnacentral_pipeline/rnacentral/r2dt/parser.py
@@ -33,54 +33,58 @@ def load_model_info(handle: ty.TextIO):
     for line in reader:
         model_name, model_id = line
         mapping[model_name] = int(model_id)
-        if model_name == 'tRNA':
-            mapping['RF00005'] = int(model_id)
+        if model_name == "tRNA":
+            mapping["RF00005"] = int(model_id)
     return mapping
 
 
 def load_hit_info(base: Path, allow_missing):
     source_directories = [
-        (base / 'crw', data.Source.crw),
-        (base / 'gtrnadb', data.Source.gtrnadb),
-        (base / 'ribovision-lsu', data.Source.ribovision),
-        (base / 'ribovision-ssu', data.Source.ribovision),
-        (base / 'rfam', data.Source.rfam),
-        (base / 'RF00005', data.Source.rfam),
+        (base / "crw", data.Source.crw),
+        (base / "gtrnadb", data.Source.gtrnadb),
+        (base / "ribovision-lsu", data.Source.ribovision),
+        (base / "ribovision-ssu", data.Source.ribovision),
+        (base / "rfam", data.Source.rfam),
+        (base / "RF00005", data.Source.rfam),
     ]
     has_ribovision = {data.Source.crw, data.Source.ribovision, data.Source.rfam}
     hit_info = {}
     for (path, source) in source_directories:
-        if source in has_ribovision \
-                and path.name != 'RF00005':
+        if not path.exists():
+            continue
+        if source in has_ribovision and path.name != "RF00005":
             update = ribovore.as_dict(path, allow_missing=True)
             if update:
                 hit_info.update(update)
     return hit_info
 
 
-def parse(info_path: ty.TextIO, base: Path, allow_missing=False) -> ty.Iterator[data.R2DTResult]:
+def parse(
+    info_path: ty.TextIO, base: Path, allow_missing=False
+) -> ty.Iterator[data.R2DTResult]:
 
     if not base.exists():
         raise ValueError("Cannot parse missing directory: %s" % base)
 
     hit_info = load_hit_info(base, allow_missing)
     model_info = load_model_info(info_path)
-    result_base = base / 'results'
-    metadata_path = result_base / 'tsv' / 'metadata.tsv'
-    with metadata_path.open('r') as raw:
-        reader = csv.reader(raw, delimiter='\t')
+    result_base = base / "results"
+    metadata_path = result_base / "tsv" / "metadata.tsv"
+    with metadata_path.open("r") as raw:
+        reader = csv.reader(raw, delimiter="\t")
         for row in reader:
             urs = row[0]
             model_name = row[1]
             source = data.Source[row[2].lower()]
             if source == data.Source.gtrnadb:
-                model_name = model_name.replace('_', '-')
+                model_name = model_name.replace("_", "-")
             if model_name not in model_info:
                 LOGGER.warning("No info for model %s", model_name)
                 continue
             model_db_id = model_info[model_name]
-            info = data.R2DTResultInfo(urs, model_name, model_db_id, source,
-                                       result_base)
+            info = data.R2DTResultInfo(
+                urs, model_name, model_db_id, source, result_base
+            )
             try:
                 info.validate()
             except Exception as e:
