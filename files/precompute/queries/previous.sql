0e63ad5a54160734629d1898205a4954c44f67c0
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-01-27 13:09:08 +0000

    Fix some queries

--- files/precompute/queries/previous.sql
+++ files/precompute/queries/previous.sql
@@ -1,11 +1,11 @@
 COPY (
 select
   json_build_object(
-    'id', pre.id,
+    'id', prev.id,
     'previous', row_to_json(prev.*)
   )
 FROM :tablename todo
-JOIN rnc_rna_precompute prev
+JOIN rnc_rna_precomputed prev
 ON
   prev.id = todo.urs_taxid
 ) TO STDOUT
