bdf0a7e0fd5b839a49a5c049d2afa55d8733d8e7
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-01-28 14:36:01 +0000

    Add option to use ids from precompute
    These ids may be faster to join/query on.

--- files/precompute/get-accessions/post-insert.sql
+++ files/precompute/get-accessions/post-insert.sql
@@ -1,3 +1,2 @@
 CREATE INDEX ix_urs_accession__urs_taxid ON urs_accession(urs_taxid);
-
-CLUSTER urs_accession USING ix_urs_accession__urs_taxid;
+CREATE INDEX ix_urs_accession__precompute_id ON urs_accession(precompute_id);
