ce09002e5a0fc4cb4598a4726276f2a3758fa024
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-02-04 16:28:46 +0000

    Move chunking to a common location
    This may be used repeatedly so make it easy to do so.

--- utils/rnc-core/src/chunking.rs
+++ utils/rnc-core/src/chunking.rs
@@ -1,8 +1,14 @@
 use serde::Deserialize;
 
 use std::{
+    fmt,
     fs::File,
-    io::BufReader,
+    io::{
+        BufReader,
+        BufWriter,
+        Read,
+        Write,
+    },
     ops::Range,
     path::{
         Path,
@@ -15,7 +21,16 @@ use anyhow::{
     Result,
 };
 
+use serde::{
+    de::DeserializeOwned,
+    ser::Serialize,
+};
+
 use csv::Reader;
+
+use itertools::Itertools;
+
+use crate::psql::JsonlIterator;
 
 pub struct SingleChunk {
     endpoints: Range<usize>,
@@ -38,7 +53,7 @@ impl From<RawChunk> for SingleChunk {
         return Self {
             endpoints: Range {
                 start: raw.start,
-                end: raw.stop,
+                end: raw.stop + 1,
             },
             filename: raw.filename,
         };
@@ -74,4 +89,32 @@ pub fn load(filename: &Path) -> Result<ChunkSpec> {
     Ok(ChunkSpec {
         ranges,
     })
+}
+
+pub fn write_splits<R: Read, T: DeserializeOwned + fmt::Debug + Serialize>(
+    iterator: JsonlIterator<R, T>,
+    id_getter: fn(&T) -> usize,
+    chunks: &Path,
+    output: &Path,
+) -> Result<()> {
+    let chunks = load(chunks)?;
+    let grouped = iterator.group_by(|entry: &T| {
+        chunks
+            .filename_of(id_getter(entry))
+            .ok_or(format!("Could not find filename for {:?}", &entry))
+            .unwrap()
+    });
+
+    for (filename, items) in &grouped {
+        let mut path = PathBuf::from(output);
+        path.push(filename);
+        let file = File::create(path)?;
+        let mut writer = BufWriter::new(file);
+        for item in items {
+            serde_json::to_writer(&mut writer, &item)?;
+            writeln!(&mut writer)?;
+        }
+    }
+
+    Ok(())
 }
