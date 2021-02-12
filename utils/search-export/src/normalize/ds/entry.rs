7c570a1945de5b92cddbb604fb27d99cb5b85d72
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-02-12 12:00:29 +0000

    Rename and add sanity check

--- utils/search-export/src/normalize/ds/entry.rs
+++ utils/search-export/src/normalize/ds/entry.rs
@@ -149,6 +149,7 @@ impl Normalized {
         let base = raw.base.clone();
 
         let urs_taxid = base.urs_taxid.to_owned();
+        assert!(urs_taxid == accessions[0].urs_taxid);
         let parts: Vec<&str> = urs_taxid.split("_").collect();
         let urs = parts[0].to_owned();
         let taxid = parts[1].parse::<usize>().unwrap();
