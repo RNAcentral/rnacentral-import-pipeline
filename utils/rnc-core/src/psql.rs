8c68344c9e3f8df9c21302c70949d1fb46fc7243
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-02-15 13:45:54 +0000

    Use escaped logic first before unescaped

--- utils/rnc-core/src/psql.rs
+++ utils/rnc-core/src/psql.rs
@@ -47,8 +47,8 @@ impl<R: Read, T: DeserializeOwned> Iterator for JsonlIterator<R, T> {
         match self.reader.read_line(&mut self.buf).unwrap() {
             0 => None,
             _ => {
-                let value: T = serde_json::from_str(&self.buf)
-                    .or(serde_json::from_str(&self.buf.replace("////", "//")))
+                let value: T = serde_json::from_str(&self.buf.replace("\\\\", "\\"))
+                    .or(serde_json::from_str(&self.buf))
                     .with_context(|| format!("Could not parse line {}", &self.buf))
                     .unwrap();
                 self.buf.clear();
