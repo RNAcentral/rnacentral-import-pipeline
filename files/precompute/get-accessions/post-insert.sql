b4f28688e45af7245156538fe1aa50f9b0559f4f
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-02-23 15:47:56 +0000

    Add timing and transactions
    This just helps with debugging things and seeing if jobs are actually
    running.

--- files/precompute/get-accessions/post-insert.sql
+++ files/precompute/get-accessions/post-insert.sql
@@ -1,2 +1,8 @@
+/timing
+
+BEGIN;
+
 CREATE INDEX ix_precompute_urs_accession__urs_taxid ON precompute_urs_accession(urs_taxid);
 CREATE INDEX ix_precompute_urs_accession__precompute_id ON precompute_urs_accession(precompute_urs_id);
+
+COMMIT;
