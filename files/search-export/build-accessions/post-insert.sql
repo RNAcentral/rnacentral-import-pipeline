e6f6304c8f1ede81f87d15f783e8b1e6fc616bde
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-02-26 09:46:54 +0000

    This index probably isn't used
    No need to have it around then.

--- files/search-export/build-accessions/post-insert.sql
+++ files/search-export/build-accessions/post-insert.sql
@@ -2,7 +2,6 @@
 
 BEGIN TRANSACTION;
 
-CREATE INDEX ix_search_accessions__urs_taxid ON search_export_accessions(urs_taxid);
 CREATE INDEX ix_search_accessions__precompute_id ON search_export_accessions(search_export_id);
 
 ALTER TABLE search_export_accessions
