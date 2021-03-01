90909a6bde99f04c9f3af71efe5da47fbaca3012
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-03-01 19:59:50 +0000

    Only create index as needed

--- files/precompute/get-accessions/post-insert.sql
+++ files/precompute/get-accessions/post-insert.sql
@@ -2,7 +2,7 @@
 
 BEGIN;
 
-CREATE INDEX ix_precompute_urs_accession__urs_taxid ON precompute_urs_accession(urs_taxid);
-CREATE INDEX ix_precompute_urs_accession__precompute_id ON precompute_urs_accession(precompute_urs_id);
+CREATE INDEX IF NOT EXISTS ix_precompute_urs_accession__urs_taxid ON precompute_urs_accession(urs_taxid);
+CREATE INDEX IF NOT EXISTS ix_precompute_urs_accession__precompute_id ON precompute_urs_accession(precompute_urs_id);
 
 COMMIT;
