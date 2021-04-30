d459ea5a24623a11c4f0cbfb9e6c74da5f933696
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-04-30 09:29:30 +0100

    Fix some of helper tests
    Not totally fixed yet, but better.

--- tests/databases/pdb/helpers_test.py
+++ tests/databases/pdb/helpers_test.py
@@ -15,7 +15,14 @@ limitations under the License.
 
 import pytest
 
+from rnacentral_pipeline.databases.pdb.data import ChainInfo
 from rnacentral_pipeline.databases.pdb import helpers
+from rnacentral_pipeline.databases.pdb import fetch
+
+
+def load(pdb_id: str, chain_id: str) -> ChainInfo:
+    chains = fetch.rna_chains(pdb_ids=[pdb_id.lower()])
+    return next(c for c in chains if c.chain_id == chain_id)
 
 
 @pytest.mark.parametrize('product,expected', [
@@ -51,34 +58,7 @@ def test_can_compute_correct_rna_types(product, expected):
     assert helpers.compound_rna_type({'compound': product}) == expected
 
 
-# def test_gets_given_taxid():
-#     assert(helpers.taxid({'taxonomyId': '562'})) == 562
-
-
-# def test_uses_synthenic_if_given_no_taxid():
-#     assert(helpers.taxid({'taxonomyId': ''})) == 32630
-
-
 def test_can_detect_if_sequence_is_not_rna():
-    data = {
-        'chainLength': '25',
-        'chainId': 'U',
-        'db_name': '',
-        'classification': 'RIBOSOME',
-        'sequence': 'GKGDRRTRRGKIWRGTYGKYRPRKK',
-        'resolution': '3.3',
-        'structureId': '5WNP',
-        'structureTitle': 'Crystal Structure of 30S ribosomal subunit from Thermus thermophilus',
-        'emdbId': '',
-        'releaseDate': '2018-02-21',
-        'source': 'Thermus thermophilus',
-        'ndbId': '',
-        'db_id': '',
-        'experimentalTechnique': 'X-RAY DIFFRACTION',
-        'compound': "RNA (5'-R(*AP*AP*AP*UP*UP*U)-3')",
-        'entityId': '21',
-        'entityMacromoleculeType': 'Polyribonucleotide (RNA)',
-        'taxonomyId': '274'
-    }
+    data = load('5wnp', 'U')
     with pytest.raises(helpers.InvalidSequence):
         helpers.sequence(data)
