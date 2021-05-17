2852f4eceed305c906dd86d8103c969f69c291c3
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-05-17 09:38:04 +0100

    Fixes to weekly update script
    Some bugs need to be ironed out.

--- weekly-update/run.sh
+++ weekly-update/run.sh
@@ -5,13 +5,17 @@ IFS=$'\n\t'
 
 export NXF_OPTS='-Dnxf.pool.type=sync -Dnxf.pool.maxThreads=10000'
 export SINGULARITY_TMPDIR='/scratch'
+export PATH="/nfs/software/singularity/3.5.0/bin:$HOME/.cargo/bin:$PATH"
 
-[ -d work/tmp] || mkdir -p work/tmp
+[ -d work/tmp ] || mkdir -p work/tmp
 [ ! -e local.config ] || rm local.config
 
 when=$(date +'%Y-%m-%d')
 
-ln -s local.config weekly-update/update.config
+ln -s weekly-update/update.config local.config 
+
+make rust
 
 ./nextflow -quiet run -with-report "$when-import.html" -profile prod import-data.nf
+./nextflow -quiet run -with-report "$when-precompute.html" -profile prod precompute.nf
 ./nextflow -quiet run -with-report "$when-search.html" -profile prod search-export.nf
