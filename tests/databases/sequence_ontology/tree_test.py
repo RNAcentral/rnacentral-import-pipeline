197f0c876152ea04f18ae388d74daac830bd32d0
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-06-07 11:22:31 +0100

    Improve SO trees
    This will now generate more trees that contain terms. This is part of
    improving the SO terms as we want nicer collections of terms. We also
    need to reject some terms, but that isn't handled yet. It may also be
    helpful to group more things into transcript, possibly by allowing a
    'part_of' relationship (or something else) instead of just 'is_a'.

--- tests/databases/sequence_ontology/tree_test.py
+++ tests/databases/sequence_ontology/tree_test.py
@@ -26,36 +26,89 @@ def load():
     return so.load_ontology(so.REMOTE_ONTOLOGY)
 
 
-@pytest.mark.parametrize("so_term_id,expected", [
-    ('rRNA', [('SO:0000655', 'ncRNA'), ('SO:0000252', 'rRNA')]),
-    ('rRNA_18S', [
-        ('SO:0000655', 'ncRNA'),
-        ('SO:0000252', 'rRNA'),
-        ('SO:0000650', 'small_subunit_rRNA'),
-        ('SO:0000407', 'rRNA_18S'),
-    ]),
-    ('lincRNA', [
-        ('SO:0000655', 'ncRNA'),
-        ('SO:0001877', 'lnc_RNA'),
-        ('SO:0001463', 'lincRNA'),
-    ]),
-    ('group_I_intron', [('SO:0000587', 'group_I_intron')])
-])
+@pytest.mark.parametrize(
+    "so_term_id,expected",
+    [
+        ("rRNA", [("SO:0000655", "ncRNA"), ("SO:0000252", "rRNA")]),
+        (
+            "rRNA_18S",
+            [
+                ("SO:0000655", "ncRNA"),
+                ("SO:0000252", "rRNA"),
+                ("SO:0000650", "small_subunit_rRNA"),
+                ("SO:0000407", "rRNA_18S"),
+            ],
+        ),
+        (
+            "lincRNA",
+            [
+                ("SO:0000655", "ncRNA"),
+                ("SO:0001877", "lncRNA"),
+                ("SO:0001463", "lincRNA"),
+            ],
+        ),
+        (
+            "group_I_intron",
+            [
+                ("SO:0000188", "intron"),
+                ("SO:0000588", "autocatalytically_spliced_intron"),
+                ("SO:0000587", "group_I_intron"),
+            ],
+        ),
+        (
+            "ribozyme",
+            [
+                ("SO:0000673", "transcript"),
+                ("SO:0000372", "enzymatic_RNA"),
+                ("SO:0000374", "ribozyme"),
+            ],
+        ),
+    ],
+)
 def test_can_compute_some_simple_paths(so_term_id, expected):
     data = load()
     assert so.rna_type_tree(data, so_term_id) == expected
 
 
-@pytest.mark.parametrize("so_id,name", [
-    ('SO:0000584', 'tmRNA'),
-    ('SO:0000602', 'guide_RNA'),
-    ('SO:0000390', 'telomerase_RNA'),
-    ('SO:0001877', 'lnc_RNA'),
-    ('SO:0001877', 'lncRNA'),
-])
+@pytest.mark.parametrize(
+    "so_id,name",
+    [
+        ("SO:0000584", "tmRNA"),
+        ("SO:0000602", "guide_RNA"),
+        ("SO:0000390", "telomerase_RNA"),
+        ("SO:0001877", "lncRNA"),
+    ],
+)
 def test_can_create_expected_mapping(so_id, name):
     with tempfile.NamedTemporaryFile() as tmp:
         ont = load()
         mapping = so.name_index(ont, tmp.name)
         assert mapping[so_id] == so_id
         assert mapping[name] == so_id
+
+
+# 'transcript',
+# 'pre-miRNA',
+# 'hammerhead_ribozyme',
+# 'riboswitch',
+# 'ncRNA_gene',
+# 'ribozyme',
+# 'regulatory_region',
+# 'primary_transcript',
+# 'group_II_intron',
+# 'five_prime_UTR',
+# 'group_I_intron',
+# 'cRISPR',
+# 'attenuator',
+# 'internal_ribosome_entry_site',
+# 'autocatalytically_spliced_intron',
+# 'three_prime_UTR',
+# 'sECIS_element',
+# 'repeat_unit',
+# 'antisense',
+# 'mRNA_region',
+# 'mature_transcript',
+# 'cis_regulatory_frameshift_element',
+# 'recoding_stimulatory_region',
+# 'rNA_sequence_secondary_structure',
+# 'rRNA_gene',
