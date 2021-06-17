fec9c305e77aeafed5355d89a8e54c38a130212a
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-06-17 08:37:38 +0100

    Clean up temp precompute tables
    These aren't needed outside of precompute, so remove them.

--- files/precompute/post-load.sql
+++ files/precompute/post-load.sql
@@ -1,1 +1,7 @@
+DROP TABLE load_qa_status;
+DROP TABLE load_precomputed;
+DROP TABLE precompute_urs;
+DROP TABLE precompute_urs_taxid;
+DROP TABLE precompute_urs_accession;
+
 vacuum (analyze, verbose);
