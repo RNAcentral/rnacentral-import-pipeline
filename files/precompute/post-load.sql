072be52c1bc7a26045d4c3ec04cb0cfee2ae54d4
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-06-18 10:49:04 +0100

    Fix cleanup ordering

--- files/precompute/post-load.sql
+++ files/precompute/post-load.sql
@@ -1,7 +1,5 @@
-DROP TABLE load_qa_status;
-DROP TABLE load_precomputed;
-DROP TABLE precompute_urs;
-DROP TABLE precompute_urs_taxid;
 DROP TABLE precompute_urs_accession;
+DROP TABLE precompute_urs_taxid;
+DROP TABLE precompute_urs;
 
 vacuum (analyze, verbose);
