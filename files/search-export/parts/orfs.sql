839aafa5945167626df9ef15e3477fca134f671a
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-07-08 12:05:05 +0100

    Group and validate search export data
    Grouping allows for more validation, which is good. Also, we need the
    expected counts in order to group properly, which means some changes to
    the pipeline.

--- files/search-export/parts/orfs.sql
+++ files/search-export/parts/orfs.sql
@@ -2,8 +2,6 @@ COPY (
 SELECT
   json_build_object(
     'id', todo.id,
-    'urs_id', todo.urs_id,
-    'urs_taxid', todo.urs_taxid,
     'source', split_part(features.feature_name, '_', 1)
   )
 FROM search_export_urs todo
