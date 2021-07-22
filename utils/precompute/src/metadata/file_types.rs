694c545a1739ce1acef0c5c643327ff15e0d1ccc
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-07-22 16:13:17 +0100

    Add missing files and definitions

---
+++ utils/precompute/src/metadata/file_types.rs
@@ -0,0 +1,19 @@
+use std::str::FromStr;
+use std::string::ToString;
+use strum_macros;
+
+#[derive(strum_macros::ToString, strum_macros::EnumString, Debug)]
+pub enum FileType {
+    #[strum(serialize = "basic")]
+    Basic,
+    #[strum(serialize = "coordinates")]
+    Coordinates,
+    #[strum(serialize = "orfs")]
+    Orfs,
+    #[strum(serialize = "previous")]
+    Previous,
+    #[strum(serialize = "r2dt-hits")]
+    R2dtHits,
+    #[strum(serialize = "rfam-hits")]
+    RfamHits,
+}
