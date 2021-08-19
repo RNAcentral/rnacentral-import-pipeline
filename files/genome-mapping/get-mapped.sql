3dc6c44c0140d46cb74f51183f9737d9eaeaccc7
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-08-19 15:33:39 +0100

    Also treat given locations as known
    Without this the program will try to map sequences which have already
    been given a location. This does not need to be done so we should
    prevent it from happening.

--- files/genome-mapping/get-mapped.sql
+++ files/genome-mapping/get-mapped.sql
@@ -4,4 +4,11 @@ COPY (
   FROM pipeline_tracking_genome_mapping
   WHERE
     assembly_id = :'assembly_id'
+  UNION
+    SELECT DISTINCT
+      urs_taxid
+    FROM rnc_sequence_regions
+    WHERE
+      assembly_id = :'assembly_id'
+      AND was_mapped = false
 ) TO STDOUT CSV;
