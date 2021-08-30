03ceb6428f9d3d34609000dcee1a89a65f73a764
Blake Sweeney <bsweeney@ebi.ac.uk>
2021-08-30 10:08:09 +0100

    Update query for getting gene info

--- files/search-export/genes/region-info.sql
+++ files/search-export/genes/region-info.sql
@@ -4,12 +4,18 @@ COPY (
     'id', todo.id,
     'urs_taxid', mem.urs_taxid,
     'locus_id', mem.locus_id,
+    'name', loc.name,
+    'assembly_id', loc.assembly_id,
+    'member_count', loc.member_count,
     'membership_status', mem.membership_status
   )
   FROM search_export_urs todo
   JOIN rnc_locus_members mem
   ON
     mem.urs_taxid = todo.urs_taxid
+  JOIN rnc_locus loc
+  ON
+    loc.id = mem.locus_id
   ORDER BY todo.id
 ) TO STDOUT
 
