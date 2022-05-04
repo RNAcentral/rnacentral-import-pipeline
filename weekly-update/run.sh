ea4e7e5ffd5395d9a121918b3247f23ad0e6c5aa
Andrew Green <agreen@ebi.ac.uk>
2022-05-04 16:11:23 +0100

    Update weekly run scripts for codon and see if it works now. For now, only running data import, not precompute or search export.
    Paths changed because nfs is no longer mounted, also removes some 
    container options and makes nextflow email me instead of Blake

--- weekly-update/run.sh
+++ weekly-update/run.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 IFS=$'\n\t'
 
 export NXF_OPTS='-Dnxf.pool.type=sync -Dnxf.pool.maxThreads=10000'
-export PATH="/nfs/software/singularity/3.5.0/bin:$HOME/.cargo/bin:$PATH"
 
 [ -d work/tmp ] || mkdir -p work/tmp
 [ ! -e local.config ] || rm local.config
@@ -17,14 +16,18 @@ fi
 
 mkdir -p singularity/bind/r2dt/data
 pushd singularity/bind/r2dt/data
-wget -O cms.tar.gz https://www.dropbox.com/s/3ie8kzb8ol658s0/cms.tar.gz?dl=1
+wget -O cms.tar.gz http://ftp.ebi.ac.uk/pub/databases/RNAcentral/r2dt/1.2/cms.tar.gz
 tar xf cms.tar.gz
 popd
 
-ln -s weekly-update/update.config local.config 
+ln -s weekly-update/update.config local.config
 
 make rust
 
-./nextflow -quiet run -with-report "$when-import.html" -profile prod import-data.nf
-./nextflow -quiet run -with-report "$when-precompute.html" -profile prod precompute.nf
-./nextflow -quiet run -with-report "$when-search.html" -profile prod search-export.nf
+# Download latest version of nextflow
+curl -s https://get.nextflow.io | bash
+
+
+./nextflow -quiet run -with-report "$when-import.html" -profile test import-data.nf
+# ./nextflow -quiet run -with-report "$when-precompute.html" -profile prod precompute.nf
+# ./nextflow -quiet run -with-report "$when-search.html" -profile prod search-export.nf
