4eda215d890794c92a2b9affdc1d8698eaf61ecd
Andrew Green <agreen@ebi.ac.uk>
2022-05-11 10:29:47 +0100

    Detect hang downloading nextflow and deal with it
    Falls back to using module installed on codon

--- weekly-update/run.sh
+++ weekly-update/run.sh
@@ -25,9 +25,17 @@ ln -s weekly-update/update.config local.config
 make rust
 
 # Download latest version of nextflow
-curl -s https://get.nextflow.io | bash
+curl --max-time 10 -s https://get.nextflow.io | bash
+res=$?
+# Load module as fallback
+if test "$res" != "0"; then
+  module load nextflow-21.10.6-gcc-9.3.0-tkuemwd
+  NF="nextflow"
+else
+  NF="./nextflow"
+fi
 
-
-./nextflow -quiet run -with-report "$when-import.html" -profile test import-data.nf
-# ./nextflow -quiet run -with-report "$when-precompute.html" -profile prod precompute.nf
-# ./nextflow -quiet run -with-report "$when-search.html" -profile prod search-export.nf
+#$NF -quiet run -with-report "$when-import.html" -profile test import-data.nf
+$NF -quiet run -with-report "$when-analyze.html" -profile test analyze.nf
+# $NF -quiet run -with-report "$when-precompute.html" -profile prod precompute.nf
+# $NF -quiet run -with-report "$when-search.html" -profile prod search-export.nf
