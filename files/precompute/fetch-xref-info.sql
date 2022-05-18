7b77b7c4089faea710dd561f74fabd190e5dd3a9
Andrew Green <agreen@ebi.ac.uk>
2022-05-18 15:00:49 +0100

    Simplify xref query
    Heavy lifting done in rust now

--- files/precompute/fetch-xref-info.sql
+++ files/precompute/fetch-xref-info.sql
@@ -1,22 +1,11 @@
-CREATE TEMP TABLE xref_releases AS
-SELECT
-  rna.id as rna_id,
-  xref.upi,
-  xref.last
-FROM xref
-JOIN rna
-ON
-  rna.upi = xref.upi
-;
+COPY(
+  SELECT
+    rna.id,
+    xref.upi,
+    xref.last
+  FROM xref
+  JOIN rna
+  ON
+    rna.upi = xref.upi
 
-CREATE INDEX ix_xref_releases_upi ON xref_releases(upi);
-
-COPY (
-SELECT
-  rna_id,
-  upi,
-  max(last)
-from xref_releases
-group by rna_id, upi
-order by rna_id ASC
 ) TO STDOUT (FORMAT CSV)
