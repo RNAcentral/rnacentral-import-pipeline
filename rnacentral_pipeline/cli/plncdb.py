1ac49632f7e08cd8e389b6882f346dea10503098
Andrew Green <agreen@ebi.ac.uk>
2022-07-01 09:26:21 +0100

    Working parser for PLncDB, with some caveats
    The description is not properly built yet
    
    The directory structure we end up with may not work with this parser, so 
    the file selection logic may need tweaking.

--- rnacentral_pipeline/cli/plncdb.py
+++ rnacentral_pipeline/cli/plncdb.py
@@ -18,5 +18,26 @@ from pathlib import Path
 import click
 from furl import furl
 
-from rnacentral_pipeline.databases.plncdb import fetch, parser
+from rnacentral_pipeline.databases.plncdb import parser
 from rnacentral_pipeline.writers import entry_writer
+
+
+@click.group("plncdb")
+def cli():
+    """
+    A group of commands dealing with PLncDB data.
+    """
+    pass
+
+
+@cli.command("parse")
+@click.argument("data", type=click.Path(dir_okay=True, readable=True, file_okay=False))
+@click.argument(
+    "output",
+    default=".",
+    type=click.Path(writable=True, dir_okay=True, file_okay=False),
+)
+def parse(data, output):
+    entries = parser.parse(Path(data))
+    with entry_writer(Path(output)) as writer:
+        writer.write(entries)
