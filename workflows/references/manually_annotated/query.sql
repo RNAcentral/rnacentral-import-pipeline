915fa9eebc58812ceb0d205dd2f726d30dc34021
carlosribas <caduribas@gmail.com>
2023-01-23 17:06:55 +0000

    Update query

--- workflows/references/manually_annotated/query.sql
+++ workflows/references/manually_annotated/query.sql
@@ -1,17 +1,13 @@
-select
-	xref.upi || '_' || xref.taxid,
-	-- acc.accession,
-	acc."database",
-	refs.pmid,
-	refs.doi,
-	refs.pmcid
-	-- refs.epmcid
-from rnc_accessions acc
-join xref
-on xref.ac = acc.accession
-join rnc_reference_map rmap on rmap.accession = acc.accession
-join rnc_references refs on refs.id = rmap.reference_id
-where
-	xref.dbid in (24, 20, 14, 16, 18, 23, 27, 44, 48)
-	and xref.deleted = 'N'
-;
+COPY(
+  SELECT
+    refs.pmid,
+    xref.upi || '_' || xref.taxid
+  FROM rnc_accessions acc
+  JOIN xref ON xref.ac = acc.accession
+  JOIN rnc_reference_map rmap ON rmap.accession = acc.accession
+  JOIN rnc_references refs ON refs.id = rmap.reference_id
+  WHERE
+	  xref.dbid IN (14, 16, 18, 20, 23, 24, 27, 44, 48)
+	  AND xref.deleted = 'N'
+	  AND refs.pmid IS NOT NULL
+) TO STDOUT (FORMAT CSV)
