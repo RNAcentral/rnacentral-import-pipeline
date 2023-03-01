6cda64e6aee3fbfe322d34b23213868686d9db7c
Andrew Green <agreen@ebi.ac.uk>
2023-03-01 10:25:55 +0000

    Start working to write out sequence features
    Will be doing some database changes in tandem

--- rnacentral_pipeline/databases/rediportal/parser.py
+++ rnacentral_pipeline/databases/rediportal/parser.py
@@ -1,7 +1,12 @@
+import json
+import re
+
 import attr
 import pandas as pd
 import pybedtools as pbt
 from attr.validators import instance_of as is_a
+
+from rnacentral_pipeline.databases import data
 
 
 @attr.s(frozen=True)
@@ -15,9 +20,54 @@ class GenomicLocation(object):
         chromosome = re.sub("^chr", "", raw["chrom"])
         return cls(
             chromosome=chromosome,
-            start=raw["start"],
-            stop=raw["end"],
+            start=raw["start_rel_URS"],
+            stop=raw["end_rel_URS"],
         )
+
+
+@attr.s(frozen=True)
+class RNAEditFeature(object):
+    upi = attr.ib(validator=is_a(str))
+    taxid = attr.ib(validator=is_a(int), converter=int)
+    repeat_type = attr.ib(validator=is_a(str))
+    ref = attr.ib(validator=is_a(str))
+    ed = attr.ib(validator == is_a(str))
+    start = attr.ib(validator=is_a(int), converter=int)
+    stop = attr.ib(validator=is_a(int), converter=int)
+    genomic_location = attr.ib(validator=is_a(GenomicLocation))
+
+    @classmethod
+    def build(cls, raw_feature):
+        upi, taxid = raw_feature["URS_taxid"].split("_")
+        return cls(
+            upi=upi,
+            taxid=taxid,
+            repeat_type=raw_feature["repeat_type"],
+            ref=raw_feature["Ref"],
+            ed=raw_feature["Ed"],
+            start=raw_feature["start_rel_URS"],
+            stop=raw_feature["end_rel_URS"],
+            genomic_location=GenomicLocation.build(raw_feature),
+        )
+
+    def writeable(self):
+        metadata = attr.asdict(self)
+        metadata = {
+            "repeat_type": self.repeat_type,
+            "genomic_location": metadata["genomic_location"],
+            "reference": self.ref,
+            "edit": self.ed,
+        }
+
+        return [
+            self.upi,
+            self.taxid,
+            None,
+            self.start,
+            self.stop,
+            "RNA_editing_event",
+            json.dumps(metadata),
+        ]
 
 
 def parse(redi_bedfile, redi_metadata, rnc_bedfile, output):
@@ -46,20 +96,13 @@ def parse(redi_bedfile, redi_metadata, rnc_bedfile, output):
                 "strand",
                 "repeat_type",
                 "_rnc_chrom",
-                "rnc_start",
-                "rnc_end",
+                "rnc_exon_start",
+                "rnc_exon_end",
                 "urs_taxid",
                 "_rnc_score",
                 "_rnc_strand",
-                "_rnc_start",
-                "_rnc_end",
-                "_rnc_bed_rgb",
-                "rnc_num_exons",
-                "rnc_exon_sizes",
-                "rnc_exon_starts",
-                "_dot",
-                "_rnc_rna_type",
-                "_rnc_providing_dbs",
+                "rnc_transcript_start",
+                "rnc_transcript_end",
             ],
             index_col=False,
         )
@@ -68,30 +111,18 @@ def parse(redi_bedfile, redi_metadata, rnc_bedfile, output):
                 "_rnc_chrom",
                 "_rnc_score",
                 "_rnc_strand",
-                "_rnc_start",
-                "_rnc_end",
-                "_rnc_bed_rgb",
-                "_dot",
-                "_rnc_rna_type",
-                "_rnc_providing_dbs",
             ],
             axis="columns",
         )
     )
 
-    # intersection["start_rel_URS"] = (
-    #     intersection["start_rel_genome"] - intersection["rnc_start"]
-    # )
-    # print(intersection.head())
-    # print(metadata.head())
-    # complete_data = intersection.merge(metadata, how="inner", on="region_id")
+    intersection["start_rel_URS"] = (
+        intersection["start_rel_genome"] - intersection["rnc_transcript_start"]
+    )
+    intersection["end_rel_URS"] = intersection["end_rel_genome"] = intersection[
+        "rnc_transcript_start"
+    ]
+    print(intersection.head())
 
-    # complete_data["rnc_exon_starts"] = complete_data["rnc_exon_starts"].apply(lambda x: [int(a) for a in x.split(',')])
-    # complete_data["rnc_exon_sizes"] = complete_data["rnc_exon_sizes"].apply(lambda x: [int(a) for a in x.split(',')])
-    # complete_data = complete_data.explode(["rnc_exon_starts","rnc_exon_sizes"])
-    # complete_data["exon_start_rel_genome"] = (complete_data["rnc_exon_starts"] + complete_data["rnc_start"].astype(int)).astype(int)
-    # complete_data["exon_end_rel_genome"] = (complete_data["exon_start_rel_genome"] + complete_data["rnc_exon_sizes"]).astype(int)
-    # print(complete_data.dtypes)
-
-    # hits = complete_data.loc[complete_data[["start_rel_genome","exon_start_rel_genome"]].max().lt(complete_data[["end_rel_genome","exon_end_rel_genome"]].min(axis=1))]
-    # print(hits.head())
+    complete_data = intersection.merge(metadata, how="inner", on="region_id")
+    print(complete_data)
