8ead580216cd6dd10f42020fa3264856a1738097
Andrew Green <agreen@ebi.ac.uk>
2024-03-05 08:37:16 +0000

    Update text mining query to create URS publication counts table as temp
    Should ensure we have the correct publication counts and fix the missing `has_lit_scan` issue

--- files/search-export/parts/text-mining.sql
+++ files/search-export/parts/text-mining.sql
@@ -1,3 +1,14 @@
+CREATE TEMPORARY TABLE search_export_publication_counts
+AS
+SELECT
+  UPPER(d.primary_id),
+  SUM(j.hit_count)
+FROM litscan_job j
+JOIN litscan_database d
+ON d.job_id = j.job_id
+WHERE j.hit_count > 0 AND d.name='rnacentral'
+GROUP BY d.primary_id;
+
 COPY (
   SELECT
     json_build_object(
