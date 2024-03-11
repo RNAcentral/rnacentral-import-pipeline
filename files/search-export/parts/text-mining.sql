d0e1c534e6fef67ae432f4b4e9cc7aba41ad2b1e
Andrew Green <agreen@ebi.ac.uk>
2024-03-11 11:52:13 +0000

    Silence output of temp table creation for litscan publication count

--- files/search-export/parts/text-mining.sql
+++ files/search-export/parts/text-mining.sql
@@ -1,3 +1,4 @@
+\o /dev/null
 CREATE TEMPORARY TABLE search_export_publication_counts
 AS
 SELECT
@@ -8,6 +9,8 @@ JOIN litscan_database d
 ON d.job_id = j.job_id
 WHERE j.hit_count > 0 AND d.name='rnacentral'
 GROUP BY d.primary_id;
+
+\o
 
 COPY (
   SELECT
