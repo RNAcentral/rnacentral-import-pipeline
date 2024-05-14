cc40c818f179053cfa0007d80ec40df9916673e5
Andrew Green <agreen@ebi.ac.uk>
2024-05-14 09:21:04 +0100

    Avoid precompute database OOM by skipping temp table creation + indexing

--- files/precompute/fetch-xref-info.sql
+++ files/precompute/fetch-xref-info.sql
@@ -1,22 +1,27 @@
-CREATE TEMP TABLE xref_releases AS
-SELECT
-  rna.id as rna_id,
-  xref.upi,
-  xref.last
-FROM xref
-JOIN rna
-ON
-  rna.upi = xref.upi
-;
+-- CREATE TEMP TABLE xref_releases AS
+-- SELECT
+--   rna.id as rna_id,
+--   xref.upi,
+--   xref.last
+-- FROM xref
+-- JOIN rna
+-- ON
+--   rna.upi = xref.upi
+-- ;
 
-CREATE INDEX ix_xref_releases_upi ON xref_releases(upi);
+-- CREATE INDEX ix_xref_releases_upi ON xref_releases(upi);
+
+-- The above causes the database to have an OOM error, so we operate directly on the tables now, and skip the indexing
+
 
 COPY (
 SELECT
-  rna_id,
-  upi,
+  rna.id as rna_id,
+  xref.upi as upi,
   max(last)
-from xref_releases
+from xref
+join rna
+  on rna.upi = xref.upi
 group by rna_id, upi
 order by rna_id ASC
 
