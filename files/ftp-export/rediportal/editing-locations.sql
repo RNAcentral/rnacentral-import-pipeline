f0c08ab260c97cfd2933d26936dafe3e36381058
Blake Sweeney <bsweeney@ebi.ac.uk>
2024-06-11 10:58:19 +0100

    Improve REDIportal export
    I think we need both sequence and genomic coordinate information.

--- files/ftp-export/rediportal/editing-locations.sql
+++ files/ftp-export/rediportal/editing-locations.sql
@@ -1,11 +1,10 @@
 COPY(
-  SELECT
-
-	upi || '_' || taxid AS urs_taxid,
-  start AS location,
+SELECT
+  upi || '_' || taxid AS urs_taxid,
+  start AS sequence_location,
+  concat(metadata::json->'genomic_location'->>'chromosome', ':', metadata::json->'genomic_location'->'start', '-', metadata::json->'genomic_location'->'stop') AS genomic_location,
   metadata::json->'reference' AS reference,
   metadata::json->'edit' AS edit
-
 FROM rnc_sequence_features
 WHERE feature_provider = 'REDIPORTAL'
 ) TO STDOUT WITH HEADER CSV QUOTE ' '
