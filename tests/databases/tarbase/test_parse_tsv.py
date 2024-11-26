b984e8d248f998101519a56bd22d8083d273f3f1
Andrew Green <agreen@ebi.ac.uk>
2024-11-26 10:58:16 +0000

    Add test case for tarbase virus data

--- tests/databases/tarbase/test_parse_tsv.py
+++ tests/databases/tarbase/test_parse_tsv.py
@@ -48,3 +48,43 @@ def test_tarbase_tsv_parse_existing():
     parsed_entry = parse("data/tarbase/tarbase.tsv")[0]
 
     assert parsed_entry == correct_entry
+
+
+@pytest.mark.tarbase
+def test_tarbase_tsv_parse_virus():
+    """
+    Try to parse the interaction for a viral species
+    """
+
+    # Accession line: "TARBASE:hsa-miR-103a-3p","","1","1","24","ncRNA","","TARBASE","hsa-miR-103a-3p","","Homo sapiens (human) hsa-miR-103a-3p","","","","","TARBASE:hsa-miR-103a-3p","","","","miRNA","{""url"": ""http://carolina.imis.athena-innovation.gr/diana_tools/web/index.php?r=tarbasev8%2Findex&miRNAs%5B%5D=hsa-miR-103a-3p""}","","","{}","SO:0000276","http://carolina.imis.athena-innovation.gr/diana_tools/web/index.php?r=tarbasev8%2Findex&miRNAs%5B%5D=hsa-miR-103a-3p"
+    # Related Seq: "TARBASE:hsa-miR-103a-3p","ENSEMBL:ENSG00000000460","target_protein","{""Microarrays""}"
+
+    related_sequences = [
+        RelatedSequence(
+            sequence_id=f"ENSEMBL:ENSMUSG00000000740",
+            relationship="target_protein",
+            evidence=RelatedEvidence(methods=["qCLASH"]),
+        )
+    ]
+    ## NB: the method changed between version 8 & 9: Microarrays -> PAR-CLIP
+    url = "https://dianalab.e-ce.uth.gr/tarbasev9/interactions?gene=Rpl13&mirna=mghv-miR-M1-1-3p"
+    correct_entry = Entry(
+        primary_id="mghv-miR-M1-1-3p",
+        accession="TARBASE:mghv-miR-M1-1-3p",
+        ncbi_tax_id=33708,
+        database="TARBASE",
+        sequence="TAGAAATGGCCGTACTTCCTTT",
+        regions=[],  ## Don't create regions for protein target
+        rna_type="SO:0000276",
+        url=url,
+        seq_version=1,
+        optional_id="MIMAT0001564",
+        description="Murid gammaherpesvirus 4 (Murine herpesvirus 68) mghv-miR-M1-1-3p",
+        note_data={"url": url},
+        xref_data={},
+        related_sequences=related_sequences,
+    )
+
+    parsed_entry = parse("data/tarbase/tarbase_viral.tsv")[0]
+
+    assert parsed_entry == correct_entry
