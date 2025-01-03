94077c076c3e97d623b77e0e5997f4f61e761c46
Andrew Green <agreen@ebi.ac.uk>
2025-01-03 10:55:18 +0000

    Rfam still uses model alias

--- rnacentral_pipeline/rnacentral/r2dt/data.py
+++ rnacentral_pipeline/rnacentral/r2dt/data.py
@@ -351,9 +351,9 @@ class R2DTResultInfo(object):
     def __filename__(self, extension):
         # if self.source == Source.gtrnadb and extension == "fasta":
         #     return f"{self.urs}.{extension}"
-        # if self.source == Source.rfam and not self.model_name.startswith("RF"):
-        #     if extension == "fasta":
-        #         return f"{self.urs}.{extension}"
+        if self.source == Source.rfam and not self.model_name.startswith("RF"):
+            if extension == "fasta":
+                return f"{self.urs}-{self.model_alias}.{extension}"
         #     assert self.model_alias.startswith("RF"), f"No existing alias for {self}"
         #     return f"{self.urs}-{self.model_alias}.{extension}"
         return f"{self.urs}-{self.model_name}.{extension}"
