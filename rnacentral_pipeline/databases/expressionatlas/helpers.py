9c35dee86ffc04fa58e6eb24bf3339d0689b28e6
Andrew Green <agreen@ebi.ac.uk>
2025-03-26 09:45:44 +0000

    Make absolutely sure sequence does not contain U

--- rnacentral_pipeline/databases/expressionatlas/helpers.py
+++ rnacentral_pipeline/databases/expressionatlas/helpers.py
@@ -81,7 +81,7 @@ def as_entry(info, experiment):
         accession=accession(info),
         ncbi_tax_id=taxid(info),
         database="EXPRESSION_ATLAS",
-        sequence=info["seq"],
+        sequence=info["seq"].replace("U", "T"),
         regions=region_builder(info),
         rna_type=rna_type(info["rna_type"]),
         url=url(experiment),
