2a4e0e90c084c438e56c4135b7cf674361216705
Andrew Green <agreen@ebi.ac.uk>
2025-04-29 12:59:25 +0100

    Rework how R2DT fetches sequences from the database
    Now, we extract directly from partition tables, so they are guaranteed to be not deleted, then we combine and uniqueify outside the database in polars.
    
    This should be orders of magnitude faster. Tested working as far as extracting the saw sequences as json

--- files/r2dt/find-sequences.sql
+++ files/r2dt/find-sequences.sql
@@ -1,12 +1,14 @@
+CREATE TEMP TABLE urs_to_fetch (urs text);
+
+\copy urs_to_fetch FROM 'urs_to_fetch.csv' DELIMITER ',' CSV HEADER;
+
 COPY (
-SELECT DISTINCT ON (rna.upi) json_build_object(
+SELECT json_build_object(
         'id', rna.upi,
         'sequence', COALESCE(rna.seq_short, rna.seq_long)
       )
 FROM rna
-JOIN xref ON xref.upi = rna.upi AND xref.deleted = 'N'
-LEFT JOIN pipeline_tracking_traveler track ON track.urs = rna.upi
-WHERE track.urs IS NULL
-  AND rna.len < :max_len
+JOIN urs_to_fetch ON rna.upi = urs_to_fetch.urs
+  where rna.len < :max_len
 LIMIT :sequence_count
 ) TO STDOUT;
