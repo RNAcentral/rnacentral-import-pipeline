0c7ca402770053548703feac229e252149282652
Andrew Green <agreen@ebi.ac.uk>
2025-05-18 21:26:55 +0100

    Handle prexisting index and constraints
    In case the tables didn't get cleared out properly I think

--- files/search-export/build-accessions/post-insert.sql
+++ files/search-export/build-accessions/post-insert.sql
@@ -2,10 +2,19 @@
 
 BEGIN TRANSACTION;
 
-CREATE INDEX ix_search_accessions__precompute_id ON search_export_accessions(search_export_id);
+CREATE INDEX IF NOT EXISTS ix_search_accessions__precompute_id ON search_export_accessions(search_export_id);
 
-ALTER TABLE search_export_accessions
-  ADD CONSTRAINT fk_search_accessions__search_id FOREIGN KEY (search_export_id) REFERENCES search_export_urs(id)
-;
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1
+    FROM information_schema.table_constraints
+    WHERE constraint_name = 'fk_search_accessions__search_id'
+      AND table_name = 'search_export_accessions'
+  ) THEN
+    ALTER TABLE search_export_accessions
+      ADD CONSTRAINT fk_search_accessions__search_id FOREIGN KEY (search_export_id) REFERENCES search_export_urs(id);
+  END IF;
+END $$;
 
 COMMIT;
