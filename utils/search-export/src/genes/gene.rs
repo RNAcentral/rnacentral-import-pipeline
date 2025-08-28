7ed7017c6b6b3a96f58b3d85df66a8fe7bba7f36
Blake Sweeney <bsweeney@ebi.ac.uk>
2025-08-28 08:19:23 +0100

    Outline initial gene export work

--- utils/search-export/src/genes/gene.rs
+++ utils/search-export/src/genes/gene.rs
@@ -1,13 +1,7 @@
 use std::iter::FromIterator;
 
-use crate::{
-    search_xml::Entry,
-    utils::set_or_check,
-};
-use serde::{
-    Deserialize,
-    Serialize,
-};
+use crate::{search_xml::Entry, utils::set_or_check};
+use serde::{Deserialize, Serialize};
 
 use super::gene_member::GeneMember;
 
@@ -31,10 +25,32 @@ impl<'a> Gene {
         let description = self.members[0].description();
         let mut entry = Entry::new(self.id.to_string(), &self.name, &description);
 
+        entry.add_field("description", "TODO");
+        entry.add_field("rna_type", "TODO");
+        entry.add_field("active", "True");
+        entry.add_field("gene", "TODO");
+        entry.add_field("taxonomy", "TODO");
+        entry.add_field("lineage_path", "TODO");
+        entry.add_field("length", "TODO");
+        entry.add_tree("so_rna_type", "TODO", &vec!["TODO".to_string()]);
+
+        let mut has_litsumm = false;
+        let mut has_lit_scan = false;
+        let mut has_go_annotations = false;
+        let mut has_secondary_structure = false;
+        let mut qc_warning_found = false;
+        let mut has_genomic_coordinates = false;
         for member in &self.members {
             entry.add_field("entry_type", "Gene");
             entry.add_field("gene_member", member.urs_taxid());
             entry.add_field("member_description", member.description());
+
+            has_litsumm = has_litsumm | member.has_litsumm();
+            has_lit_scan = has_lit_scan | member.has_lit_scan();
+            has_go_annotations = has_go_annotations | member.has_go_annotations();
+            has_secondary_structure = has_secondary_structure | member.has_secondary_structure();
+            qc_warning_found = qc_warning_found | member.qc_warning_found();
+            has_genomic_coordinates = has_genomic_coordinates | member.has_genomic_coordinates();
 
             entry.add_ref("rnacentral", member.urs_taxid());
             for xref in member.cross_references() {
@@ -44,6 +60,13 @@ impl<'a> Gene {
                 entry.add_ref(xref.name(), xref.external_id())
             }
         }
+
+        entry.add_field("has_genomic_coordinates", &has_litsumm.to_string());
+        entry.add_field("qc_warning_found", &qc_warning_found.to_string());
+        entry.add_field("has_secondary_structure", &has_secondary_structure.to_string());
+        entry.add_field("has_go_annotations", &has_go_annotations.to_string());
+        entry.add_field("has_lit_scan", &has_lit_scan.to_string());
+        entry.add_field("has_litsumm", &has_litsumm.to_string());
 
         entry
     }
