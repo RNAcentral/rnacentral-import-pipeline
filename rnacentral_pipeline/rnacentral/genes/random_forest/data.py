6346272d442f96611134defe8690c911b7c01af4
Andrew Green <agreen@ebi.ac.uk>
2025-09-01 15:12:55 +0100

    Handle metadata for getes, including descriptions and types
    Uses a score-based selection method derived from the same kind of logic in precompute. Works ok on human genes, not yet tested on other organisms

--- rnacentral_pipeline/rnacentral/genes/random_forest/data.py
+++ rnacentral_pipeline/rnacentral/genes/random_forest/data.py
@@ -19,11 +19,23 @@ import re
 import uuid
 from functools import lru_cache
 from rnacentral_pipeline.rnacentral.genes.random_forest.preprocessing import exon_overlap
+from rnacentral_pipeline.rnacentral.precompute import utils
 
 import numpy as np
 import polars as pl
 import psycopg2 as pg
 from psycopg2.extras import RealDictCursor
+from rnacentral_pipeline.databases.data import Database
+from tqdm import tqdm
+
+import time
+import obonet as obo
+import networkx as nx
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import multiprocessing as mp    
+
+
+SO_ONTOLOGY_URL = "https://raw.githubusercontent.com/The-Sequence-Ontology/SO-Ontologies/refs/heads/master/Ontology_Files/so-simple.obo"
 
 ## This query might be replaced with something from rnc_sequence_regions_active
 QUERY = """
@@ -47,6 +59,94 @@ join rnc_rna_precomputed pc on pc.id = sr.urs_taxid
 
 where pc.taxid = %s
 """
+
+
+
+description_scores = {
+    Database.mirbase.value.descr: 100, ## Pretty good for miRNA
+    Database.wormbase.value.descr: 100, ## Good for worm, but sometimes vague
+    Database.hgnc.value.descr: 100, ## Gold standard
+    Database.gencode.value.descr: 70,## pretty good
+    Database.ensembl.value.descr: 50, ## Variable quality...
+    Database.tair.value.descr: 100, ## Good for arabidopsis
+    Database.sgd.value.descr: 100, ## SGD is usually pretty good
+    Database.flybase.value.descr: 100, ## Good, but sometimes vague
+    Database.dictybase.value.descr: 100, ## Very good
+    Database.pombase.value.descr: 70, ## Pombase's descriptions are a bit vague
+    Database.mgi.value.descr: 80, ## MGI's descriptions have a lot of 'uncharacterised' in them
+    Database.rgd.value.descr: 70, ## Similar for RGD
+    Database.zfin.value.descr: 50, ## They're mostly just gene names
+    Database.mirgenedb.value.descr: 90, ## Very good for miRNA
+    Database.plncdb.value.descr: 10, ## Should prefer other plant sources if available
+    Database.lncipedia.value.descr: 40, ## Not bad but a bit vague
+    Database.lncrnadb.value.descr: 90, ## Pretty good for lncRNA
+    Database.lncbook.value.descr: 40, ## Vague, just gene names mostly
+    Database.gtrnadb.value.descr: 60, ## Just a gene name and species, but pretty good for tRNA
+    Database.tmrna_website.value.descr: 90, ## Should be the standard for tmRNA, hopefully no type conflicts
+    Database.five_srrnadb.value.descr: 30, ## Just species name + 5SrRNA, not that informative
+    Database.ribocentre.value.descr: 40, ## Can be a bit formulaic and uninformative
+    Database.pdbe.value.descr: 40, ## Can be really informative, or can be crazy long strings of nonsense
+    Database.refseq.value.descr: 30, ## Not very informative
+    Database.ensembl_plants.value.descr: 15, ## Slightly preferable to plncdb if available
+    Database.ensembl_metazoa.value.descr: 10, ## Just species + gene name, not very informative
+    Database.ensembl_protists.value.descr: 20, ## Slicghtly more informative than metazoa
+    Database.ensembl_fungi.value.descr: 40, ## Needs to be lower than SGD andPomBase, but is still quite informative
+    Database.genecards.value.descr: 40, ## All human, all homo sapiens + gene ID, so not that informative
+    Database.malacards.value.descr: 40, ## As with genecards
+    Database.intact.value.descr: 40, ## Not as good as mir-specifics, and not quite as good as tRNA specifics, but still pretty good
+    Database.expression_atlas.value.descr: 10, ## Only species + gene ID
+    Database.rfam.value.descr: 65, ## Place these slightly above others, but not by much. Now includes cm description too.
+    Database.tarbase.value.descr: 10, ## Species + gene name
+    Database.lncbase.value.descr: 10, ## Species + gene name
+    Database.snodb.value.descr: 50, ## Slightly below Rfam, but better than others
+    Database.snorna_database.value.descr: 40, ## Slightly worse than snodb
+    Database.pirbase.value.descr: 0, ## piRNAs will be getting filtered, so this should not contribute 
+    Database.modomics.value.descr: 10, ## Species + type
+    Database.vega.value.descr: 20, ## Long and potentially not that valuable
+    Database.srpdb.value.descr: 30, ## Quite informative, but should be well below MODs
+    Database.snopy.value.descr: 10, ## Species + type, not that informative
+    Database.crw.value.descr: 10, ## Very short, not very informative
+    Database.silva.value.descr: 5, ## Not very informative
+    Database.greengenes.value.descr: 5, ## Not very informative
+    Database.rdp.value.descr: 5, ## Species + type
+    Database.ena.value.descr: 2, ## Often vague and uninformative
+    Database.zwd.value.descr: 2, ## Not very uninformative
+    Database.noncode.value.descr: 5, ## Species + ncRNA - not very useful
+    Database.evlncrnas.value.descr: 1, ## Very vague
+    Database.mgnify.value.descr: 1, ## All metagemonic, not super useful
+
+    "R2DT": 30, ## Can be very specific, but only has the model name, so not that informative
+}
+
+## Database type scoring should be slightly different
+type_scores = {
+    Database.five_srrnadb.value.descr: 100,
+    Database.flybase.value.descr: 100,
+    Database.gtrnadb.value.descr: 60, 
+    Database.lncbase.value.descr: 50,
+    Database.lncipedia.value.descr: 40,
+    Database.mirbase.value.descr: 100, 
+    Database.mirgenedb.value.descr: 90,
+    Database.pirbase.value.descr: 50, 
+    Database.pombase.value.descr: 70, 
+    Database.snodb.value.descr: 50, 
+    Database.snorna_database.value.descr: 40, 
+    Database.tarbase.value.descr: 100,
+    Database.zwd.value.descr: 2, 
+    Database.tmrna_website.value.descr: 90, 
+    Database.pdbe.value.descr: 40, 
+    Database.gencode.value.descr: 70,
+
+    Database.genecards.value.descr: -20, ## These should be ignored
+    Database.malacards.value.descr: -20, 
+
+    Database.rfam.value.descr: 100,
+    "R2DT": 100, ## Can be very specific
+
+}
+
+so_graph = obo.read_obo(SO_ONTOLOGY_URL)
+
 
 
 @lru_cache()
@@ -473,3 +573,268 @@ def store_genes(final_genes, taxid, db_str):
     """
     cur.execute(member_upsert_query)
     conn.commit()   
+
+
+def get_accessions(urs_taxids, db_str):
+    conn = pg.connect(db_str)
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+
+    cur.execute(
+    """SELECT 
+    a.urs_taxid, 
+    ac.database, 
+    ac.description, 
+    ac.rna_type,
+    0.0 as cm_overlap
+    FROM
+    rnc_accession_active a
+    JOIN genes_urs_taxid t 
+        ON t.urs_taxid = a.urs_taxid
+    JOIN rnc_accessions ac
+        ON ac.accession = a.accession
+        WHERE t.urs_taxid = ANY(%s)"""
+    , (urs_taxids,))
+
+    accessions = pl.DataFrame(cur.fetchall()).cast({"cm_overlap": pl.Float64})
+    conn.close()
+    return accessions
+
+def get_cm_hits(urs_taxids, db_str):
+    conn = pg.connect(db_str)
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+
+    cur.execute(
+    """(SELECT 
+            g.urs_taxid,
+            'RFAM' as database,
+            rfm.long_name as description,
+            rfm.so_rna_type as rna_type,
+            rf.sequence_completeness as cm_overlap
+        
+            FROM genes_urs_taxid g
+            LEFT JOIN rfam_model_hits_old rf
+                    ON rf.upi = g.urs
+            JOIN rfam_models rfm
+                    ON rfm.rfam_model_id = rf.rfam_model_id
+            WHERE g.urs_taxid = ANY(%s)
+            )
+
+            UNION
+
+            (SELECT 
+            g.urs_taxid,
+            'R2DT' as database,
+            r2m.model_name as description,
+            r2m.so_term_id as rna_type,
+            r2.sequence_coverage as cm_overlap
+            
+
+            FROM genes_urs_taxid g
+            LEFT JOIN r2dt_results r2
+                    ON r2.urs = g.urs
+            JOIN r2dt_models r2m
+                    ON r2m.id = r2.model_id
+            WHERE g.urs_taxid = ANY(%s)
+        )"""
+    , (urs_taxids,urs_taxids,)
+    )
+    res = cur.fetchall()
+    if len(res) > 0:
+        hits = pl.DataFrame(res)
+    else:
+        hits = pl.DataFrame([{"urs_taxid": None, "database": None, "description": None, "rna_type": None, "cm_overlap": None}]).filter(pl.col("urs_taxid").is_not_null())
+    conn.close()
+    return hits
+
+def calculate_type_specificity(so_type):
+        if so_type is None:
+            return -1000
+        try:
+            return nx.shortest_path_length(so_graph, so_type, "SO:0000673")
+        except nx.NetworkXNoPath:
+            return -1000
+
+def calculate_description_score(args):        
+    db_score = description_scores.get(args["database"], 0)
+    entropy_score = utils.entropy(args["description"])
+    return db_score + entropy_score
+
+
+def calculate_cm_type_score(args):
+    db_score = type_scores.get(args["database"], 0)
+    specificity_score = calculate_type_specificity(args["rna_type"])
+    overlap_score = float(args.get("cm_overlap", 0.0)) * 100.0
+    return db_score + specificity_score + overlap_score
+
+def process_chunk(chunk, db_str):
+    return [process_group(group_item, db_str) for group_item in chunk]
+
+def process_group(group_data, db_str, progress_queue=None):
+    """Process a single group - extracted from your original loop"""
+    group_key, group_df = group_data
+    gene_name, = group_key
+    
+    try:
+        descriptions = get_accessions(group_df.get_column("urs_taxid").to_list(), db_str)
+        cm_hits = get_cm_hits(group_df.get_column("urs_taxid").to_list(), db_str)
+        cm_hits = cm_hits.with_columns(pl.col("cm_overlap").fill_null(0.0))
+        descriptions = pl.concat([descriptions, cm_hits], how="vertical")
+        descriptions_with_scores = descriptions.with_columns(
+            desc_score=pl.struct(pl.col("database"), pl.col("description"))
+            .map_elements(calculate_description_score, return_dtype=pl.Float32)
+        )
+        descriptions_with_scores = descriptions_with_scores.with_columns(
+            type_score=pl.struct(pl.col("urs_taxid"), pl.col("database"), pl.col("rna_type"), pl.col("cm_overlap"))
+            .map_elements(calculate_cm_type_score, return_dtype=pl.Float32)
+        )
+
+        
+        best_description = (descriptions_with_scores
+                           .sort(by='desc_score', descending=True)
+                           .get_column("description")
+                           .to_list()[0])
+        best_type = (descriptions_with_scores
+                     .sort(by='type_score', descending=True)
+                     .get_column("rna_type")
+                     .to_list()[0])
+        
+        result = {"name": gene_name, "description": best_description, "rna_type": best_type}
+        
+        # Update progress if queue provided
+        if progress_queue:
+            progress_queue.put(1)
+            
+        return result
+        
+    except Exception as e:
+        print(f"Error processing {gene_name}: {e}")
+        if progress_queue:
+            progress_queue.put(1)
+        return {"name": gene_name, "description": "ERROR"}
+
+
+def remove_species_mention(description, species_name=None, common_name=None):
+    """
+    Remove species mentions from descriptions to make a short description.
+    
+    Args:
+        description: The text to clean
+        species_name: Scientific name (e.g., "Homo sapiens") - optional
+        common_name: Common name (e.g., "human") - optional
+    """
+    
+    # If specific species/common names are provided, remove them
+    if species_name:
+        # Remove scientific name (case insensitive)
+        description = re.sub(rf'\b{re.escape(species_name)}\b', '', description, flags=re.IGNORECASE)
+    
+    if common_name:
+        # Remove common name in parentheses
+        description = re.sub(rf'\s*\(\s*{re.escape(common_name)}\s*\)', '', description, flags=re.IGNORECASE)
+        # Also remove standalone common name
+        description = re.sub(rf'\b{re.escape(common_name)}\b', '', description, flags=re.IGNORECASE)
+    
+    # General pattern to catch common formats if no specific names provided
+    if not species_name and not common_name:
+        # Remove pattern like "Genus species (common name)"
+        description = re.sub(r'\b[A-Z][a-z]+ [a-z]+ \([^)]+\)', '', description)
+    
+    # Clean up extra whitespace
+    description = re.sub(r'\s+', ' ', description).strip()
+    
+    return description
+
+
+def get_metadata(final_genes, db_str):
+    """
+    From the gene members, we want to get the descriptions from the precompute
+    table for each member URS_taxid, and where they came from.
+    
+    Then apply an ordered choice to select the best description for the gene.
+     
+
+    """
+    final_genes = pl.read_json(final_genes).with_columns(n_members=pl.col("members").list.len()).sort(by="n_members", descending=False).head(10)
+    final_genes = final_genes.explode("members").with_columns(pl.col("members").str.split('@').list.first().alias("urs_taxid"))
+    
+    buffer = io.StringIO()
+    for name in final_genes.get_column("urs_taxid").to_list():
+        buffer.write(f"{name}\t{name.split('_')[0]}\n")
+    buffer.seek(0)
+
+    ## Create a temp table from the region names
+    conn = pg.connect(db_str)
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    cur.execute("DROP TABLE IF EXISTS genes_urs_taxid")
+    cur.execute("CREATE TABLE genes_urs_taxid (urs_taxid TEXT, urs TEXT)")
+    cur.copy_from(buffer, "genes_urs_taxid", columns=("urs_taxid", "urs",))
+    conn.commit()
+    conn.close()
+    # Main parallelization code
+    grouped = final_genes.group_by("name", maintain_order=True)
+    group_items = list(grouped)  # Convert to list for multiprocessing
+    chunk_size = 250
+    chunks = [group_items[i:i + chunk_size] for i in range(0, len(group_items), chunk_size)]
+    results = []
+    max_workers = 6 ## Based in the number of connections and what saturates the DB
+    
+    
+    
+    with ProcessPoolExecutor(max_workers=max_workers) as executor:
+        chunk_futures = [executor.submit(process_chunk, chunk, db_str) for chunk in chunks]
+        
+        for future in tqdm(as_completed(chunk_futures), 
+                          total=len(chunks), 
+                          desc="Processing chunks"):
+            try:
+                chunk_results = future.result()
+                results.extend([r for r in chunk_results if r is not None])
+            except Exception as e:
+                print(f"Error processing chunk: {e}")
+    
+    metadata =  pl.DataFrame(results)
+    metadata = metadata.with_columns(short_description=pl.col("description").map_elements(remove_species_mention, return_dtype=pl.Utf8).alias("short_description"))
+    return metadata
+
+
+def store_metadata(metadata, db_str):
+    conn = pg.connect(db_str)
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+
+    metadata = pl.read_json(metadata)
+
+    buffer = io.StringIO()
+    for name in metadata.get_column("name").to_list():
+        buffer.write(f"{name}\n")
+    buffer.seek(0)
+    print(metadata)
+    ## Create a temp table from the region names
+    cur.execute("CREATE TEMPORARY TABLE gene_metadata_lookup (public_name TEXT)")
+    cur.copy_from(buffer, "gene_metadata_lookup", columns=("public_name",))
+    conn.commit()
+    cur.execute("""SELECT g.id as rnc_gene_id, g.public_name FROM rnc_genes g
+    JOIN gene_metadata_lookup l ON l.public_name = g.public_name""")
+
+    gene_ids = pl.DataFrame(cur.fetchall())
+    print(gene_ids)
+    metadata = metadata.join(gene_ids, left_on="name", right_on="public_name", how="inner")
+    print(metadata)
+    insert_query = """
+    INSERT INTO rnc_gene_metadata (rnc_gene_id, description, so_rna_type) VALUES """
+    args_str = ""
+    for row in metadata.iter_rows():
+        args_str += cur.mogrify("(%s,%s,%s),", row).decode('utf-8') 
+    
+    args_str = args_str.rstrip(',')
+
+    upsert_query = insert_query + args_str + """
+    ON CONFLICT (rnc_gene_id) 
+    DO UPDATE SET 
+        rnc_gene_id = EXCLUDED.rnc_gene_id,
+        description = EXCLUDED.description,
+        so_rna_type = EXCLUDED.so_rna_type
+    """
+    cur.execute(upsert_query)
+    conn.commit()
+
+    conn.close()
