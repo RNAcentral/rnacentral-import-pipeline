816e8950afebbe9efe4c8d16bca0544b1192823e
Andrew Green <agreen@ebi.ac.uk>
2026-03-23 16:26:18 +0000

    Add parsing of reference proteome data from UniProt STATS file

--- rnacentral_pipeline/databases/ncbi/taxonomy.py
+++ rnacentral_pipeline/databases/ncbi/taxonomy.py
@@ -46,9 +46,10 @@ class TaxonomyEntry(object):
     aliases = attr.ib(validator=is_a(list), hash=False)
     replaced_by = attr.ib(validator=optional(is_a(int)))
     rank = attr.ib(validator=is_a(str), default="")
+    reference_proteome = attr.ib(validator=is_a(bool), default=False)
 
     @classmethod
-    def build(cls, entry, names, rank=""):
+    def build(cls, entry, names, rank="", reference_proteome=False):
         aliases = set()
         for name_entry in names:
             (tax_id, name, _, name_class) = name_entry
@@ -63,6 +64,7 @@ class TaxonomyEntry(object):
             aliases=sorted(aliases),
             replaced_by=None,
             rank=rank,
+            reference_proteome=reference_proteome,
         )
 
     def writeable(self):
@@ -73,6 +75,7 @@ class TaxonomyEntry(object):
             json.dumps(self.aliases),
             self.replaced_by,
             self.rank,
+            self.reference_proteome,
         ]
 
 
@@ -98,16 +101,29 @@ def parse_nodes(handle):
     return {row[0]: row[2] for row in reader}
 
 
-def parse(handle, names_handle, merged_handle, nodes_handle):
+def parse_ref_proteomes(handle):
+    reader = csv.reader(handle, delimiter="\t")
+    for _ in range(15):
+        next(reader)  # skip preamble & header
+    return {int(row[1]) for row in reader}
+
+
+def parse(handle, names_handle, merged_handle, nodes_handle, ref_proteomes_handle=None):
     lineage = ncbi_reader(handle)
     names = grouped_extra(names_handle)
     merged = grouped_extra(merged_handle, group_idx=1)
     nodes = parse_nodes(nodes_handle)
+    ref_proteomes = (
+        parse_ref_proteomes(ref_proteomes_handle) if ref_proteomes_handle else set()
+    )
 
     for raw in lineage:
         possible_names = names.get(raw[0], [])
         rank = nodes.get(raw[0], "")
-        entry = TaxonomyEntry.build(raw, possible_names, rank=rank)
+        is_ref_proteome = int(raw[0]) in ref_proteomes
+        entry = TaxonomyEntry.build(
+            raw, possible_names, rank=rank, reference_proteome=is_ref_proteome
+        )
         yield entry
 
         for (old_tax_id, replaced) in merged.get(raw[0], []):
@@ -115,17 +131,23 @@ def parse(handle, names_handle, merged_handle, nodes_handle):
             yield attr.evolve(entry, tax_id=int(old_tax_id), replaced_by=entry.tax_id)
 
 
-def parse_directory(directory: Path) -> ty.Iterable[TaxonomyEntry]:
+def parse_directory(
+    directory: Path, ref_proteomes_path=None
+) -> ty.Iterable[TaxonomyEntry]:
     names = ["fullnamelineage.dmp", "names.dmp", "merged.dmp", "nodes.dmp"]
     filenames = [os.path.join(directory, name) for name in names]
     with ExitStack() as stack:
         files = [stack.enter_context(open(f)) for f in filenames]
-        yield from parse(*files)
+        if ref_proteomes_path:
+            ref_handle = stack.enter_context(open(ref_proteomes_path))
+            yield from parse(*files, ref_proteomes_handle=ref_handle)
+        else:
+            yield from parse(*files)
 
 
-def write(directory: Path, output):
+def write(directory: Path, output, ref_proteomes_path=None):
     writer = csv.writer(output)
-    for entry in parse_directory(directory):
+    for entry in parse_directory(directory, ref_proteomes_path=ref_proteomes_path):
         writer.writerows(entry.writeable())
 
 
