2f8290b9cde841be36fd927d1c49c40b286bebc2
Andrew Green <agreen@ebi.ac.uk>
2026-06-08 15:13:32 +0100

    Remove unneeded json casts for metadata
    It is stored as jsonb so we shouldn't need to cast it to pull stuff out like this

--- files/ftp-export/rediportal/editing-locations.sql
+++ files/ftp-export/rediportal/editing-locations.sql
@@ -2,9 +2,9 @@ COPY(
 SELECT
   upi || '_' || taxid AS urs_taxid,
   start AS sequence_location,
-  concat(metadata::json->'genomic_location'->>'chromosome', ':', metadata::json->'genomic_location'->'start', '-', metadata::json->'genomic_location'->'stop') AS genomic_location,
-  metadata::json->'reference' AS reference,
-  metadata::json->'edit' AS edit
+  concat(metadata->'genomic_location'->>'chromosome', ':', metadata->'genomic_location'->'start', '-', metadata->'genomic_location'->'stop') AS genomic_location,
+  metadata->'reference' AS reference,
+  metadata->'edit' AS edit
 FROM rnc_sequence_features
 WHERE feature_provider = 'REDIPORTAL'
 ) TO STDOUT WITH HEADER CSV QUOTE ' '
