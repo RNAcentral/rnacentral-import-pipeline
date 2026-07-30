925cdcc6141c89351d29bfd4cc1012b793a1c2aa
Andrew Green <agreen@ebi.ac.uk>
2026-07-30 11:15:58 +0100

    Test updates

--- tests/databases/ncbi/gene/fetch_test.py
+++ tests/databases/ncbi/gene/fetch_test.py
@@ -15,8 +15,7 @@ limitations under the License.
 
 import pytest
 
-from rnacentral_pipeline.databases.ncbi.gene import fetch
-from rnacentral_pipeline.databases.ncbi.gene import helpers
+from rnacentral_pipeline.databases.ncbi.gene import fetch, helpers
 
 
 @pytest.fixture(scope="module")
@@ -34,7 +33,7 @@ def test_can_get_expected_sequences(simple_sequences):
     "gene_id,sequence_id",
     {
         ("108869573", "NC_024802.2"),
-        ("107303968", "XR_001549991.1"),
+        ("107303968", "XR_001549991.2"),
     },
 )
 def test_can_fetch_correct_sequence_data(simple_sequences, gene_id, sequence_id):
