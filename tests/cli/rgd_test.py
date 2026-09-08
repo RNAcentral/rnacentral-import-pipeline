13882c006fdc0fe6bcb046964cb5fa87d9ab2a62
Andrew Green <agreen@ebi.ac.uk>
2026-09-08 14:37:18 +0100

    FIx to use monkeypatched obo read_obo in rgd test

--- tests/cli/rgd_test.py
+++ tests/cli/rgd_test.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from click.testing import CliRunner
 import obonet
+from click.testing import CliRunner
 
-obonet.read_obo = lambda _: {}
 from rnacentral_pipeline.cli import rgd as rgd_cli
 from rnacentral_pipeline.databases import rgd as rgd_db
 
 
 def test_can_parse_rgd_files(tmp_path, monkeypatch):
+    monkeypatch.setattr(obonet, "read_obo", lambda _: {})
     monkeypatch.setattr(rgd_db.helpers.phy, "lineage", lambda _: "lineage")
     monkeypatch.setattr(rgd_db.helpers.phy, "common_name", lambda _: "Norway rat")
     monkeypatch.setattr(rgd_db.helpers.phy, "species", lambda _: "Rattus norvegicus")
