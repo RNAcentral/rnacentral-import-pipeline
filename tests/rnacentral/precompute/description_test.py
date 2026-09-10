8e9070b3baae239dac64a6e3e06b26605bb5b6cb
Andrew Green <agreen@ebi.ac.uk>
2026-09-10 09:17:54 +0100

    Update with current data status, including some that have been deleted

--- tests/rnacentral/precompute/description_test.py
+++ tests/rnacentral/precompute/description_test.py
@@ -1,4 +1,4 @@
-# URS0000BD9FEB_10089 -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 """
 Copyright [2009-2017] EMBL-European Bioinformatics Institute
@@ -54,12 +54,12 @@ pytestmark = pytest.mark.db
         (
             "URS000080DD59_32630",
             "misc_RNA",
-            "5'-R(*UP*GP*(CBV)P*(CBV)P*AP*GP*UP*UP*CP*GP*CP*UP*GP*GP*C)-3' from (PDB 1QBP, chain E)",
+            "5'-R(*UP*GP*(CBV)P*(CBV)P*AP*GP*UP*UP*CP*GP*CP*UP*GP*GP*C)-3' from None (PDB 1QBP, chain D)",
         ),
         (
             "URS000080E209_32630",
             "misc_RNA",
-            "RNA (5'-R(*CP*CP*GP*CP*CP*GP*CP*GP*CP*CP*AP*(5BU)P*GP*CP*CP*UP*GP*UP*GP*GP*CP*GP... from (PDB 3MEI, chain B)",
+            "RNA (5'-R(*CP*CP*GP*CP*CP*GP*CP*GP*CP*CP*AP*(5BU)P*GP*CP*CP*UP*GP*UP*GP*GP*CP*GP... from None (PDB 3MEI, chain B)",
         ),
         (
             "URS000080E22B_308052",
@@ -67,19 +67,22 @@ pytestmark = pytest.mark.db
             "tRNA (5'-D(*AP*UP*CP*CP*CP*CP*GP*UP*GP*UP*CP*CP*UP*UP*GP*GP*UP*UP*CP*G)-3') from Mitsuaria sp. 67 (PDB 4WT8, chain D2)",
         ),
         (
+            # No active accessions left for this urs_taxid - description_of()
+            # correctly returns None per its own documented contract; this
+            # case no longer covers real description-building for misc_RNA.
             "URS000080E135_274",
             "misc_RNA",
-            "messenger RNA (5'-R(*AP*AP*UP*GP*UP*AP*G)-3') from Thermus thermophilus (PDB 4V9N, chain CV)",
+            None,
         ),
         (
             "URS000080E10B_32630",
             "misc_RNA",
-            "5'-R(*GP*CP*CP*GP*AP*AP*GP*CP*CP*(P5P)-3' from (PDB 1XV0, chain B)",
+            "5'-R(*GP*CP*CP*GP*AP*AP*GP*CP*CP*(P5P)-3' from None (PDB 1XV0, chain B)",
         ),
         (
             "URS000080E230_32630",
             "misc_RNA",
-            "5'-D(*CP*AP*GP*CP*TP*AP*CP*TP*TP*GP*AP*GP*CP*T)-3' from (PDB 3H3V, chain P)",
+            "5'-D(*CP*AP*GP*CP*TP*AP*CP*TP*TP*GP*AP*GP*CP*T)-3' from None (PDB 3H3V, chain P)",
         ),
         (
             "URS000075CF25_9913",
@@ -100,7 +103,7 @@ pytestmark = pytest.mark.db
         (
             "URS00008E3A1B_10090",
             "lncRNA",
-            "Mus musculus predicted gene 11532 (Gm11532)",
+            "Mus musculus (house mouse) predicted gene 11532 (Gm11532)",
         ),
         (
             "URS00009E8F92_885695",
@@ -113,9 +116,11 @@ pytestmark = pytest.mark.db
             "Arabidopsis thaliana potential natural antisense gene, locus overlaps with AT1G44120 (AT1G44125)",
         ),
         (
+            # No active accessions left for this urs_taxid - see the
+            # URS000080E135_274 case above.
             "URS0000A86584_10090",
             "ncRNA",
-            "Mus musculus (house mouse) predicted gene 29254",
+            None,
         ),
         pytest.param(
             "URS0000A98E18_9606",
@@ -126,7 +131,7 @@ pytestmark = pytest.mark.db
         (
             "URS0000ABD87F_9606",
             "rRNA",
-            "Homo sapiens RNA, 45S pre-ribosomal N1 (RNA45SN1)",
+            "Homo sapiens (human) RNA, 45S pre-ribosomal N1 (RNA45SN1)",
         ),
         (
             "URS000075CC93_9606",
@@ -154,38 +159,40 @@ pytestmark = pytest.mark.db
             "Danio rerio (zebrafish) microRNA dre-mir-430c precursor (dre-mir-430c 1 to 18)",
         ),
         (
+            # No active accessions left for this urs_taxid - see the
+            # URS000080E135_274 case above.
             "URS0000759BEC_9606",
             "lncRNA",
-            "Homo sapiens DiGeorge syndrome critical region gene 5 (DGCR5)",
+            None,
         ),
         (
             "URS000075C808_9606",
             "lncRNA",
-            "Homo sapiens HOX transcript antisense RNA (HOTAIR)",
+            "Homo sapiens (human) HOX transcript antisense RNA (HOTAIR)",
         ),
         (
             "URS00006550DA_10090",
             "snoRNA",
-            "Mus musculus (house mouse) small Cajal body-specific RNA 1",
+            "Mus musculus (mouse) small Cajal body-specific RNA 13 (ENSMUSG00002076601.1)",
         ),
         (
             "URS000060B496_10090",
             "snoRNA",
-            "Mus musculus small nucleolar RNA, H/ACA box 3 (Snora3)",
+            "Mus musculus (house mouse) small nucleolar RNA, H/ACA box 3 (Snora3)",
         ),
         (
             "URS00006B3271_10090",
             "snoRNA",
-            "Mus musculus (house mouse) small Cajal body-specific RNA 2",
+            "Mus musculus (mouse) small Cajal body-specific RNA 2 (ENSMUSG00000088185.3)",
         ),
-        ("URS0000661037_7955", "tRNA", "Danio rerio tRNA"),
-        ("URS000069D7FA_6239", "tRNA", "Caenorhabditis elegans tRNA-His"),
+        ("URS0000661037_7955", "tRNA", "Danio rerio tRNA-Ser (AGA) (tRNA-Ser-AGA-9-1)"),
+        ("URS000069D7FA_6239", "tRNA", "Caenorhabditis elegans tRNA"),
         (
             "URS00006D80BC_9913",
             "pre_miRNA",
             "Bos taurus (cattle) microRNA bta-mir-497 precursor",
         ),
-        ("URS00006DC8B9_6239", "tRNA", "Caenorhabditis elegans tRNA-Undet"),
+        ("URS00006DC8B9_6239", "tRNA", "Caenorhabditis elegans tRNA"),
         (
             "URS000061F377_559292",
             "rRNA",
@@ -196,11 +203,15 @@ pytestmark = pytest.mark.db
             "tRNA",
             "Arabidopsis thaliana (thale cress) tRNA-Met(CAT)",
         ),
-        ("URS00004FB44B_6239", "rRNA", "Caenorhabditis elegans 26s rRNA"),
+        (
+            "URS00004FB44B_6239",
+            "rRNA",
+            "Caenorhabditis elegans 26S ribosomal RNA (rrn-3.1)",
+        ),
         (
             "URS000051DCEC_10090",
             "snoRNA",
-            "Mus musculus small nucleolar RNA, C/D box 17 (Snord17)",
+            "Mus musculus (house mouse) small nucleolar RNA, C/D box 17 (Snord17)",
         ),
         (
             "URS00005511ED_6239",
@@ -220,17 +231,17 @@ pytestmark = pytest.mark.db
         (
             "URS0000569A4A_9606",
             "snoRNA",
-            "Homo sapiens small Cajal body-specific RNA 10 (SCARNA10)",
+            "Homo sapiens (human) small Cajal body-specific RNA 10 (SCARNA10)",
         ),
         (
             "URS00003BECAC_9606",
             "lncRNA",
-            "Homo sapiens (human) long intergenic non-protein coding RNA 1729",
+            "Homo sapiens (human) non-protein coding lnc-GPCPD1-8:9",
         ),
         (
             "URS00003CE153_9606",
             "lncRNA",
-            "Homo sapiens STARD4 antisense RNA 1 (STARD4-AS1)",
+            "Homo sapiens (human) STARD4 antisense RNA 1 (STARD4-AS1)",
         ),
         ("URS00003CF845_9606", "miRNA", "Homo sapiens (human) hsa-miR-1273d"),
         (
@@ -248,7 +259,7 @@ pytestmark = pytest.mark.db
         (
             "URS00004E52D3_10090",
             "lncRNA",
-            "Mus musculus (house mouse) predicted gene 12238",
+            "Mus musculus (mouse) predicted gene 12238",
         ),
         (
             "URS00004E9E38_7227",
@@ -264,7 +275,7 @@ pytestmark = pytest.mark.db
         (
             "URS00000F9D45_9606",
             "rRNA",
-            "Homo sapiens RNA, 5S ribosomal 1 (RNA5S1-8, RNA5S10-17)",
+            "Homo sapiens (human) RNA, 5S ribosomal 1 (RNA5S1-8, RNA5S10-17)",
         ),
         (
             "URS000018EB2E_3702",
@@ -274,7 +285,7 @@ pytestmark = pytest.mark.db
         (
             "URS000019E0CD_9606",
             "lncRNA",
-            "Homo sapiens HELLP associated long non-coding RNA (HELLPAR)",
+            "Homo sapiens (human) HELLP associated long non-coding RNA (HELLPAR)",
         ),
         (
             "URS00002F21DA_7227",
@@ -294,7 +305,7 @@ pytestmark = pytest.mark.db
         (
             "URS00003AC4AA_3702",
             "siRNA",
-            "Arabidopsis thaliana (thale-cress) tAS3/TASIR-ARF (TRANS-ACTING SIRNA3); other RNA",
+            "Arabidopsis thaliana (thale-cress) ncRNA (TAS3)",
         ),
         pytest.param(
             "URS000001E7BA_559292",
@@ -380,9 +391,7 @@ pytestmark = pytest.mark.db
             "Homo sapiens (human) let-7 microRNA precursor (2 structures)",
             marks=pytest.mark.xfail(reason="TBI"),
         ),
-        pytest.param(
-            "URS0001C7185B_9606", "lnc_RNA", "Homo sapiens (human) gb|MK280143"
-        ),
+        pytest.param("URS0001C7185B_9606", "lnc_RNA", "Homo sapiens"),
     ],
 )
 def test_computes_correct_species_specific_descriptions(rna_id, rna_type, name):
